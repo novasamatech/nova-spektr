32f793d190a5d237a6980c0a3eb6a46c972da14c
Aleksandr Makhnev <alma2610@ya.ru>
2022-08-03 18:32:39 +0500

    feat: implement balances service (#12)
    * feat: implement balances service
    * feat: integrate data verification
    * feat: add sorting for chains

--- src/renderer/screens/Balances/Balances.test.tsx
+++ src/renderer/screens/Balances/Balances.test.tsx
@@ -2,11 +2,53 @@ import { render, screen } from '@testing-library/react';
 
 import Balances from './Balances';
 
+jest.mock('@renderer/context/NetworkContext', () => ({
+  useNetworkContext: jest.fn().mockReturnValue({
+    connections: {
+      '0x0000000000000000000000000000000000000000': {
+        chainId: '1',
+        assets: [
+          {
+            assetId: '1',
+          },
+          { assetId: '2' },
+        ],
+      },
+      '0x0000000000000000000000000000000000000001': {
+        chainId: '2',
+        assets: [
+          {
+            assetId: '1',
+          },
+        ],
+      },
+    },
+  }),
+}));
+
+// TODO: Find way to avoid warnings with this mock
+jest.mock('./Balance', () => () => <div>Balance</div>);
+
 describe('screen/Balances', () => {
   test('should render component', () => {
     render(<Balances />);
-
     const text = screen.getByText('Balances');
+
     expect(text).toBeInTheDocument();
+  });
+
+  test('should render chains', () => {
+    render(<Balances />);
+    const chains = screen.getAllByText('Token');
+
+    expect(chains.length).toEqual(2);
+  });
+
+  test('should render balances', () => {
+    render(<Balances />);
+
+    const balances = screen.getAllByText('Balance');
+
+    expect(balances.length).toEqual(3);
   });
 });
--- src/renderer/services/network/chainsService.ts
+++ src/renderer/services/network/chainsService.ts
@@ -1,8 +1,24 @@
+import { sortBy } from 'lodash';
+
 import { Chain, IChainService } from './common/types';
 import chains from './common/chains.json';
+import { notNull } from '@renderer/utils/objects';
 
 export function useChains(): IChainService {
   return {
     getChainsData: () => Promise.resolve(chains as unknown as Chain[]),
+    sortChains: (chains: Chain[]): Chain[] => {
+      const polkadot = chains.find((chain) => chain.name === 'Polkadot');
+      if (polkadot) {
+        chains.splice(chains.indexOf(polkadot), 1);
+      }
+
+      const kusama = chains.find((chain) => chain.name === 'Kusama');
+      if (kusama) {
+        chains.splice(chains.indexOf(kusama), 1);
+      }
+
+      return [polkadot, kusama, ...sortBy(chains, 'name')].filter(notNull);
+    },
   };
 }
