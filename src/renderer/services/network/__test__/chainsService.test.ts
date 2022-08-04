16f7ec5354ffb1c8ce9e9828165b5da5589de281
asmadek <alma2610@ya.ru>
2022-08-04 15:37:15 +0500

    fix: add unit tests for sorting

--- src/renderer/services/network/__test__/chainsService.test.ts
+++ src/renderer/services/network/__test__/chainsService.test.ts
@@ -1,5 +1,6 @@
 import { useChains } from '../chainsService';
 import chains from '../common/chains.json';
+import { Chain } from '../common/types';
 
 describe('service/network', () => {
   test('should init', () => {
@@ -9,9 +10,16 @@ describe('service/network', () => {
   });
 
   test('should provide data', async () => {
-    const { getChainsData } = useChains();
-    const data = await getChainsData();
+    const polkadot = chains.find((chain) => chain.name === 'Polkadot') as Chain;
+    const kusama = chains.find((chain) => chain.name === 'Kusama') as Chain;
+    const testnet = chains.find((chain) => chain.options?.includes('testnet')) as Chain;
+    const parachain = chains.find(
+      (chain) => chain.name !== 'Polkadot' && chain.name !== 'Kusama' && !chain.options?.includes('testnet'),
+    ) as Chain;
 
-    expect(data).toEqual(chains);
+    const { sortChains } = useChains();
+    const data = sortChains([testnet, polkadot, parachain, kusama]);
+
+    expect(data).toEqual([polkadot, kusama, parachain, testnet]);
   });
 });
