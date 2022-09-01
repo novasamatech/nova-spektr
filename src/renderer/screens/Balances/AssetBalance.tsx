2f5186cb3d3f0dc696502a96c7f88e762a0e2de1
asmadek <alma2610@ya.ru>
2022-09-01 19:04:28 +0500

    chore: fix border for selected balance

--- src/renderer/screens/Balances/AssetBalance.tsx
+++ src/renderer/screens/Balances/AssetBalance.tsx
@@ -15,62 +15,63 @@ const BalanceRow = ({ asset, balance }: Props) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <div
-      onClick={() => setIsExpanded(!isExpanded)}
-      className={cn(
-        'hover:bg-shade-5 cursor-pointer border-2',
-        isExpanded ? 'rounded-2lg border-primary' : 'border-transparent',
-      )}
-    >
-      <div className="flex items-center justify-between gap-7.5 h-[60px] p-[15px] text-xl ">
-        <div className="flex items-center gap-x-2.5 text-neutral">
-          <div className="flex items-center justify-center bg-shade-40 border border-white border-opacity-75 rounded-full w-[34px] h-[34px] box-border">
-            <img src={asset.icon} alt="" width={26} height={26} />
+    <div onClick={() => setIsExpanded(!isExpanded)}>
+      <div
+        className={cn(
+          'hover:bg-shade-5 cursor-pointer border-2',
+          isExpanded ? 'rounded-2lg border-primary' : 'border-transparent',
+        )}
+      >
+        <div className="flex items-center justify-between gap-7.5 h-[60px] p-[15px] text-xl ">
+          <div className="flex items-center gap-x-2.5 text-neutral">
+            <div className="flex items-center justify-center bg-shade-40 border border-white border-opacity-75 rounded-full w-[34px] h-[34px] box-border">
+              <img src={asset.icon} alt="" width={26} height={26} />
+            </div>
+            {asset.name}
           </div>
-          {asset.name}
+          <div className={cn('font-semibold', !balance?.verified && 'text-shade-50')} data-testid="balance">
+            {balance && balance.free !== undefined ? (
+              `${formatBalance(total(balance), asset.precision)} ${asset.symbol}`
+            ) : (
+              <Shimmering width="200px" height="20px" />
+            )}
+          </div>
         </div>
-        <div className={cn('font-semibold', !balance?.verified && 'text-shade-50')} data-testid="balance">
-          {balance && balance.free !== undefined ? (
-            `${formatBalance(total(balance), asset.precision)} ${asset.symbol}`
-          ) : (
-            <Shimmering width="200px" height="20px" />
-          )}
-        </div>
+        {isExpanded && (
+          <div className="flex divide-x gap-6 px-[15px] py-2">
+            <div>
+              <div className="text-neutral text-sm font-semibold">Transferable</div>
+              <div className="text-neutral-variant text-xs font-bold">
+                {balance ? (
+                  `${formatBalance(transferable(balance), asset.precision)} ${asset.symbol}`
+                ) : (
+                  <Shimmering width="200px" height="20px" />
+                )}
+              </div>
+            </div>
+            <div className="pl-6">
+              <div className="text-neutral text-sm font-semibold">Locked</div>
+              <div className="text-neutral-variant text-xs font-bold">
+                {balance?.frozen ? (
+                  `${formatBalance(locked(balance), asset.precision)} ${asset.symbol}`
+                ) : (
+                  <Shimmering width="200px" height="20px" />
+                )}
+              </div>
+            </div>
+            <div className="pl-6">
+              <div className="text-neutral text-sm font-semibold">Reserved</div>
+              <div className="text-neutral-variant text-xs font-bold">
+                {balance ? (
+                  `${formatBalance(balance.reserved || '0', asset.precision)} ${asset.symbol}`
+                ) : (
+                  <Shimmering width="200px" height="20px" />
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-      {isExpanded && (
-        <div className="flex divide-x gap-6 px-[15px] py-2">
-          <div>
-            <div className="text-neutral text-sm font-semibold">Transferable</div>
-            <div className="text-neutral-variant text-xs font-bold">
-              {balance ? (
-                `${formatBalance(transferable(balance), asset.precision)} ${asset.symbol}`
-              ) : (
-                <Shimmering width="200px" height="20px" />
-              )}
-            </div>
-          </div>
-          <div className="pl-6">
-            <div className="text-neutral text-sm font-semibold">Locked</div>
-            <div className="text-neutral-variant text-xs font-bold">
-              {balance?.frozen ? (
-                `${formatBalance(locked(balance), asset.precision)} ${asset.symbol}`
-              ) : (
-                <Shimmering width="200px" height="20px" />
-              )}
-            </div>
-          </div>
-          <div className="pl-6">
-            <div className="text-neutral text-sm font-semibold">Reserved</div>
-            <div className="text-neutral-variant text-xs font-bold">
-              {balance ? (
-                `${formatBalance(balance.reserved || '0', asset.precision)} ${asset.symbol}`
-              ) : (
-                <Shimmering width="200px" height="20px" />
-              )}
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 };
