fac9dac06b7c5febc4d0dd678f6c02d29cabb5da
Yaroslav Grachev <yaroslav@novawallet.io>
2022-09-06 19:14:34 +0300

    fix: parity signer bugfix

--- src/renderer/components/ui/Identicon/Identicon.tsx
+++ src/renderer/components/ui/Identicon/Identicon.tsx
@@ -1,14 +1,16 @@
 import { Identicon as PolkadotIdenticon } from '@polkadot/react-identicon';
 import { IconTheme } from '@polkadot/react-identicon/types';
+import cn from 'classnames';
 import { useLayoutEffect, useRef } from 'react';
 
 type Props = {
   theme?: IconTheme;
   address: string;
-  size: number;
+  size?: number;
+  background?: boolean;
 };
 
-const Identicon = ({ theme = 'polkadot', address, size }: Props) => {
+const Identicon = ({ theme = 'polkadot', address, size = 24, background = true }: Props) => {
   const wrapperRef = useRef<HTMLButtonElement>(null);
 
   useLayoutEffect(() => {
@@ -25,12 +27,17 @@ const Identicon = ({ theme = 'polkadot', address, size }: Props) => {
     <button
       type="button"
       ref={wrapperRef}
-      className={`flex justify-center items-center bg-white rounded-full cursor-copy`}
+      className={cn(`flex justify-center items-center rounded-full cursor-copy`, background && 'bg-white')}
       style={{ width: size, height: size }}
       data-testid={`identicon-${address}`}
       onClick={onCopyToClipboard}
     >
-      <PolkadotIdenticon theme={theme} value={address} size={size * 0.75} className="pointer-events-none" />
+      <PolkadotIdenticon
+        theme={theme}
+        value={address}
+        size={background ? size * 0.75 : size}
+        className="pointer-events-none"
+      />
     </button>
   );
 };
