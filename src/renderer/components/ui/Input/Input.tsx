1069e239208e3a966fbc626633f1b1294b76ff17
Yaroslav Grachev <tuulwq@gmail.com>
2022-09-07 09:26:27 +0300

    fix: parity signer bugfix (#37)

--- src/renderer/components/ui/Input/Input.tsx
+++ src/renderer/components/ui/Input/Input.tsx
@@ -36,7 +36,8 @@ const Input = forwardRef<Ref, InputProps>(
     return (
       <label
         className={cn(
-          'relative flex items-center rounded-2lg p-2.5 box-border border-2 bg-shade-2 text-sm font-normal leading-5',
+          'relative flex items-center rounded-2lg p-2.5 box-border border-2 bg-shade-2',
+          'text-sm font-normal leading-5 focus-within:border-primary',
           invalid ? 'border-error' : 'border-shade-2',
           label && 'rounded-2lg text-lg px-2.5 pb-0 pt-5',
           wrapperClass,
@@ -46,8 +47,8 @@ const Input = forwardRef<Ref, InputProps>(
         {label && <span className="absolute top-2.5 font-bold text-neutral-variant uppercase text-xs">{label}</span>}
         <input
           className={cn(
-            'focus:outline-none rounded-sm leading-5 bg-transparent flex-1',
-            value && !invalid && 'text-primary',
+            'rounded-sm leading-5 bg-transparent flex-1 focus:outline-none focus:text-primary',
+            value && !invalid && 'text-neutral',
             invalid && 'text-error',
             label && 'py-1 my-4',
             prefixElement && 'ml-3',
--- src/renderer/screens/Onboarding/Parity/StepThree/StepThree.test.tsx
+++ src/renderer/screens/Onboarding/Parity/StepThree/StepThree.test.tsx
@@ -30,6 +30,46 @@ describe('screens/Onboard/Parity/StepThree', () => {
     });
 
     const title = screen.getByRole('heading', { name: 'Please choose a name for your wallet' });
+    const address = screen.getByDisplayValue('1ChFWeNRLa...Wz7jX7iTVZ');
     expect(title).toBeInTheDocument();
+    expect(address).toBeInTheDocument();
+  });
+
+  test('should not submit form without name', async () => {
+    const spyNextStep = jest.fn();
+
+    await act(async () => {
+      render(
+        <StepThree
+          ss58Address="1ChFWeNRLarAPRCTM3bfJmncJbSAbSS9yqjueWz7jX7iTVZ"
+          onNextStep={spyNextStep}
+          onPrevStep={() => {}}
+        />,
+      );
+    });
+
+    const button = screen.getByRole('button', { name: 'Type a name to finish...' });
+    button.click();
+
+    expect(spyNextStep).not.toBeCalled();
+  });
+
+  test('should go to scan page', async () => {
+    const spyPrevStep = jest.fn();
+
+    await act(async () => {
+      render(
+        <StepThree
+          ss58Address="1ChFWeNRLarAPRCTM3bfJmncJbSAbSS9yqjueWz7jX7iTVZ"
+          onNextStep={() => {}}
+          onPrevStep={spyPrevStep}
+        />,
+      );
+    });
+
+    const button = screen.getByRole('button', { name: 'Rescan QR code' });
+    button.click();
+
+    expect(spyPrevStep).toBeCalled();
   });
 });
