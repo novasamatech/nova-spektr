99130aa098c0158870754468a533aa56dba72f43
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-09-12 17:37:36 +0300

    cahnge matrix test

--- tests/integrations/matrix/matrix.base.test.ts
+++ tests/integrations/matrix/matrix.base.test.ts
@@ -4,7 +4,7 @@ import { BASE_MATRIX_URL } from '@renderer/services/matrix/common/constants';
 import Matrix, { Membership, Signatory } from '../../../src/renderer/services/matrix';
 import { createRoom } from '../utils/matrixCreateRoom';
 import { matrixLoginAndSync } from '../utils/matrixLogin';
-import signatories from './data.json';
+import test_data from './matrix_data.json';
 
 /**
  * Matrix integration tests
@@ -26,36 +26,35 @@ describe('services/matrix', () => {
     await matrix.logout();
   });
 
-  test.each([['omni_test_account', '1q2w3eQWE!@']])('User %s can login', async (login, password) => {
-    await matrixLoginAndSync(matrix, login, password);
+  test.each([test_data.test_accounts])('User %s can login', async (credentials) => {
+    await matrixLoginAndSync(matrix, credentials.login, credentials.password);
 
     expect(matrix.isLoggedIn).toBe(true);
   });
 
-  test.each([['omni_test_account', '1q2w3eQWE!@', signatories]])(
-    'User %s can create the room',
-    async (login, password, signatories) => {
-      await matrixLoginAndSync(matrix, login, password);
+  test.each([test_data.test_accounts])('User %s can create the room', async (credentials) => {
+    await matrixLoginAndSync(matrix, credentials.login, credentials.password);
 
-      const signatoriesArray: Signatory[] = signatories;
+    const signatoriesArray: Signatory[] = test_data.signatories;
 
-      const room = await createRoom(
-        matrix,
-        '5Cf68pb7B4kDS59PXwXKXf196HECmRh6St6aZ22U8PS13iMN',
-        login,
-        2,
-        signatoriesArray,
-      );
+    const room = await createRoom(
+      matrix,
+      '5Cf68pb7B4kDS59PXwXKXf196HECmRh6St6aZ22U8PS13iMN',
+      credentials.login,
+      2,
+      signatoriesArray,
+    );
 
-      const room_list_after_creating = matrix.listOfOmniRooms(Membership.JOIN);
+    await matrix.joinRoom(room.roomId);
 
-      expect(room_list_after_creating).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            roomId: room.roomId,
-          }),
-        ]),
-      );
-    },
-  );
+    const room_list_after_creating = matrix.listOfOmniRooms(Membership.JOIN);
+
+    expect(room_list_after_creating).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          roomId: room.roomId,
+        }),
+      ]),
+    );
+  });
 });
