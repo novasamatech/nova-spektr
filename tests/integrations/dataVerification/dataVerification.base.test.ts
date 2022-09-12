7e95b55801c1e8bbe3125fff9e68c6a79c0b8bc2
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-09-12 22:48:21 +0300

    Adding negative cases

--- tests/integrations/dataVerification/dataVerification.base.test.ts
+++ tests/integrations/dataVerification/dataVerification.base.test.ts
@@ -1,5 +1,6 @@
 import { ApiPromise } from '@polkadot/api';
 import { AccountInfo } from '@polkadot/types/interfaces';
+import { Codec } from '@polkadot/types/types';
 
 import chains from '../../../src/renderer/services/network/common/chains.json';
 import { validate } from '../../../src/renderer/services/dataVerification/dataVerification';
@@ -21,38 +22,64 @@ let [networks, polkadotParachains, kusamaParachains, polkadot, kusama] = prepare
  * @group dataVerification/base
  */
 
-describe('services/dataVerification', () => {
+describe('Verification function can verify parachains', () => {
   let polkadotApi: ApiPromise;
   let kusamaApi: ApiPromise;
   let testAccounts: TestAccounts[];
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     polkadotApi = await createWsConnection(polkadot.nodes[0].url);
     kusamaApi = await createWsConnection(kusama.nodes[0].url);
     testAccounts = await getTestAccounts(TestAccountsURL);
   });
 
-  test.each(polkadotParachains)('Can verify data for polkadot', async (parachain) => {
+  test.each(polkadotParachains)('Can verify data for polkadot parachain: $name', async (parachain) => {
     const parachainApi = await createWsConnection(parachain.nodes[0].url);
     const parachainAccount = testAccounts.find((data) => data.chainId == parachain.chainId.slice(2));
-
     const storageKey = polkadotApi.query.system.account.key(parachainAccount?.account);
 
-    parachainApi.query.system.account(parachainAccount?.account, async (data: AccountInfo) => {
-      const validationStatus = await validate(polkadotApi, parachainApi, storageKey, data);
-      expect(validationStatus).toBe(true);
-    });
+    const data = await parachainApi.query.system.account(parachainAccount?.account);
+    const validationStatus = await validate(polkadotApi, parachainApi, storageKey, data);
+
+    expect(validationStatus).toBe(true);
   });
 
-  test.each(kusamaParachains)('Can verify data for kusama', async (parachain) => {
+  test.each(kusamaParachains)('Can verify data for kusama parachain: $name', async (parachain) => {
     const parachainApi = await createWsConnection(parachain.nodes[0].url);
-
     const parachainAccount = testAccounts.find((data) => data.chainId == parachain.chainId.slice(2));
     const storageKey = polkadotApi.query.system.account.key(parachainAccount?.account);
 
-    parachainApi.query.system.account(parachainAccount?.account, async (data: AccountInfo) => {
-      const validationStatus = await validate(kusamaApi, parachainApi, storageKey, data);
-      expect(validationStatus).toBe(true);
-    });
+    const data = await parachainApi.query.system.account(parachainAccount?.account);
+    const validationStatus = await validate(kusamaApi, parachainApi, storageKey, data);
+
+    expect(validationStatus).toBe(true);
+  });
+
+  test.each(kusamaParachains)('Verification return false if nonce was changed for $name', async (parachain) => {
+    const parachainApi = await createWsConnection(parachain.nodes[0].url);
+    const parachainAccount = testAccounts.find((data) => data.chainId == parachain.chainId.slice(2));
+    const storageKey = polkadotApi.query.system.account.key(parachainAccount?.account);
+
+    let data = (await parachainApi.query.system.account(parachainAccount?.account)) as AccountInfo;
+    // changing nonce
+    const new_nonce_value = data.data.free as Codec;
+    data.set('nonce', new_nonce_value);
+
+    const validationStatus = await validate(kusamaApi, parachainApi, storageKey, data);
+    expect(validationStatus).toBe(false);
+  });
+
+  test.each(polkadotParachains)('Verification return false if balance was changed for $name', async (parachain) => {
+    const parachainApi = await createWsConnection(parachain.nodes[0].url);
+    const parachainAccount = testAccounts.find((data) => data.chainId == parachain.chainId.slice(2));
+    const storageKey = polkadotApi.query.system.account.key(parachainAccount?.account);
+
+    let data = (await parachainApi.query.system.account(parachainAccount?.account)) as AccountInfo;
+    // changing balance
+    const new_balance_value = data.data.feeFrozen as Codec;
+    data.data.set('free', new_balance_value);
+
+    const validationStatus = await validate(polkadotApi, parachainApi, storageKey, data);
+    expect(validationStatus).toBe(false);
   });
 });
