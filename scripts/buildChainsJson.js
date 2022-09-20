1c2b1ca8ed3337f6862fd17849e1d1cb0667a30a
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-09-20 15:05:03 +0300

    Fix some problems in implementation

--- scripts/buildChainsJson.js
+++ scripts/buildChainsJson.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 
 const tokenNames = require('./assetsNameMap.json');
 
-const NOVA_CONFIG_VERSION = process.env.CHAINS_VERSION ? process.env.CHAINS_VERSION : 'v5';
+const NOVA_CONFIG_VERSION = process.env.CHAINS_VERSION || 'v5';
 const CONFIG_PATH = 'src/renderer/services/network/common';
 const NOVA_CONFIG_URL = `https://raw.githubusercontent.com/nova-wallet/nova-utils/master/chains/${NOVA_CONFIG_VERSION}/`;
 
@@ -12,51 +12,43 @@ const CHAINS_ENV = ['chains_dev.json', 'chains.json'];
 
 const DO_NOT_SUPPORT_CHAINS = ['Moonriver', 'Moonbeam'];
 
-async function getDataViaHttp(url, file_path) {
-  return await axios
-    .get(url + file_path)
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      console.log('Error: ', err.message);
-    });
+async function getDataViaHttp(url, filePath) {
+  try {
+    const response = await axios.get(url + filePath);
+
+    return response.data;
+  } catch (error) {
+    console.log('Error: ', error?.message || 'getDataViaHttp failed');
+  }
 }
 
-async function modifydData(rawData) {
-  const newDataSet = rawData.map((chain) => {
+function getTransformedData(rawData) {
+  return rawData.reduce((acc, chain) => {
     if (!DO_NOT_SUPPORT_CHAINS.includes(chain.name)) {
-      chain.chainId = ['0x', chain.chainId].join('');
+      chain.chainId = `0x${chain.chainId}`;
+      if (chain.parentId) chain.parentId = `0x${chain.parentId}`;
       chain.assets.forEach((asset) => {
-        asset.name = tokenNames[asset.symbol] ? tokenNames[asset.symbol] : 'Need to add in assetsNameMap';
+        asset.name = tokenNames[asset.symbol] || 'Should be included in assetsNameMap';
       });
-
-      return chain;
+      acc.push(chain);
     }
 
-    return null;
-  });
-
-  return newDataSet.filter((chain) => {
-    return chain != null;
-  });
+    return acc;
+  }, []);
 }
 
-async function saveNewFile(new_json, file_name) {
+async function saveNewFile(newJson, file_name) {
   try {
-    await writeFile(resolve(CONFIG_PATH, file_name), JSON.stringify(new_json, null, 2));
-  } catch ({ message }) {
-    console.log(`
-    🛑 Something went wrong!\n
-      👀 Error: ${message}
-    `);
+    await writeFile(resolve(CONFIG_PATH, file_name), JSON.stringify(newJson, null, 2));
+  } catch (error) {
+    console.log('Error: ', error?.message || '🛑 Something went wrong in writing file');
   }
 }
 
 async function buildFullChainsJSON() {
   CHAINS_ENV.forEach(async function (chain) {
     const novaChainsConfig = await getDataViaHttp(NOVA_CONFIG_URL, chain);
-    const modifiedData = await modifydData(novaChainsConfig);
+    const modifiedData = await getTransformedData(novaChainsConfig);
     await saveNewFile(modifiedData, 'omni-' + chain);
     console.log('Was successfuly generated for ', chain);
   });
