928ef54515f3695ef10a32c6e6cf09b35ff20ddf
pgolovkin <golovkin.pl@gmail.com>
2022-10-15 10:01:35 +0300

    Added comment that clarify how mulutiframe scanning works.

--- src/renderer/screens/CameraDev/MultiframeQrReader.tsx
+++ src/renderer/screens/CameraDev/MultiframeQrReader.tsx
@@ -25,6 +25,7 @@ type Props = {
   onError?: (error: ErrorObject) => void;
 };
 
+//for decoding from scale encoded bytes to the TS object
 interface AddrInfo {
   name: string;
   network: string;
@@ -33,6 +34,7 @@ interface AddrInfo {
   encryption: Encryption
 }
 
+//encryption enum
 enum Encryption {
   Ed25519,
   Sr25519,
@@ -50,6 +52,7 @@ const $addr_info: Codec<AddrInfo> = $.object(
   ["encryption", $encryption],
 );
 
+//export address format for decoding. Rust enum is a tagged union.
 const $export_address = $.taggedUnion("ExportAddrs", [["V1", ["addrs", $.array($addr_info)]]]);
 
 
@@ -64,6 +67,8 @@ const MultiframeQRReader = ({ size = 300, cameraId, onCameraList, onResult, onSt
 
   const selectedDeviceId = '548f70c0a4f4e422895669d5a8d8aa512219126e1d41b1bd3c98eede896bf0e3';
   const videoRef = useRef<HTMLVideoElement>(null);
+
+  //this object holds the progress of the multiframe scanning
   let inProgress = {
     length: -1,
     total: -1,
@@ -82,11 +87,13 @@ const MultiframeQRReader = ({ size = 300, cameraId, onCameraList, onResult, onSt
             try {
               frame = RaptorqFrame.try_from(scanningResult[0]);
             } catch (e) {
+              //if frame may not be recognized then it's a plain text, like `substrate:${address}:${wallet.publicKey}:Ff`
               console.log(`text in the result is ${result.getText()}`);
 
               return;
             } //non text result
             if (frame) {
+              //if frame was recognized it has to be processed
               let length = frame.size();
               let total = frame.total();
               let newPacket = frame.payload();
@@ -95,14 +102,17 @@ const MultiframeQRReader = ({ size = 300, cameraId, onCameraList, onResult, onSt
               let raptorDecoder = Decoder.with_defaults(BigInt(length), decodedPacket.data().length);
               packets.push(newPacket);
               if (status == 0) {
+                //if it's the first frame from the multiframe QR
                 let fountainResult = raptorDecoder.decode(newPacket);
                 if (fountainResult) {
+                  //try to decode the 1st frame. Maybe it's a single frame QR.
                   console.log(`result ${u8aToHex(fountainResult)}`);
                   let res = $export_address.decode(fountainResult);
                   console.log(res);
                   status = 0;
                   packets = [];
                 } else {
+                  //if there are more then 1 frame proceed scanning and keep the progress
                   status = 1;
                   inProgress = {
                     length,
@@ -112,14 +122,18 @@ const MultiframeQRReader = ({ size = 300, cameraId, onCameraList, onResult, onSt
                   inProgress.collected.add(blockNumber);
                 }
               } else if (status == 1) {
+                //check if the user has started to scan another QR code.
                 if (inProgress.length != length) {
                   console.error(`This is conflicted payload ${inProgress.length} ${length}`);
                 }
+                //collect new frame
                 inProgress.collected.add(blockNumber);
                 console.log(`added ${inProgress.collected.size} and total is ${inProgress.total}`);
                 for (let i = 0; i < packets.length; i++) {
+                  //try to decode data.
                   let fountainResult = raptorDecoder.decode(packets[i]);
                   if (fountainResult) {
+                    //if data was decoded show it and finish scanning
                     console.log(`result ${u8aToHex(fountainResult)}`);
                     let res = $export_address.decode(fountainResult);
                     console.log(res);
