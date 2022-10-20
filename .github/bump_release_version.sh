83b0f236666fe30dd768cfbd8fe498d88dc217fe
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-10-20 17:04:49 +0500

    fix version

--- .github/bump_release_version.sh
+++ .github/bump_release_version.sh
@@ -11,27 +11,27 @@ if [ $2 == 'patch' ]
 then
   echo 'Major bumped version is: '${arrIN[0]}
   echo 'Minor bumped version is: '${arrIN[1]}
-  echo 'Patch bumped version is: '${arrIN[2]}+1
+  echo 'Patch bumped version is: '$((${arrIN[2]}+1))
 
   echo "{major}=${arrIN[0]}" >> $GITHUB_OUTPUT
   echo "{minor}=${arrIN[1]}" >> $GITHUB_OUTPUT
-  echo "{patch}=${arrIN[2]}+1" >> $GITHUB_OUTPUT
+  echo "{patch}=$((${arrIN[2]}+1))" >> $GITHUB_OUTPUT
 elif [ $2 == 'minor' ]
 then
   echo 'Major bumped version is: '${arrIN[0]}
-  echo 'Minor bumped version is: '${arrIN[1]}+1
+  echo 'Minor bumped version is: '$((${arrIN[1]}+1))
   echo 'Patch bumped version is: '0
 
   echo "{major}=${arrIN[0]}" >> $GITHUB_OUTPUT
-  echo "{minor}=${arrIN[1]}+1" >> $GITHUB_OUTPUT
+  echo "{minor}=$((${arrIN[1]}+1))" >> $GITHUB_OUTPUT
   echo "{patch}=0" >> $GITHUB_OUTPUT
 elif [ $2 == 'major' ]
 then
-  echo 'Major bumped version is: '${arrIN[0]}+1
+  echo 'Major bumped version is: '$((${arrIN[0]}+1))
   echo 'Minor bumped version is: '0
   echo 'Patch bumped version is: '0
 
-  echo "{major}=${arrIN[0]}+1" >> $GITHUB_OUTPUT
+  echo "{major}=$((${arrIN[0]}+1))" >> $GITHUB_OUTPUT
   echo "{minor}=0" >> $GITHUB_OUTPUT
   echo "{patch}=0" >> $GITHUB_OUTPUT
 else
