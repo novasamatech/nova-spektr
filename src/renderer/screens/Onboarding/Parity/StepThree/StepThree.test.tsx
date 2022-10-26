9bb429e50081c56c5f0e93fe3e8237264ec60f18
asmadek <alma2610@ya.ru>
2022-10-26 17:54:22 +0500

    fix: update tests

--- src/renderer/screens/Onboarding/Parity/StepThree/StepThree.test.tsx
+++ src/renderer/screens/Onboarding/Parity/StepThree/StepThree.test.tsx
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import { SeedInfo } from '@renderer/components/common/QrCode/QrReader/common/types';
 import { CryptoTypeString } from '@renderer/domain/shared-kernel';
 import StepThree from './StepThree';
+import { Chain } from '@renderer/domain/chain';
 
 jest.mock('@renderer/context/I18nContext', () => ({
   useI18n: jest.fn().mockReturnValue({
@@ -10,11 +11,25 @@ jest.mock('@renderer/context/I18nContext', () => ({
   }),
 }));
 
+jest.mock('@renderer/services/wallet/walletService', () => ({
+  useWallet: jest.fn().mockReturnValue({
+    addWallet: jest.fn(),
+    setActiveWallet: jest.fn(),
+  }),
+}));
+
+jest.mock('@renderer/services/network/chainsService', () => ({
+  useChains: jest.fn().mockReturnValue({
+    getChainsData: jest.fn().mockReturnValue([]),
+    sortChains: (value: Chain[]) => value,
+  }),
+}));
+
 describe('screens/Onboard/Parity/StepThree', () => {
   test('should render component', async () => {
     const data: SeedInfo = {
       name: 'test wallet',
-      multiSigner: { MultiSigner: CryptoTypeString.SR25519, public: new Uint8Array([1, 2, 3]) },
+      multiSigner: { MultiSigner: CryptoTypeString.SR25519, public: new Uint8Array([0]) },
       derivedKeys: [],
     };
 
