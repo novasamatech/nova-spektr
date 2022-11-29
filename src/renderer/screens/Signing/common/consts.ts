449eb218195c63d312fea82afcdbeecd210ad6b6
Pavel <pavel@novawallet.io>
2022-11-29 14:00:22 +0300

    Changed wiki link for PS troubleshooter
    * Changed wiki link for PS troubleshooter

--- src/renderer/screens/Signing/common/consts.ts
+++ src/renderer/screens/Signing/common/consts.ts
@@ -1,6 +1,7 @@
 import { ChainId } from '@renderer/domain/shared-kernel';
 
 const METADATA_PORTAL_URL = 'https://nova-wallet.github.io/metadata-portal/#/';
-export const TROUBLESHOOTING_URL = 'https://github.com/nova-wallet/nova-utils/wiki/Parity-Signer-troubleshooting';
+export const TROUBLESHOOTING_URL =
+  'https://docs.novawallet.io/nova-wallet-wiki/help-and-support/troubleshooting#parity-signer-troubleshooting';
 
 export const getMetadataPortalUrl = (chainId: ChainId) => `${METADATA_PORTAL_URL}${chainId}`;
