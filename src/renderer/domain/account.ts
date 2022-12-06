540ee5c4784556680331dce23ffd79dda3b63e14
asmadek <alma2610@ya.ru>
2022-12-06 17:40:38 +0500

    fix: make publicKey and cryptoType optional

--- src/renderer/domain/account.ts
+++ src/renderer/domain/account.ts
@@ -6,10 +6,10 @@ export type Account = {
   walletId: IndexableType;
   rootId?: IndexableType;
   name?: string;
-  publicKey: PublicKey;
+  publicKey?: PublicKey;
   accountId: string;
   signingType: SigningType;
-  cryptoType: CryptoType;
+  cryptoType?: CryptoType;
   chainClass: ChainClass;
   chainId?: ChainId;
   // TODO: rename this to something as replacer for root account
