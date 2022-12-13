8d45948ed6243739ad8069c96388538ee851ebea
Pavel <pavel@novawallet.io>
2022-12-13 09:25:36 +0300

    Update metadata portal link

--- src/renderer/screens/Signing/common/consts.ts
+++ src/renderer/screens/Signing/common/consts.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '@renderer/domain/shared-kernel';
 
-const METADATA_PORTAL_URL = 'https://nova-wallet.github.io/metadata-portal/#/';
+const METADATA_PORTAL_URL = 'https://metadata.novasama.io/#/';
 export const TROUBLESHOOTING_URL =
   'https://docs.novawallet.io/nova-wallet-wiki/help-and-support/troubleshooting#parity-signer-troubleshooting';
 
