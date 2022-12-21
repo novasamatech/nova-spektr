03a70ce42e5f4374b65ffe97063543bd224792dd
Aleksandr Makhnev <alma2610@ya.ru>
2022-12-21 20:50:41 +0500

    fix: wallets list fixes (#155)
    * fix: scan qr code
    * fix: wallet list amount
    * fix: wallet list search

--- src/renderer/components/layout/PrimaryLayout/Wallets/types.ts
+++ src/renderer/components/layout/PrimaryLayout/Wallets/types.ts
@@ -3,5 +3,5 @@ import { Wallet } from '@renderer/domain/wallet';
 import { AccountDS } from '@renderer/services/storage';
 
 export type ChainWithAccounts = Chain & { isActive: boolean; accounts: AccountDS[] };
-export type RootAccount = AccountDS & { chains: ChainWithAccounts[] };
-export type WalletStructure = Wallet & { rootAccounts: RootAccount[] };
+export type RootAccount = AccountDS & { chains: ChainWithAccounts[]; amount: number };
+export type WalletStructure = Wallet & { rootAccounts: RootAccount[]; amount: number };
