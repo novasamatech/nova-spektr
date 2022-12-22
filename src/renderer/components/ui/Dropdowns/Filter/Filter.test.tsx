c150890aaf7ad0bdbe6542823b76a1a443474113
Yaroslav Grachev <yaroslav@novawallet.io>
2022-12-22 21:09:54 +0300

    feat: filter tests

--- src/renderer/components/ui/Dropdowns/Filter/Filter.test.tsx
+++ src/renderer/components/ui/Dropdowns/Filter/Filter.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 
 import Filter from './Filter';
 
@@ -25,41 +25,29 @@ describe('ui/Dropdowns/Filter', () => {
     expect(placeholder).toBeInTheDocument();
   });
 
-  // test('should call onChange', async () => {
-  //   const spyChange = jest.fn();
-  //   render(<Filter {...defaultProps} onChange={spyChange} />);
-  //
-  //   const button = screen.getByRole('button');
-  //   await act(() => button.click());
-  //
-  //   const optionOne = screen.getByRole('option', { name: options[0].element });
-  //   await act(() => optionOne.click());
-  //   expect(spyChange).toBeCalledWith([{ id: options[0].id, value: options[0].value }]);
-  //
-  //   const optionTwo = screen.getByRole('option', { name: options[1].element });
-  //   await act(() => optionTwo.click());
-  //   expect(spyChange).toBeCalledWith([{ id: options[1].id, value: options[1].value }]);
-  // });
-  //
-  // test('should render summary options', () => {
-  //   render(<Filter {...defaultProps} activeIds={[options[1].id]} />);
-  //
-  //   const summary = screen.getByRole('button');
-  //   expect(summary).toHaveTextContent(/1Total/);
-  // });
-  //
-  // test('should render selected options', async () => {
-  //   const { rerender } = render(<Filter {...defaultProps} />);
-  //
-  //   const button = screen.getByRole('button');
-  //   await act(() => button.click());
-  //
-  //   let selectedOption = screen.queryByRole('checkbox', { checked: true });
-  //   expect(selectedOption).not.toBeInTheDocument();
-  //
-  //   rerender(<Filter {...defaultProps} activeIds={[options[1].id]} />);
-  //
-  //   selectedOption = screen.getByRole('checkbox', { checked: true });
-  //   expect(selectedOption).toBeInTheDocument();
-  // });
+  test('should call onChange', async () => {
+    const spyChange = jest.fn();
+    render(<Filter {...defaultProps} onChange={spyChange} />);
+
+    const button = screen.getByRole('button');
+    await act(() => button.click());
+
+    const optionOne = screen.getByRole('option', { name: options[0].element });
+    await act(() => optionOne.click());
+    expect(spyChange).toBeCalledWith([{ id: options[0].id, value: options[0].value }]);
+
+    const optionTwo = screen.getByRole('option', { name: options[1].element });
+    await act(() => optionTwo.click());
+    expect(spyChange).toBeCalledWith([{ id: options[1].id, value: options[1].value }]);
+  });
+
+  test('should render list', async () => {
+    render(<Filter {...defaultProps} />);
+
+    const button = screen.getByRole('button');
+    await act(() => button.click());
+
+    const selectedOptions = screen.getAllByRole('option');
+    expect(selectedOptions).toHaveLength(3);
+  });
 });
