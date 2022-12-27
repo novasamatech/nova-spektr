b9479506d181f91bb52167871dbb13200a2cdd8f
asmadek <alma2610@ya.ru>
2022-12-27 18:12:02 +0500

    feat: use multi for balances

--- src/renderer/services/balance/balanceService.ts
+++ src/renderer/services/balance/balanceService.ts
@@ -71,8 +71,8 @@ export const useBalance = (): IBalanceService => {
     }
   };
 
-  const subscribeBalanceChange = (
-    publicKey: PublicKey,
+  const subscribeBalancesChange = (
+    publicKeys: PublicKey[],
     chain: ExtendedChain,
     relaychain: ExtendedChain | undefined,
     asset: Asset,
@@ -80,40 +80,42 @@ export const useBalance = (): IBalanceService => {
     const api = chain.api;
     if (!api) return;
 
-    const address = toAddress(publicKey, chain.addressPrefix);
+    const addresses = publicKeys.map((pk) => toAddress(pk, chain.addressPrefix));
 
-    return api.query.system.account(address, (data: AccountInfo) => {
-      const miscFrozen = new BN(data.data.miscFrozen);
-      const feeFrozen = new BN(data.data.feeFrozen);
+    return api.query.system.account.multi(addresses, (data: AccountInfo[]) => {
+      data.forEach((accountInfo, i) => {
+        const miscFrozen = new BN(accountInfo.data.miscFrozen);
+        const feeFrozen = new BN(accountInfo.data.feeFrozen);
 
-      const balance = {
-        publicKey,
-        chainId: chain.chainId,
-        assetId: asset.assetId.toString(),
-        verified: true,
-        free: data.data.free.toString(),
-        frozen: miscFrozen.gt(feeFrozen) ? miscFrozen.toString() : feeFrozen.toString(),
-        reserved: data.data.reserved.toString(),
-      };
+        const balance = {
+          publicKey: publicKeys[i],
+          chainId: chain.chainId,
+          assetId: asset.assetId.toString(),
+          verified: true,
+          free: accountInfo.data.free.toString(),
+          frozen: miscFrozen.gt(feeFrozen) ? miscFrozen.toString() : feeFrozen.toString(),
+          reserved: accountInfo.data.reserved.toString(),
+        };
 
-      updateBalance(balance);
+        updateBalance(balance);
 
-      if (relaychain?.api && isLightClient(relaychain)) {
-        const storageKey = api.query.system.account.key(address);
-        runValidation(
-          relaychain.api,
-          api,
-          storageKey,
-          data,
-          () => handleValidation(balance, true),
-          () => handleValidation(balance, false),
-        );
-      }
+        if (relaychain?.api && isLightClient(relaychain)) {
+          const storageKey = api.query.system.account.key(addresses[i]);
+          runValidation(
+            relaychain.api,
+            api,
+            storageKey,
+            accountInfo,
+            () => handleValidation(balance, true),
+            () => handleValidation(balance, false),
+          );
+        }
+      });
     });
   };
 
-  const subscribeStatemineAssetChange = (
-    publicKey: PublicKey,
+  const subscribeStatemineAssetsChange = (
+    publicKeys: PublicKey[],
     chain: ExtendedChain,
     relaychain: ExtendedChain | undefined,
     asset: Asset,
@@ -122,42 +124,47 @@ export const useBalance = (): IBalanceService => {
     const api = chain.api;
     if (!api) return;
 
-    const address = toAddress(publicKey, chain.addressPrefix);
+    const addresses = publicKeys.map((pk) => toAddress(pk, chain.addressPrefix));
 
-    return api.query.assets.account(statemineAssetId, address, (data: any) => {
-      try {
-        const free = data.isNone ? '0' : data.unwrap().balance.toString();
-        const balance = {
-          publicKey,
-          chainId: chain.chainId,
-          assetId: asset.assetId.toString(),
-          verified: true,
-          free: free.toString(),
-          frozen: (0).toString(),
-          reserved: (0).toString(),
-        };
+    return api.query.assets.account.multi(
+      addresses.map((a) => [statemineAssetId, a]),
+      (data: any[]) => {
+        data.forEach((accountInfo, i) => {
+          try {
+            const free = accountInfo.isNone ? '0' : accountInfo.unwrap().balance.toString();
+            const balance = {
+              publicKey: publicKeys[i],
+              chainId: chain.chainId,
+              assetId: asset.assetId.toString(),
+              verified: true,
+              free: free.toString(),
+              frozen: (0).toString(),
+              reserved: (0).toString(),
+            };
 
-        updateBalance(balance);
+            updateBalance(balance);
 
-        if (relaychain?.api && isLightClient(relaychain)) {
-          const storageKey = api.query.assets.account.key(statemineAssetId, address);
-          runValidation(
-            relaychain.api,
-            api,
-            storageKey,
-            data,
-            () => handleValidation(balance, true),
-            () => handleValidation(balance, false),
-          );
-        }
-      } catch (e) {
-        console.warn(e);
-      }
-    });
+            if (relaychain?.api && isLightClient(relaychain)) {
+              const storageKey = api.query.assets.account.key(statemineAssetId, addresses[i]);
+              runValidation(
+                relaychain.api,
+                api,
+                storageKey,
+                accountInfo,
+                () => handleValidation(balance, true),
+                () => handleValidation(balance, false),
+              );
+            }
+          } catch (e) {
+            console.warn(e);
+          }
+        });
+      },
+    );
   };
 
-  const subscribeOrmlAssetChange = async (
-    publicKey: PublicKey,
+  const subscribeOrmlAssetsChange = async (
+    publicKeys: PublicKey[],
     chain: ExtendedChain,
     relaychain: ExtendedChain | undefined,
     asset: Asset,
@@ -166,85 +173,97 @@ export const useBalance = (): IBalanceService => {
     const api = chain.api;
     if (!api) return;
 
-    const address = toAddress(publicKey, chain.addressPrefix);
+    const addresses = publicKeys.map((pk) => toAddress(pk, chain.addressPrefix));
 
     const method = api.query.tokens ? api.query.tokens.accounts : api.query.currencies.accounts;
 
-    return method(address, ormlAssetId, (data: any) => {
-      const { free, reserved, frozen } = data;
-      const balance = {
-        publicKey,
-        chainId: chain.chainId,
-        assetId: asset.assetId.toString(),
-        verified: true,
-        free: free.toString(),
-        frozen: frozen.toString(),
-        reserved: reserved.toString(),
-      };
+    return method.multi(
+      addresses.map((a) => [a, ormlAssetId]),
+      (data: any[]) => {
+        data.forEach((accountInfo, i) => {
+          const { free, reserved, frozen } = accountInfo;
+          const balance = {
+            publicKey: publicKeys[i],
+            chainId: chain.chainId,
+            assetId: asset.assetId.toString(),
+            verified: true,
+            free: free.toString(),
+            frozen: frozen.toString(),
+            reserved: reserved.toString(),
+          };
 
-      updateBalance(balance);
+          updateBalance(balance);
 
-      if (relaychain?.api && isLightClient(relaychain)) {
-        const storageKey = method.key(address, ormlAssetId);
-        runValidation(
-          relaychain.api,
-          api,
-          storageKey,
-          data,
-          () => handleValidation(balance, true),
-          () => handleValidation(balance, false),
-        );
-      }
-    });
+          if (relaychain?.api && isLightClient(relaychain)) {
+            const storageKey = method.key(addresses[i], ormlAssetId);
+            runValidation(
+              relaychain.api,
+              api,
+              storageKey,
+              accountInfo,
+              () => handleValidation(balance, true),
+              () => handleValidation(balance, false),
+            );
+          }
+        });
+      },
+    );
   };
 
-  const subscribeLockBalanceChange = (publicKey: PublicKey, chain: ExtendedChain, asset: Asset) => {
+  const subscribeLockBalanceChange = (publicKeys: PublicKey[], chain: ExtendedChain, asset: Asset) => {
     const api = chain.api;
     if (!api) return;
 
-    const address = toAddress(publicKey, chain.addressPrefix);
+    const addresses = publicKeys.map((pk) => toAddress(pk, chain.addressPrefix));
 
-    return api.query.balances.locks(address, (data: BalanceLock[]) => {
-      const balance = {
-        publicKey,
-        chainId: chain.chainId,
-        assetId: asset.assetId.toString(),
-        locked: [
-          ...data.map((lock: BalanceLock) => ({
-            type: lock.id.toString(),
-            amount: lock.amount.toString(),
-          })),
-        ],
-      };
+    return api.query.balances.locks.multi(addresses, (balanceLocks: any[]) => {
+      balanceLocks.forEach((balanceLock, i) => {
+        const balance = {
+          publicKey: publicKeys[i],
+          chainId: chain.chainId,
+          assetId: asset.assetId.toString(),
+          locked: [
+            ...balanceLock.map((lock: BalanceLock) => ({
+              type: lock.id.toString(),
+              amount: lock.amount.toString(),
+            })),
+          ],
+        };
 
-      updateBalance(balance);
+        updateBalance(balance);
+      });
     });
   };
 
-  const subscribeLockOrmlAssetChange = async (publicKey: PublicKey, chain: ExtendedChain, asset: Asset) => {
+  const subscribeLockOrmlAssetChange = async (publicKeys: PublicKey[], chain: ExtendedChain, asset: Asset) => {
     const ormlAssetId = (asset?.typeExtras as OrmlExtras).currencyIdScale;
     const api = chain.api;
     if (!api) return;
 
-    const address = toAddress(publicKey, chain.addressPrefix);
+    const addresses = publicKeys.map((pk) => toAddress(pk, chain.addressPrefix));
 
     const method = api.query.tokens ? api.query.tokens.locks : api.query.currencies.locks;
 
-    return method(address, ormlAssetId, (data: BalanceLock[]) => {
-      const balance = {
-        publicKey,
-        chainId: chain.chainId,
-        assetId: asset.assetId.toString(),
-        locked: [
-          ...data.map((lock: BalanceLock) => ({
-            type: lock.id.toString(),
-            amount: lock.amount.toString(),
-          })),
-        ],
-      };
+    return method.multi(
+      addresses.map((a) => [a, ormlAssetId]),
+      (balanceLocks: any[]) => {
+        balanceLocks.forEach((balanceLock, i) => {
+          const balance = {
+            publicKey: publicKeys[i],
+            chainId: chain.chainId,
+            assetId: asset.assetId.toString(),
+            locked: [
+              ...balanceLock.map((lock: BalanceLock) => ({
+                type: lock.id.toString(),
+                amount: lock.amount.toString(),
+              })),
+            ],
+          };
 
-      updateBalance(balance);
-    });
+          updateBalance(balance);
+        });
+      },
+    );
   };
 
   const subscribeBalances = (
@@ -252,37 +271,33 @@ export const useBalance = (): IBalanceService => {
     relaychain: ExtendedChain | undefined,
     publicKeys: PublicKey[],
   ): Promise<any> => {
-    const unsubscribe = publicKeys.map((publicKey) =>
-      chain.assets?.map((asset) => {
-        if (!asset.type) {
-          return subscribeBalanceChange(publicKey, chain, relaychain, asset);
-        }
+    const unsubscribe = chain.assets?.map((asset) => {
+      if (!asset.type) {
+        return subscribeBalancesChange(publicKeys, chain, relaychain, asset);
+      }
 
-        if (asset.type === AssetType.STATEMINE) {
-          return subscribeStatemineAssetChange(publicKey, chain, relaychain, asset);
-        }
+      if (asset.type === AssetType.STATEMINE) {
+        return subscribeStatemineAssetsChange(publicKeys, chain, relaychain, asset);
+      }
 
-        if (asset.type === AssetType.ORML) {
-          return subscribeOrmlAssetChange(publicKey, chain, relaychain, asset);
-        }
-      }),
-    );
+      if (asset.type === AssetType.ORML) {
+        return subscribeOrmlAssetsChange(publicKeys, chain, relaychain, asset);
+      }
+    });
 
     return Promise.all(unsubscribe.flat());
   };
 
   const subscribeLockBalances = (chain: ExtendedChain, publicKeys: PublicKey[]): Promise<any> => {
-    const unsubscribe = publicKeys.map((publicKey) =>
-      chain.assets?.map((asset) => {
-        if (!asset.type) {
-          return subscribeLockBalanceChange(publicKey, chain, asset);
-        }
+    const unsubscribe = chain.assets?.map((asset) => {
+      if (!asset.type) {
+        return subscribeLockBalanceChange(publicKeys, chain, asset);
+      }
 
-        if (asset.type === AssetType.ORML) {
-          return subscribeLockOrmlAssetChange(publicKey, chain, asset);
-        }
-      }),
-    );
+      if (asset.type === AssetType.ORML) {
+        return subscribeLockOrmlAssetChange(publicKeys, chain, asset);
+      }
+    });
 
     return Promise.all(unsubscribe.flat());
   };
