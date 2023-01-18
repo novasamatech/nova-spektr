b14bc8814fbcf60299dfd98ff7153b89d2b9c1bc
Yaroslav Grachev <tuulwq@gmail.com>
2023-01-18 15:21:43 +0300

    Feat: Get oversubscribed info for validators

--- src/renderer/services/staking/stakingDataService.ts
+++ src/renderer/services/staking/stakingDataService.ts
@@ -1,39 +1,9 @@
 import { ApiPromise } from '@polkadot/api';
-import { u8aToString } from '@polkadot/util';
-import { useState } from 'react';
 
 import { AccountID, ChainId } from '@renderer/domain/shared-kernel';
-import { getValidatorsApy } from '@renderer/services/staking/apyCalculator';
-import {
-  Identity,
-  IStakingDataService,
-  Staking,
-  SubIdentity,
-  Validator,
-  ValidatorMap,
-  StakingMap,
-} from './common/types';
+import { IStakingDataService, Staking, StakingMap } from './common/types';
 
-// TODO: divide into Validator service & Staking service (maybe even Era service)
 export const useStakingData = (): IStakingDataService => {
-  const [validators, setValidators] = useState<ValidatorMap>({});
-
-  const subscribeActiveEra = (
-    chainId: ChainId,
-    api: ApiPromise,
-    callback: (era?: number) => void,
-  ): Promise<() => void> => {
-    return api.query.staking.activeEra((data: any) => {
-      try {
-        const unwrappedData = data.unwrap();
-        callback(unwrappedData.get('index').toNumber());
-      } catch (error) {
-        console.warn(error);
-        callback(undefined);
-      }
-    });
-  };
-
   const subscribeStaking = async (
     chainId: ChainId,
     api: ApiPromise,
@@ -103,167 +73,6 @@ export const useStakingData = (): IStakingDataService => {
     });
   };
 
-  const getValidators = async (chainId: ChainId, api: ApiPromise, era: number): Promise<void> => {
-    const [totalStake, commission] = await Promise.all([setValidatorsStake(api, era), setValidatorsPrefs(api, era)]);
-
-    const { addresses, apyPayload } = Object.entries(totalStake).reduce<Record<string, any[]>>(
-      (acc, [address, totalStake]) => {
-        acc.addresses.push(address);
-        acc.apyPayload.push({ address, totalStake, commission: commission[address] });
-
-        return acc;
-      },
-      { addresses: [], apyPayload: [] },
-    );
-
-    await Promise.all([setIdentities(api, addresses), calculateValidatorsApy(api, apyPayload)]);
-  };
-
-  const setValidatorsStake = async (api: ApiPromise, era: number): Promise<Record<AccountID, string>> => {
-    const data = await api.query.staking.erasStakersClipped.entries(era);
-
-    const { full, short } = data.reduce(
-      (acc, [storageKey, type]) => {
-        const address = storageKey.args[1].toString();
-        const totalStake = type.total.toString();
-        const payload = { totalStake, address, ownStake: type.own.toString() };
-
-        acc.full = { ...acc.full, [address]: payload };
-        acc.short = { ...acc.short, [address]: totalStake };
-
-        return acc;
-      },
-      { full: {}, short: {} },
-    );
-
-    setValidators((prev) =>
-      Object.entries(full).reduce((acc, [address, prefs]) => {
-        const payload = { ...prev[address], ...(prefs as ValidatorMap) };
-
-        return { ...acc, [address]: payload };
-      }, {}),
-    );
-
-    return short;
-  };
-
-  const setValidatorsPrefs = async (api: ApiPromise, era: number): Promise<Record<AccountID, number>> => {
-    const data = await api.query.staking.erasValidatorPrefs.entries(era);
-
-    const { full, short } = data.reduce(
-      (acc, [storageKey, type]) => {
-        const address = storageKey.args[1].toString();
-        const commission = parseFloat(type.commission.toHuman() as string);
-        const payload = { address, commission, blocked: type.blocked.toHuman() };
-
-        acc.full = { ...acc.full, [address]: payload };
-        acc.short = { ...acc.short, [address]: commission };
-
-        return acc;
-      },
-      { full: {}, short: {} },
-    );
-
-    setValidators((prev) =>
-      Object.entries(full).reduce((acc, [address, prefs]) => {
-        const payload = { ...prev[address], ...(prefs as ValidatorMap) };
-
-        return { ...acc, [address]: payload };
-      }, {}),
-    );
-
-    return short;
-  };
-
-  const getMaxValidators = (api: ApiPromise): number => {
-    return api.consts.staking.maxNominations.toNumber();
-  };
-
-  const setIdentities = async (api: ApiPromise, addresses: AccountID[]): Promise<void> => {
-    const subIdentities = await getSubIdentities(api, addresses);
-    const parentIdentities = await getParentIdentities(api, subIdentities);
-
-    setValidators((prev) =>
-      Object.entries(prev).reduce((acc, [address, validator]) => {
-        const payload = { ...validator, identity: parentIdentities[address] };
-
-        return { ...acc, [address]: payload };
-      }, {}),
-    );
-  };
-
-  const getSubIdentities = async (api: ApiPromise, addresses: AccountID[]): Promise<SubIdentity[]> => {
-    const subIdentities = await api.query.identity.superOf.multi(addresses);
-
-    return subIdentities.reduce<SubIdentity[]>((acc, identity, index) => {
-      const payload = { sub: addresses[index], parent: addresses[index], subName: '' };
-      if (!identity.isNone) {
-        const [address, rawData] = identity.unwrap();
-        payload.parent = address.toHuman();
-        payload.subName = rawData.isRaw ? u8aToString(rawData.asRaw) : rawData.value.toString();
-      }
-
-      return acc.concat(payload);
-    }, []);
-  };
-
-  const getParentIdentities = async (
-    api: ApiPromise,
-    subIdentities: SubIdentity[],
-  ): Promise<Record<AccountID, Identity>> => {
-    const identityAddresses = subIdentities.map((identity) => identity.parent);
-
-    const parentIdentities = await api.query.identity.identityOf.multi(identityAddresses);
-
-    return parentIdentities.reduce<Record<AccountID, Identity>>((acc, identity, index) => {
-      if (identity.isNone) return acc;
-
-      const { parent, sub, subName } = subIdentities[index];
-      const { info } = identity.unwrap(); // { judgements, info }
-      const { display, web, riot, email, twitter } = info;
-
-      const payload: Identity = {
-        subName,
-        email: email.isRaw ? u8aToString(email.asRaw) : email.value.toString(),
-        riot: riot.isRaw ? u8aToString(riot.asRaw) : riot.value.toString(),
-        twitter: twitter.isRaw ? u8aToString(twitter.asRaw) : twitter.value.toString(),
-        website: web.isRaw ? u8aToString(web.asRaw) : web.value.toString(),
-        parent: {
-          address: parent,
-          name: display.isRaw ? u8aToString(display.asRaw) : display.value.toString(),
-        },
-      };
-
-      return { ...acc, [sub]: payload };
-    }, {});
-  };
-
-  const calculateValidatorsApy = async (api: ApiPromise, validators: Validator[]) => {
-    const apys = await getValidatorsApy(api, validators);
-
-    setValidators((prev) =>
-      Object.entries(apys).reduce((acc, [address, apy]) => {
-        const payload = { ...prev[address], apy };
-
-        return { ...acc, [address]: payload };
-      }, {}),
-    );
-  };
-
-  const getNominators = async (api: ApiPromise, account: AccountID): Promise<string[]> => {
-    try {
-      const data = await api?.query.staking.nominators(account);
-      if (data.isNone) return [];
-      const unwrappedData = data.unwrap();
-
-      return unwrappedData.targets.toArray().map((nominator) => nominator.toString());
-    } catch (error) {
-      console.warn(error);
-
-      return [];
-    }
-  };
-
   const getMinNominatorBond = async (api: ApiPromise): Promise<string> => {
     try {
       return (await api.query.staking.minNominatorBond()).toString();
@@ -275,12 +84,7 @@ export const useStakingData = (): IStakingDataService => {
   };
 
   return {
-    validators,
-    subscribeActiveEra,
     subscribeStaking,
-    getValidators,
-    getMaxValidators,
-    getNominators,
     getMinNominatorBond,
   };
 };
