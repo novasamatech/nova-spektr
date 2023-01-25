a571f6d58af2bc00393f049d74891710786a2af2
Yaroslav Grachev <tuulwq@gmail.com>
2023-01-25 12:03:52 +0300

    Feat: Table component | Staking sorting (#187)

---
+++ src/renderer/components/ui/Table/Table.tsx
@@ -0,0 +1,132 @@
+import cn from 'classnames';
+import { PropsWithChildren, useCallback, useMemo, useState } from 'react';
+
+import { SortConfig, SortType, AnyRecord, IndexKey, ColumnConfig } from './common/types';
+import { getUpdatedConfig, getSortedData } from './common/utils';
+import { TableBody, TableCell, TableColumn, TableHeader, TableRow } from './TableParts';
+import { TableContext } from './TableContext';
+
+type Props<T extends AnyRecord> = {
+  by: IndexKey;
+  dataSource: T[];
+  className?: string;
+  selectedKeys?: IndexKey[];
+  onSelect?: (keys: IndexKey[]) => void;
+};
+
+// TODO: think about nice TS support
+// type TableComposition = {
+//   Header: Parameters<typeof TableHeader>;
+//   Column: Parameters<typeof TableColumn>;
+//   Body: Parameters<typeof TableBody>;
+//   Row: Parameters<typeof TableRow>;
+//   Cell: Parameters<typeof TableCell>;
+// };
+
+const Table = <T extends AnyRecord>({
+  by,
+  dataSource,
+  className,
+  selectedKeys,
+  onSelect,
+  children,
+}: PropsWithChildren<Props<T>>) => {
+  const [sortConfig, setSortConfig] = useState<SortConfig>({});
+  const [excludedKeys, setExcludedKeys] = useState<IndexKey[]>([]);
+
+  const allRowsSelected = dataSource.length - excludedKeys.length === selectedKeys?.length;
+
+  const addSortingConfig = useCallback(
+    ({ dataKey, align, sortable, sortType }: ColumnConfig) => {
+      const payload = {
+        dataKey,
+        align,
+        sortable,
+        sortType: sortType || SortType.NONE,
+      };
+
+      setSortConfig((prev) => ({ ...prev, [dataKey]: payload }));
+    },
+    [setSortConfig],
+  );
+
+  const updateSortingOrder = useCallback(
+    (column: string) => {
+      if (sortConfig[column]) {
+        setSortConfig((prev) => getUpdatedConfig(column, prev));
+      } else {
+        console.warn(`${column} is absent`);
+      }
+    },
+    [sortConfig, setSortConfig],
+  );
+
+  const excludeKey = useCallback(
+    (key: IndexKey) => {
+      setExcludedKeys((prev) => prev.concat(key));
+    },
+    [setExcludedKeys],
+  );
+
+  const selectAll = useCallback(() => {
+    if (!selectedKeys || !onSelect) return;
+
+    if (allRowsSelected) {
+      onSelect?.([]);
+    } else {
+      const allSelectedKeys = dataSource.reduce<IndexKey[]>((acc, source) => {
+        if (!excludedKeys.includes(source[by])) {
+          acc.push(source[by]);
+        }
+
+        return acc;
+      }, []);
+
+      onSelect(allSelectedKeys);
+    }
+  }, [dataSource, excludedKeys, allRowsSelected, onSelect]);
+
+  const selectRow = useCallback(
+    (key: IndexKey) => {
+      if (!selectedKeys || !onSelect) return;
+
+      if (selectedKeys.includes(key)) {
+        onSelect(selectedKeys.filter((k) => k !== key));
+      } else {
+        onSelect(selectedKeys.concat(key));
+      }
+    },
+    [selectedKeys, onSelect],
+  );
+
+  const sortedData = useMemo(() => {
+    return getSortedData(dataSource, sortConfig);
+  }, [dataSource, sortConfig]);
+
+  const value = {
+    by,
+    dataSource: sortedData,
+    sortConfig,
+    selectedKeys,
+    allRowsSelected,
+    addSortingConfig,
+    updateSortingOrder,
+    excludeKey,
+    selectAll,
+    selectRow,
+  };
+
+  return (
+    <TableContext.Provider value={value}>
+      <table className={cn('w-full bg-white rounded-2lg table-auto shadow-surface', className)}>{children}</table>
+    </TableContext.Provider>
+  );
+};
+
+Table.Header = TableHeader;
+Table.Column = TableColumn;
+Table.Body = TableBody;
+Table.Cell = TableCell;
+Table.Row = TableRow;
+
+export default Table;
