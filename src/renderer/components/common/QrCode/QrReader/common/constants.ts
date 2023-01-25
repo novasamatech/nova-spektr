e9066580a1de21cb94b8aa758b8173bb84662f2f
Aleksandr Makhnev <alma2610@ya.ru>
2023-01-25 17:43:19 +0500

    feat: multiple transactions signing
    * feat: multiple transactions signing
    * feat: show wallets inside pages

--- src/renderer/components/common/QrCode/QrReader/common/constants.ts
+++ src/renderer/components/common/QrCode/QrReader/common/constants.ts
@@ -1,4 +1,4 @@
-import { array, Codec, object, option, sizedUint8Array, str, taggedUnion, u8 } from 'parity-scale-codec';
+import { array, Codec, object, option, sizedUint8Array, str, taggedUnion, u8, uint8Array } from 'parity-scale-codec';
 
 import { CryptoType, CryptoTypeString } from '@renderer/domain/shared-kernel';
 import { AddressInfo, SeedInfo } from './types';
@@ -16,6 +16,12 @@ const MULTI_SIGNER = taggedUnion('MultiSigner', [
   [CryptoTypeString.ECDSA, ['public', sizedUint8Array(33)]],
 ]);
 
+const MULTI_SIGNATURE = taggedUnion('MultiSignature', [
+  [CryptoTypeString.ED25519, ['signature', sizedUint8Array(64)]],
+  [CryptoTypeString.SR25519, ['signature', sizedUint8Array(64)]],
+  [CryptoTypeString.ECDSA, ['signature', sizedUint8Array(65)]],
+]);
+
 const ADDRESS_INFO: Codec<AddressInfo> = object(
   ['address', str],
   ['derivationPath', option(str)],
@@ -31,3 +37,7 @@ const SEED_INFO: Codec<SeedInfo> = object(
 
 // Export address format for decoding; Rust enum is a tagged union
 export const EXPORT_ADDRESS = taggedUnion('ExportAddrs', [['V1', ['payload', array(SEED_INFO)]]]);
+export const TRANSACTION_BULK = taggedUnion('TransactionBulk', [['V1', ['payload', array(uint8Array)]]]);
+export const SIGNED_TRANSACTION_BULK = taggedUnion('SignaturesBulk', {
+  4: ['V1', ['payload', array(MULTI_SIGNATURE)]],
+});
