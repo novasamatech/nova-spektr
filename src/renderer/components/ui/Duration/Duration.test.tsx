516684aa0bbd8da3f0a059c5d8fe8eff058cc367
asmadek <alma2610@ya.ru>
2023-02-03 20:55:16 +0500

    fix: tests

--- src/renderer/components/ui/Duration/Duration.test.tsx
+++ src/renderer/components/ui/Duration/Duration.test.tsx
@@ -2,11 +2,17 @@ import { render, screen } from '@testing-library/react';
 
 import Duration from './Duration';
 
+jest.mock('@renderer/context/I18nContext', () => ({
+  useI18n: jest.fn().mockReturnValue({
+    t: (key: string) => key,
+  }),
+}));
+
 describe('ui/Duration', () => {
   test('should render component', () => {
     render(<Duration seconds={'1'} />);
 
-    const durationValue = screen.getByText('0:01');
+    const durationValue = screen.getByText('time.minutesSeconds');
     expect(durationValue).toBeInTheDocument();
   });
 });
