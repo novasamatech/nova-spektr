3414ea08e6a72188d41936ed5d7d0b90664cd23c
Egor <egor@novawallet.io>
2023-03-28 17:22:39 +0300

    fix: added i18n mock for input test

--- src/renderer/components/ui-redesign/Inputs/PasswordInput/PasswordInput.test.tsx
+++ src/renderer/components/ui-redesign/Inputs/PasswordInput/PasswordInput.test.tsx
@@ -2,6 +2,12 @@ import { render, screen } from '@testing-library/react';
 
 import PasswordInput from './PasswordInput';
 
+jest.mock('@renderer/context/I18nContext', () => ({
+  useI18n: jest.fn().mockReturnValue({
+    t: (key: string) => key,
+  }),
+}));
+
 describe('ui/Inputs/PasswordInput', () => {
   test('should render password visibility button', async () => {
     render(<PasswordInput />);
