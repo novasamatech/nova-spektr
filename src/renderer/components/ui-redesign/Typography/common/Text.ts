d1b79bb6f87923c28003bcdb9cc31eea3a07b163
egor0798 <egorbirukov1234@gmail.com>
2023-03-29 10:10:56 +0300

    feat: input redesign and some typography (#571)

---
+++ src/renderer/components/ui-redesign/Typography/common/Text.ts
@@ -0,0 +1,10 @@
+import { createElement } from 'react';
+import cn from 'classnames';
+
+import { TypographyProps } from '@renderer/components/ui-redesign/Typography/common/types';
+
+const Text = ({ as = 'p', fontWeight = 'regular', align = 'left', className, children }: TypographyProps) => {
+  return createElement(as, { className: cn(`text-${align} font-${fontWeight}`, className) }, children);
+};
+
+export default Text;
