c1a6e03a2f1df65822f6c92f916514a10fa0c082
asmadek <alma2610@ya.ru>
2023-03-31 15:34:27 +0500

    chore: add tests

---
+++ src/renderer/screens/Operations/components/Chain.test.tsx
@@ -0,0 +1,15 @@
+import { act, render, screen } from '@testing-library/react';
+
+import Chain from './Chain';
+import { TEST_CHAIN_ID } from '@renderer/shared/utils/constants';
+
+describe('screen/Operations/components/Chain', () => {
+  test('should render component', async () => {
+    await act(async () => {
+      render(<Chain chainId={TEST_CHAIN_ID} />);
+    });
+
+    const title = screen.getByText('Polkadot');
+    expect(title).toBeInTheDocument();
+  });
+});
