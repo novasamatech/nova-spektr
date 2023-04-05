bcf3befe0143d63dd36c5707c00eb7c63d2f4ad0
Egor <egor@novawallet.io>
2023-04-05 12:11:46 +0300

    feat: added tests

--- src/renderer/components/ui-redesign/Tabs/Tabs.stories.tsx
+++ src/renderer/components/ui-redesign/Tabs/Tabs.stories.tsx
@@ -8,10 +8,10 @@ export default {
   parameters: { actions: { argTypesRegex: '^on.*' } },
 } as ComponentMeta<typeof Tabs>;
 
-const tabItems: TabItem[] = [
-  { id: '1', title: 'Tab 1', panel: <div>tab 1 content</div> },
-  { id: '2', title: 'Tab 2', panel: <div>tab 2 content</div> },
-  { id: '3', title: 'Tab 3', panel: <div>tab 3 content</div> },
+export const tabItems: TabItem[] = [
+  { id: '1', title: 'Tab 1 title', panel: <div>tab 1 content</div> },
+  { id: '2', title: 'Tab 2 title', panel: <div>tab 2 content</div> },
+  { id: '3', title: 'Tab 3 title', panel: <div>tab 3 content</div> },
 ];
 
 const Template: ComponentStory<typeof Tabs> = (args) => <Tabs {...args} />;
