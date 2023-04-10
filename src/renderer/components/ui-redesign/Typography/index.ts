b7bb43e89bb2b7eb4a979eb371c7454f8732c5e8
Egor <egor@novawallet.io>
2023-04-10 18:20:57 +0300

    feat: block explorer link

---
+++ src/renderer/components/ui-redesign/Typography/index.ts
@@ -0,0 +1,6 @@
+import CalloutText from './components/CalloutText';
+import BodyText from './components/BodyText';
+import SmallTitle from './components/SmallTitle';
+import TitleText from './components/TitleText';
+
+export { CalloutText, BodyText, SmallTitle, TitleText };
