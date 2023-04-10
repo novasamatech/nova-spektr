ad4e1265f34d0df84a5a3fa3651840fd4d7e43f8
Egor <egor@novawallet.io>
2023-04-10 03:08:59 +0300

    fix: linter fix

--- src/renderer/components/ui-redesign/Typography/common/TextBase.ts
+++ src/renderer/components/ui-redesign/Typography/common/TextBase.ts
@@ -4,7 +4,11 @@ import cn from 'classnames';
 import { TypographyProps } from '@renderer/components/ui-redesign/Typography/common/types';
 
 const TextBase = ({ as = 'p', fontWeight = 'regular', align = 'left', className, children }: TypographyProps) => {
-  return createElement(as, { className: cn('text-text-primary', `text-${align} font-${fontWeight}`, className) }, children);
+  return createElement(
+    as,
+    { className: cn('text-text-primary', `text-${align} font-${fontWeight}`, className) },
+    children,
+  );
 };
 
 export type TextBaseType = typeof TextBase;
