b47edd18bcff0d2b628b84cb7ef08af5ba59bcad
Egor <egor@novawallet.io>
2023-04-10 18:20:57 +0300

    feat: block explorer link

---
+++ src/renderer/components/ui-redesign/index.ts
@@ -0,0 +1,26 @@
+import Input from './Inputs/Input/Input';
+import PasswordInput from './Inputs/PasswordInput/PasswordInput';
+import InputHint from './InputHint/InputHint';
+import Button from './Buttons/Button/Button';
+import ButtonBack from './Buttons/ButtonBack/ButtonBack';
+import BaseModal from './Modals/BaseModal/BaseModal';
+import InfoPopover from './Popovers/InfoPopover/InfoPopover';
+import Popover from './Popovers/Popover/Popover';
+import InfoLink from './InfoLink/InfoLink';
+import { CalloutText, BodyText, SmallTitle, TitleText } from '@renderer/components/ui-redesign/Typography';
+
+export {
+  Input,
+  PasswordInput,
+  InputHint,
+  Button,
+  ButtonBack,
+  BaseModal,
+  InfoPopover,
+  Popover,
+  InfoLink,
+  CalloutText,
+  BodyText,
+  SmallTitle,
+  TitleText,
+};
