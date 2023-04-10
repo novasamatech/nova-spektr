6cc3b81a978f6726c0504bba93cd802566583317
Egor <egor@novawallet.io>
2023-04-11 02:01:17 +0300

    refactor: moved modal transitions to helper components

--- src/renderer/components/ui-redesign/OperationResult/OperationResult.tsx
+++ src/renderer/components/ui-redesign/OperationResult/OperationResult.tsx
@@ -4,6 +4,7 @@ import { Dialog, Transition } from '@headlessui/react';
 import { Icon } from '@renderer/components/ui';
 import BodyText from '@renderer/components/ui-redesign/Typography/components/BodyText';
 import CalloutText from '@renderer/components/ui-redesign/Typography/components/CalloutText';
+import { ModalBackdrop, ModalTransition } from '@renderer/components/ui-redesign/Modals/common';
 
 type Variant = 'success' | 'error' | 'loading';
 
@@ -31,44 +32,24 @@ const OperationResult = ({
 }: PropsWithChildren<Props>) => (
   <Transition appear show={isOpen} as={Fragment}>
     <Dialog as="div" className="relative z-30" onClose={onClose}>
-      <Transition.Child
-        as={Fragment}
-        enter="ease-out duration-300"
-        enterFrom="opacity-0"
-        enterTo="opacity-100"
-        leave="ease-in duration-200"
-        leaveFrom="opacity-100"
-        leaveTo="opacity-0"
-      >
-        <div className="fixed inset-0 bg-dim-background" />
-      </Transition.Child>
+      <ModalBackdrop />
 
-      <div className="fixed inset-0 overflow-y-auto">
-        <div className="flex min-h-full w-full items-center justify-center text-center">
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            {/* TODO: change bg color */}
-            <Dialog.Panel className="w-[240px] max-w-md transform overflow-hidden flex flex-col items-center justify-center rounded-lg bg-white px-4 py-5 align-middle shadow-card-shadow transition-all">
-              <Icon name={variantIcons[variant]} className="mb-2" />
-              <Dialog.Title className="font-semibold mb-1.5">
-                <BodyText>{title}</BodyText>
-              </Dialog.Title>
-              {description && (
-                <Dialog.Description className="text-text-tertiary">
-                  <CalloutText>{description}</CalloutText>
-                </Dialog.Description>
-              )}
-              {children}
-            </Dialog.Panel>
-          </Transition.Child>
-        </div>
+      <div className="fixed inset-0 overflow-hidden flex min-h-full w-full items-center justify-center text-center">
+        <ModalTransition>
+          {/* TODO: change bg color */}
+          <Dialog.Panel className="w-[240px] max-w-md transform flex flex-col items-center justify-center rounded-lg bg-white px-4 py-5 align-middle shadow-card-shadow transition-all">
+            <Icon name={variantIcons[variant]} className="mb-2" />
+            <Dialog.Title className="font-semibold mb-1.5">
+              <BodyText>{title}</BodyText>
+            </Dialog.Title>
+            {description && (
+              <Dialog.Description className="text-text-tertiary">
+                <CalloutText>{description}</CalloutText>
+              </Dialog.Description>
+            )}
+            {children}
+          </Dialog.Panel>
+        </ModalTransition>
       </div>
     </Dialog>
   </Transition>
