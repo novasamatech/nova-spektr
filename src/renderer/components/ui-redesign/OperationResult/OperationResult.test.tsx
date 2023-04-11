b63959e89b75e098a8f0246ff34f9f5a9d61c232
egor0798 <egorbirukov1234@gmail.com>
2023-04-11 13:04:04 +0300

    Update src/renderer/components/ui-redesign/OperationResult/OperationResult.test.tsx
    Co-authored-by: Yaroslav Grachev <tuulwq@gmail.com>

--- src/renderer/components/ui-redesign/OperationResult/OperationResult.test.tsx
+++ src/renderer/components/ui-redesign/OperationResult/OperationResult.test.tsx
@@ -5,6 +5,15 @@ import OperationResult from './OperationResult';
 describe('ui/OperationResult', () => {
   test('should render component', () => {
     render(
+      <OperationResult title="success" isOpen onClose={noop}>
+          children
+      </OperationResult>,
+    );
+
+    const children = screen.getByText('children');
+    expect(children).toBeInTheDocument();
+  });
+    render(
       <OperationResult title="success" isOpen onClose={() => undefined}>
         <button type="button">ok</button>
       </OperationResult>,
