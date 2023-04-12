99675d29f81cc0b8fc1055d07a3b656bad9c236a
Egor <egor@novawallet.io>
2023-04-12 19:35:20 +0300

    feat: updated button styles, added new fonts

--- src/renderer/components/ui-redesign/Buttons/common/constants.ts
+++ src/renderer/components/ui-redesign/Buttons/common/constants.ts
@@ -3,13 +3,14 @@ import { Pallet, Variant } from './types';
 // TODO add action state styles e.g. active, disabled
 export const ViewClass: Record<`${Variant}_${Pallet}`, string> = {
   text_primary: 'text-action-text-default hover:text-action-text border-transparent bg-transparent', // missing: disabled, active
-  fill_primary: 'text-button-text border-0 bg-button-background-primary', // missing: hover, disabled, active
-  outline_primary: 'text-action-text border border-filter-border', // missing: hover, disabled, active
+  fill_primary:
+    'text-button-text border-0 bg-button-background-primary hover:bg-primary-button-background-hover active:bg-primary-button-background-active disabled:bg-primary-button-background-inactive',
+  text_secondary: '', // idk if its gonna be a thing later, leave it here for now
+  fill_secondary:
+    'text-text-primary bg-action-background-hover hover:bg-secondary-button-background-hover active:bg-secondary-button-background-active disabled:text-button-text-inactive disabled:bg-action-background-hover',
 };
 
-export const WeightClass = {
-  // xs: 'text-xs leading-3.5 font-semibold h-5 px-2 rounded-md',
-  // sm: 'text-xs leading-3.5 font-semibold h-6 px-2 rounded-md',
-  md: 'text-sm leading-3.5 font-semibold h-7.5 px-2 rounded',
-  // lg: 'text-base leading-5 font-semibold h-10 px-3 rounded-2lg',
+export const SizeClass = {
+  big: '',
+  small: '',
 };
