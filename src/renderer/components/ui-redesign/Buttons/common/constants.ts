4fac1eee49ed52a54828e427c74ba353dfb23fc9
Egor <egor@novawallet.io>
2023-04-13 23:06:32 +0300

    chore: updated color name

--- src/renderer/components/ui-redesign/Buttons/common/constants.ts
+++ src/renderer/components/ui-redesign/Buttons/common/constants.ts
@@ -3,7 +3,7 @@ import { Pallet, Variant } from './types';
 // TODO add action state styles e.g. active, disabled
 export const ViewClass: Record<`${Variant}_${Pallet}`, string> = {
   text_primary: 'text-action-text-default hover:text-action-text border-transparent bg-transparent', // missing: disabled, active
-  fill_primary: 'text-button-text border-0 bg-button-background-primary disabled:bg-primary-button-background-inactive',
+  fill_primary: 'text-button-text border-0 bg-primary-button-background-default disabled:bg-primary-button-background-inactive',
   text_secondary: '', // IDK if it's going to be a thing later, leave it here for now
   fill_secondary:
     'text-text-primary bg-action-background-hover hover:bg-secondary-button-background-hover active:bg-secondary-button-background-active disabled:text-button-text-inactive disabled:bg-action-background-hover',
