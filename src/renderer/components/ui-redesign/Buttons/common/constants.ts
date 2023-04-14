d20ed6f51e9cf7aa4faed6551e70ba9d9f6ce244
Egor <egor@novawallet.io>
2023-04-14 16:07:30 +0300

    refactor: changed button size class name

--- src/renderer/components/ui-redesign/Buttons/common/constants.ts
+++ src/renderer/components/ui-redesign/Buttons/common/constants.ts
@@ -11,6 +11,6 @@ export const ViewClass: Record<`${Variant}_${Pallet}`, string> = {
 };
 
 export const SizeClass = {
-  big: 'h-8 rounded-[34px] px-4 pb-2 pt-1.5 text-body',
-  small: 'h-6 rounded-[34px] px-3 py-1 text-footnote',
+  sm: 'h-6 rounded-[34px] px-3 py-1 text-footnote',
+  md: 'h-8 rounded-[34px] px-4 pb-2 pt-1.5 text-body',
 };
