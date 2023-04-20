55f6b994b93fed09eac0afc869ce707d6f5e6f59
Egor <egor@novawallet.io>
2023-04-20 15:16:18 +0300

    fix: option list style fix

--- src/renderer/components/ui-redesign/Dropdowns/common/constants.ts
+++ src/renderer/components/ui-redesign/Dropdowns/common/constants.ts
@@ -6,5 +6,5 @@ export const ViewClass: Record<Exclude<Position, 'auto'>, string> = {
 };
 
 export const OptionsContainerStyle =
-  'mt-1 absolute z-20 py-2 px-1 max-h-60 w-full overflow-auto border border-token-container-border rounded';
+  'mt-1 absolute z-20 py-2 px-1 max-h-60 w-full overflow-auto border border-token-container-border rounded bg-input-background';
 export const OptionStyle = 'py-2 px-3 hover:bg-action-background-hover rounded cursor-pointer';
