2ebb2cabb996523719176cd05b58731703237c35
Egor <egor@novawallet.io>
2023-04-21 16:43:00 +0300

    feat: moved popover link to separate component

--- src/renderer/components/ui-redesign/InfoLink/InfoLink.tsx
+++ src/renderer/components/ui-redesign/InfoLink/InfoLink.tsx
@@ -4,19 +4,13 @@ import cn from 'classnames';
 import { IconNames } from '@renderer/components/ui/Icon/data';
 import { Icon } from '@renderer/components/ui';
 
-interface BaseProps {
-  url?: string;
+type Props = {
+  url: string;
   showIcon?: boolean;
   iconName?: IconNames;
   className?: string;
   fontClass?: string;
-}
-
-type ExternalLink = Required<Pick<BaseProps, 'url'>> & Omit<BaseProps, 'url'>;
-type PopoverLink = Required<Pick<BaseProps, 'showIcon'>> & Omit<BaseProps, 'showIcon'>;
-
-// This component intended to be used either as external link or icon + text for popover anchor,
-// so I tried to emphasize via types that either url or showIcon must be present
+};
 
 const InfoLink = ({
   url,
@@ -25,21 +19,21 @@ const InfoLink = ({
   iconName = 'info',
   className,
   fontClass = 'text-footnote',
-}: PropsWithChildren<ExternalLink | PopoverLink>) => {
-  return (
-    <a
-      {...(url ? { href: url, rel: 'noopener noreferrer', target: '_blank' } : {})}
-      className={cn(
-        'text-action-text-default hover:text-action-text cursor-pointer',
-        fontClass,
-        showIcon && 'flex items-center gap-x-1',
-        className,
-      )}
-    >
-      {showIcon && <Icon name={iconName} size={14} />}
-      {children}
-    </a>
-  );
-};
+}: PropsWithChildren<Props>) => (
+  <a
+    href={url}
+    rel="noopener noreferrer"
+    target="_blank"
+    className={cn(
+      'text-action-text-default hover:text-action-text cursor-pointer',
+      fontClass,
+      showIcon && 'flex items-center gap-x-1',
+      className,
+    )}
+  >
+    {showIcon && <Icon name={iconName} size={14} />}
+    {children}
+  </a>
+);
 
 export default InfoLink;
