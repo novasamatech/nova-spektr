a9f32f93434d4557a83894eef7aeeb4a832df0e0
egor0798 <egorbirukov1234@gmail.com>
2023-05-17 17:37:54 +0400

    fix: changed text base to show default color
    Co-authored-by: Egor <egor@novawallet.io>

--- src/renderer/components/ui-redesign/Typography/common/TextBase.ts
+++ src/renderer/components/ui-redesign/Typography/common/TextBase.ts
@@ -3,14 +3,12 @@ import cn from 'classnames';
 
 import { TypographyProps } from '@renderer/components/ui-redesign/Typography/common/types';
 
-const TextBase = ({
-  as = 'p',
-  fontWeight = 'normal',
-  align = 'left',
-  className = 'text-text-primary',
-  children,
-}: TypographyProps) => {
-  return createElement(as, { className: cn(`text-${align} font-${fontWeight}`, className) }, children);
+const TextBase = ({ as = 'p', fontWeight = 'normal', align = 'left', className, children }: TypographyProps) => {
+  return createElement(
+    as,
+    { className: cn(`text-${align} font-${fontWeight} text-text-primary`, className) },
+    children,
+  );
 };
 
 export type TextBaseType = typeof TextBase;
