76e07abe45e9792f55ec1a52a86a9515229ce0cd
Egor <egor@novawallet.io>
2023-06-05 19:08:01 +0400

    style: fixed vertocal align for empty state

--- src/renderer/screens/Operations/components/EmptyState/EmptyOperations.tsx
+++ src/renderer/screens/Operations/components/EmptyState/EmptyOperations.tsx
@@ -18,7 +18,7 @@ const EmptyOperations = ({ multisigAccount, isEmptyFromFilters }: Props) => {
     : 'operations.noOperationsWalletNotMulti';
 
   return (
-    <div className="flex flex-col items-center justify-center gap-y-8 w-full h-full mt-auto mb-auto">
+    <div className="flex flex-col items-center justify-center gap-y-8 flex-1 w-full">
       <Icon as="img" name="emptyList" alt={t('operations.noOperationsDescription')} size={178} />
       <BodyText align="center" className="text-text-tertiary max-w-[340px]">
         {t(emptyText)}
