b4aabc33221c20f226e611e3dd2bff37c416d8a7
Aleksandr Makhnev <alma2610@ya.ru>
2023-06-05 20:32:25 +0500

    feat: onboarding redesign (#746)

--- src/renderer/components/ui-redesign/Typography/components/HeaderTitleText.tsx
+++ src/renderer/components/ui-redesign/Typography/components/HeaderTitleText.tsx
@@ -2,6 +2,6 @@ import cnTw from '@renderer/shared/utils/twMerge';
 import { TypographyProps } from '@renderer/components/ui-redesign/Typography/common/types';
 import TextBase from '@renderer/components/ui-redesign/Typography/common/TextBase';
 
-export const SmallTitleText = ({ className, as = 'h4', fontWeight = 'bold', ...props }: TypographyProps) => (
-  <TextBase className={cnTw('text-small-title font-manrope', className)} fontWeight={fontWeight} {...props} />
+export const HeaderTitleText = ({ className, as = 'h3', fontWeight = 'bold', ...props }: TypographyProps) => (
+  <TextBase className={cnTw('text-header-title font-manrope', className)} fontWeight={fontWeight} {...props} />
 );
