7796673f0a72db4520d301a2ded47df49cd5bc8f
pgolovkin <golovkin.pl@gmail.com>
2023-06-07 17:23:35 +0300

    chore: links for the relase

--- src/renderer/screens/Signing/common/consts.ts
+++ src/renderer/screens/Signing/common/consts.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@renderer/domain/shared-kernel';
 
 const METADATA_PORTAL_URL = 'https://metadata.novasama.io/#/';
 export const TROUBLESHOOTING_URL =
-  'https://docs.novawallet.io/nova-wallet-wiki/help-and-support/troubleshooting#parity-signer-troubleshooting';
+  'https://docs.novaspektr.io/create-and-sign-operations/create-and-sign-operation-faq';
 
 export const getMetadataPortalUrl = (chainId: ChainId) => `${METADATA_PORTAL_URL}${chainId}`;
 
