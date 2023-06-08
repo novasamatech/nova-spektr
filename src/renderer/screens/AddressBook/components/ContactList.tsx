30f33692654403edbe34950343140e5bbb853567
Aleksandr Makhnev <alma2610@ya.ru>
2023-06-08 11:42:58 +0500

    Feat: improve light client data fetch (#770)
    * feat: improve light client data fetch
    * fix: address book comments
    * fix: network disconnect

--- src/renderer/screens/AddressBook/components/ContactList.tsx
+++ src/renderer/screens/AddressBook/components/ContactList.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 
-import { Button, Icon, Table } from '@renderer/components/ui';
 import { useI18n } from '@renderer/context/I18nContext';
 import { ContactDS } from '@renderer/services/storage';
 import { useToggle } from '@renderer/shared/hooks';
@@ -8,7 +7,7 @@ import ContactModal from './ContactModal';
 import EmptySearch from './EmptyState/EmptySearch';
 import { includes } from '@renderer/shared/utils/strings';
 import { AddressWithName } from '@renderer/components/common';
-import { BodyText } from '@renderer/components/ui-redesign';
+import { BodyText, FootnoteText, IconButton, Plate } from '@renderer/components/ui-redesign';
 
 type Props = {
   query?: string;
@@ -36,44 +35,41 @@ const ContactList = ({ contacts, query }: Props) => {
   }
 
   return (
-    <>
-      <Table by="id" className="border-spacing-y-2 border-separate" dataSource={filteredContacts}>
-        <Table.Header>
-          <Table.Column dataKey="name" align="left" width={250}>
-            <div className="flex items-center gap-x-1">{t('addressBook.contactList.nameColumnTitle')}</div>
-          </Table.Column>
-          <Table.Column dataKey="matrixId" align="left" width={250}>
-            <div className="flex items-center gap-x-1">{t('addressBook.contactList.matrixIdColumnTitle')}</div>
-          </Table.Column>
-          <Table.Column dataKey="actions" align="right"></Table.Column>
-        </Table.Header>
-        <Table.Body<ContactDS>>
-          {(contact) => (
-            <Table.Row key={contact.id} className="bg-row-background" height="lg">
-              <Table.Cell className="rounded-l">
-                <AddressWithName size={20} type="short" name={contact.name} canCopySubName address={contact.address} />
-              </Table.Cell>
-              <Table.Cell>
-                <BodyText className="text-text-primary">{contact.matrixId || '-'}</BodyText>
-              </Table.Cell>
-              <Table.Cell className="rounded-r">
-                <Button
-                  variant="text"
-                  pallet="shade"
-                  prefixElement={<Icon size={16} name="edit" />}
-                  onClick={() => {
-                    setCurrentContact(contact);
-                    toggleEditModal();
-                  }}
-                />
-              </Table.Cell>
-            </Table.Row>
-          )}
-        </Table.Body>
-      </Table>
+    <div className="flex flex-col gap-y-2">
+      <div className="grid grid-cols-[250px,250px,1fr] items-center px-3">
+        <FootnoteText className="text-text-secondary">{t('addressBook.contactList.nameColumnTitle')}</FootnoteText>
+        <FootnoteText className="text-text-secondary">{t('addressBook.contactList.matrixIdColumnTitle')}</FootnoteText>
+      </div>
+
+      <ul className="flex flex-col gap-y-2">
+        {filteredContacts.map((contact) => (
+          <li key={contact.id}>
+            <Plate className="grid grid-cols-[250px,250px,1fr] items-center p-0">
+              <AddressWithName
+                size={20}
+                type="short"
+                className="w-full truncate p-3"
+                name={contact.name}
+                canCopySubName
+                address={contact.address}
+              />
+              <BodyText className="text-text-primary p-3 truncate">{contact.matrixId || '-'}</BodyText>
+              <IconButton
+                size={16}
+                name="edit"
+                className="m-3"
+                onClick={() => {
+                  setCurrentContact(contact);
+                  toggleEditModal();
+                }}
+              />
+            </Plate>
+          </li>
+        ))}
+      </ul>
 
       <ContactModal isOpen={isEditModalShown} contact={currentContact} onToggle={toggleEditModal} />
-    </>
+    </div>
   );
 };
 
