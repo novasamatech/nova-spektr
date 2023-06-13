4418d15c429c3f795308bfa88e0eb563b37a1a25
Yaroslav Grachev <yaroslav@novawallet.io>
2023-06-13 19:26:10 +0300

    feat: add new svg logo

--- src/renderer/components/common/SplashScreen/SplashScreen.tsx
+++ src/renderer/components/common/SplashScreen/SplashScreen.tsx
@@ -2,7 +2,7 @@ import { Icon } from '@renderer/components/ui';
 
 const SplashScreen = () => {
   return (
-    <main className="flex items-center justify-center bg-cover h-screen">
+    <main className="flex items-center justify-center bg-cover h-screen gap-x-10">
       <Icon as="img" name="logo" size={140} alt={process.env.PRODUCT_NAME} />
       <h1 className="text-5xl font-semibold text-neutral-variant">{process.env.PRODUCT_NAME}</h1>
     </main>
