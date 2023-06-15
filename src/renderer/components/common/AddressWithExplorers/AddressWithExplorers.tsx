9f934ad3a8c7d8e0212442bd9a4365b5d00e076f
Aleksandr Makhnev <alma2610@ya.ru>
2023-06-15 19:35:14 +0500

    fix: transfer feedback (#814)

--- src/renderer/components/common/AddressWithExplorers/AddressWithExplorers.tsx
+++ src/renderer/components/common/AddressWithExplorers/AddressWithExplorers.tsx
@@ -18,8 +18,8 @@ const AddressWithExplorers = ({ explorers = [], showMatrix, ...addressProps }: P
 
   return (
     <InfoPopover data={popoverItems}>
-      <div className="flex items-center gap-x-1 cursor-pointer group hover:bg-action-background-hover px-2 py-1 rounded">
-        <AccountAddress {...addressProps} />
+      <div className="flex max-w-full items-center gap-x-1 cursor-pointer group hover:bg-action-background-hover px-2 h-6 rounded">
+        <AccountAddress className="w-full" {...addressProps} />
         <Icon name="info" size={16} className="text-icon-default group-hover:text-icon-hover" />
       </div>
     </InfoPopover>
