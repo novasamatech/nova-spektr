5552d0846e30af9e2b7c39205dea56e7801ce118
Egor <egor@novawallet.io>
2023-06-22 19:07:18 +0400

    chore: flipped password input condition

--- src/renderer/components/ui-redesign/Inputs/PasswordInput/PasswordInput.tsx
+++ src/renderer/components/ui-redesign/Inputs/PasswordInput/PasswordInput.tsx
@@ -19,7 +19,7 @@ const PasswordInput = forwardRef<HTMLInputElement, Props>(({ ...props }, ref) =>
       placeholder={t('settings.matrix.passwordPlaceholder')}
       suffixElement={
         <button className="ml-2 text-icon-default" onClick={toggleVisibility}>
-          <Icon name={!isHidden ? 'eyeSlashed' : 'eye'} size={20} alt={t('settings.matrix.passwordVisibilityButton')} />
+          <Icon name={isHidden ? 'eye' : 'eyeSlashed'} size={20} alt={t('settings.matrix.passwordVisibilityButton')} />
         </button>
       }
       {...props}
