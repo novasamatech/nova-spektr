0690311ac849bc1bbea4dd9b254a5aa12c02c0dd
Egor <egor@novawallet.io>
2023-07-04 15:05:10 +0400

    fix: added address prefix for selectable signatory

--- src/renderer/screens/Operations/components/modals/SignatorySelectModal.tsx
+++ src/renderer/screens/Operations/components/modals/SignatorySelectModal.tsx
@@ -2,21 +2,19 @@ import { BaseModal } from '@renderer/components/ui-redesign';
 import { useI18n } from '@renderer/context/I18nContext';
 import { AccountDS } from '@renderer/services/storage';
 import SelectableSignatory from '@renderer/components/common/SelectableSignatory/SelectableSignatory';
-import { ChainId } from '@renderer/domain/shared-kernel';
 import { Asset } from '@renderer/domain/asset';
-import { Explorer } from '@renderer/domain/chain';
+import { Chain } from '@renderer/domain/chain';
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
   onSelect: (account: AccountDS) => void;
   accounts: AccountDS[];
-  chainId?: ChainId;
+  chain: Chain;
   asset?: Asset;
-  explorers?: Explorer[];
 };
 
-const SignatorySelectModal = ({ isOpen, onClose, onSelect, accounts, asset, chainId, explorers }: Props) => {
+const SignatorySelectModal = ({ isOpen, onClose, onSelect, accounts, asset, chain }: Props) => {
   const { t } = useI18n();
 
   return (
@@ -29,15 +27,15 @@ const SignatorySelectModal = ({ isOpen, onClose, onSelect, accounts, asset, chai
     >
       <ul className="mt-1 max-h-[332px] overflow-y-scroll">
         {asset &&
-          chainId &&
           accounts.map((a) => (
             <li key={a.id}>
               <SelectableSignatory
                 accountId={a.accountId}
+                addressPrefix={chain.addressPrefix}
                 name={a.name}
                 asset={asset}
-                chainId={chainId}
-                explorers={explorers}
+                chainId={chain.chainId}
+                explorers={chain.explorers}
                 value={a}
                 onSelected={onSelect}
               />
