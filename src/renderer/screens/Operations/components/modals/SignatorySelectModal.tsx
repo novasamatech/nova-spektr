a87485f95a65233617c8dffe21acd45decd2796c
egor0798 <egorbirukov1234@gmail.com>
2023-07-04 19:37:46 +0400

    Fix: added scroll for signatory select window
    * fix: added scroll for signatory select window
    
    * fix: added address prefix for selectable signatory
    
    ---------
    
    Co-authored-by: Egor <egor@novawallet.io>

--- src/renderer/screens/Operations/components/modals/SignatorySelectModal.tsx
+++ src/renderer/screens/Operations/components/modals/SignatorySelectModal.tsx
@@ -2,21 +2,19 @@ import { BaseModal } from '@renderer/components/ui-redesign';
 import { useI18n } from '@renderer/context/I18nContext';
 import { AccountDS } from '@renderer/services/storage';
 import SelectableSignatory from '@renderer/components/common/SelectableSignatory/SelectableSignatory';
-import { ChainId } from '@renderer/domain/shared-kernel';
 import { Asset } from '@renderer/domain/asset';
-import { Explorer } from '@renderer/domain/chain';
+import { Chain } from '@renderer/domain/chain';
 
 type Props = {
   isOpen: boolean;
   onClose: () => void;
   onSelect: (account: AccountDS) => void;
   accounts: AccountDS[];
-  chainId?: ChainId;
+  chain: Chain;
   asset?: Asset;
-  explorers?: Explorer[];
 };
 
-const SignatorySelectModal = ({ isOpen, onClose, onSelect, accounts, asset, chainId, explorers }: Props) => {
+const SignatorySelectModal = ({ isOpen, onClose, onSelect, accounts, asset, chain }: Props) => {
   const { t } = useI18n();
 
   return (
@@ -27,17 +25,17 @@ const SignatorySelectModal = ({ isOpen, onClose, onSelect, accounts, asset, chai
       panelClass="w-[420px]"
       onClose={onClose}
     >
-      <ul className="mt-1">
+      <ul className="mt-1 max-h-[332px] overflow-y-scroll">
         {asset &&
-          chainId &&
           accounts.map((a) => (
             <li key={a.id}>
               <SelectableSignatory
                 accountId={a.accountId}
+                addressPrefix={chain.addressPrefix}
                 name={a.name}
                 asset={asset}
-                chainId={chainId}
-                explorers={explorers}
+                chainId={chain.chainId}
+                explorers={chain.explorers}
                 value={a}
                 onSelected={onSelect}
               />
