33c4b23f8a466a151663003af7ac49abe5f91c91
Egor <egor@novawallet.io>
2023-07-06 20:01:55 +0400

    fix: updated select style

--- src/renderer/screens/Operations/components/Filters.tsx
+++ src/renderer/screens/Operations/components/Filters.tsx
@@ -116,7 +116,7 @@ const Filters = ({ txs, onChangeFilters }: Props) => {
     selectedOptions.network.length || selectedOptions.status.length || selectedOptions.type.length;
 
   return (
-    <div className="flex items-center gap-2 my-4 w-[736px] ml-6">
+    <div className="flex items-center gap-2 my-4 w-[736px] h-9 ml-6">
       <MultiSelect
         className="w-[200px]"
         placeholder={t('operations.filters.statusPlaceholder')}
