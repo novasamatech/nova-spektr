7582023ef12a38889ea87e061d48112159163cd0
Aleksandr Makhnev <alma2610@ya.ru>
2023-07-07 14:37:09 +0500

    fix: loading balance for multishard (#947)

--- src/renderer/screens/Assets/components/NetworkAssets/NetworkAssets.test.tsx
+++ src/renderer/screens/Assets/components/NetworkAssets/NetworkAssets.test.tsx
@@ -4,6 +4,8 @@ import { Chain } from '@renderer/domain/chain';
 import { TEST_ACCOUNT_ID } from '@renderer/shared/utils/constants';
 import chains from '@renderer/services/network/common/chains/chains.json';
 import { NetworkAssets } from './NetworkAssets';
+import { Account } from '@renderer/domain/account';
+import { ChainType, CryptoType, SigningType } from '@renderer/domain/shared-kernel';
 
 const testChain = chains.find((chain) => chain.assets.length > 1) as Chain;
 const testAsset = testChain.assets[0];
@@ -43,23 +45,35 @@ jest.mock('../AssetCard/AssetCard', () => ({
   AssetCard: ({ asset }: any) => <span data-testid="AssetCard">{asset.name}</span>,
 }));
 
+const accounts = [
+  {
+    accountId: TEST_ACCOUNT_ID,
+    name: 'test',
+    isActive: true,
+    isMain: true,
+    signingType: SigningType.PARITY_SIGNER,
+    cryptoType: CryptoType.SR25519,
+    chainType: ChainType.SUBSTRATE,
+  },
+] as Account[];
+
 describe('screen/Assets/NetworkAssets', () => {
   test('should render component', () => {
-    render(<NetworkAssets chain={testChain} accountIds={[TEST_ACCOUNT_ID]} />);
+    render(<NetworkAssets chain={testChain} accounts={accounts} />);
 
     const text = screen.getByText(testChain.name);
     expect(text).toBeInTheDocument();
   });
 
   test('should render assets', () => {
-    render(<NetworkAssets chain={testChain} accountIds={[TEST_ACCOUNT_ID]} />);
+    render(<NetworkAssets chain={testChain} accounts={accounts} />);
 
     const balances = screen.getAllByTestId('AssetCard');
     expect(balances).toHaveLength(7);
   });
 
   test('should hide assets', async () => {
-    render(<NetworkAssets chain={testChain} accountIds={[TEST_ACCOUNT_ID]} />);
+    render(<NetworkAssets chain={testChain} accounts={accounts} />);
 
     const balancesBefore = screen.getAllByTestId('AssetCard');
     expect(balancesBefore).toHaveLength(7);
@@ -72,14 +86,14 @@ describe('screen/Assets/NetworkAssets', () => {
   });
 
   test('should show unverified badge', () => {
-    render(<NetworkAssets chain={testChain} accountIds={[TEST_ACCOUNT_ID]} />);
+    render(<NetworkAssets chain={testChain} accounts={accounts} />);
 
     const unverifiedBadge = screen.getByText('balances.verificationFailedLabel');
     expect(unverifiedBadge).toBeInTheDocument();
   });
 
   test('should sort assets by balance and name', () => {
-    render(<NetworkAssets chain={testChain} accountIds={[TEST_ACCOUNT_ID]} />);
+    render(<NetworkAssets chain={testChain} accounts={accounts} />);
 
     const assetsNames = screen.getAllByTestId('AssetCard').map((element) => element.firstChild);
 
--- src/renderer/screens/Assets/components/NetworkAssets/NetworkAssets.tsx
+++ src/renderer/screens/Assets/components/NetworkAssets/NetworkAssets.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { groupBy } from 'lodash';
 
 import { Icon } from '@renderer/components/ui';
 import { Asset } from '@renderer/domain/asset';
 import { Chain as ChainType } from '@renderer/domain/chain';
-import { AccountId } from '@renderer/domain/shared-kernel';
 import { useBalance } from '@renderer/services/balance/balanceService';
 import { ZERO_BALANCE } from '@renderer/services/balance/common/constants';
 import { totalAmount } from '@renderer/shared/utils/balance';
@@ -14,6 +14,9 @@ import { includes } from '@renderer/shared/utils/strings';
 import { CaptionText, Chain, Tooltip, Accordion } from '@renderer/components/ui-redesign';
 import { AssetCard } from '../AssetCard/AssetCard';
 import { balanceSorter, sumBalances } from '../../common/utils';
+import { BalanceDS } from '@renderer/services/storage';
+import { Account } from '@renderer/domain/account';
+import { AccountId } from '@renderer/domain/shared-kernel';
 import cnTw from '@renderer/shared/utils/twMerge';
 
 type Props = {
@@ -21,7 +24,7 @@ type Props = {
   searchSymbolOnly?: boolean;
   query?: string;
   chain: ChainType | ExtendedChain;
-  accountIds: AccountId[];
+  accounts: Account[];
   canMakeActions?: boolean;
   onReceiveClick?: (asset: Asset) => void;
   onTransferClick?: (asset: Asset) => void;
@@ -31,7 +34,7 @@ export const NetworkAssets = ({
   query,
   hideZeroBalance,
   chain,
-  accountIds,
+  accounts,
   searchSymbolOnly,
   canMakeActions,
   onReceiveClick,
@@ -41,15 +44,39 @@ export const NetworkAssets = ({
   const { getLiveNetworkBalances } = useBalance();
 
   const [filteredAssets, setFilteredAssets] = useState<Asset[]>([]);
+  const [balancesObject, setBalancesObject] = useState<Record<string, Balance>>({});
+
+  const selectedAccountIds = accounts.map((a) => a.accountId).join('');
+
+  const accountIds = useMemo(() => {
+    return accounts.reduce<AccountId[]>((acc, account) => {
+      if (!account.chainId || account.chainId === chain.chainId) acc.push(account.accountId);
+
+      return acc;
+    }, []);
+  }, [chain.chainId, selectedAccountIds]);
 
   const balances = getLiveNetworkBalances(accountIds, chain.chainId);
 
-  const balancesObject =
-    balances?.reduce<Record<string, Balance>>((acc, balance) => {
-      acc[balance.assetId] = sumBalances(balance, acc[balance.assetId]);
+  useEffect(() => {
+    const accountsAmount = new Set(accountIds).size;
 
-      return acc;
-    }, {}) || {};
+    const newBalancesObject = Object.values(groupBy(balances, 'assetId')).reduce<Record<string, Balance>>(
+      (acc, balances: BalanceDS[]) => {
+        if (balances.length === accountsAmount) {
+          acc[balances[0].assetId] = balances.reduce<Balance>(
+            (acc, balance) => sumBalances(balance, acc),
+            {} as Balance,
+          );
+        }
+
+        return acc;
+      },
+      {},
+    );
+
+    setBalancesObject(newBalancesObject);
+  }, [balances, accountIds.join('')]);
 
   useEffect(() => {
     const filteredAssets = chain.assets.filter((asset) => {
@@ -63,12 +90,13 @@ export const NetworkAssets = ({
 
       const balance = balancesObject[asset.assetId];
 
-      return !hideZeroBalance || !balance?.verified || totalAmount(balance) !== ZERO_BALANCE;
+      return !hideZeroBalance || balance?.verified === false || (balance && totalAmount(balance) !== ZERO_BALANCE);
     });
+
     filteredAssets.sort((a, b) => balanceSorter(a, b, balancesObject));
 
     setFilteredAssets(filteredAssets);
-  }, [balances, query, hideZeroBalance]);
+  }, [balancesObject, query, hideZeroBalance]);
 
   if (filteredAssets.length === 0) {
     return null;
