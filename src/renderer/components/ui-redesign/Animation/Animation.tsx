986b7fb92f0ab9403e90f5d36bf09c2ccc6beeaa
egor0798 <egorbirukov1234@gmail.com>
2023-07-17 17:46:06 +0400

    fix: Animation leads to app stuck
    * fix: [wip] removed animation from operation result
    
    * fix: animation memory leak fix
    
    ---------
    
    Co-authored-by: Egor <egor@novawallet.io>

--- src/renderer/components/ui-redesign/Animation/Animation.tsx
+++ src/renderer/components/ui-redesign/Animation/Animation.tsx
@@ -5,7 +5,7 @@ import Animations from './Data';
 export type AnimationNames = keyof typeof Animations;
 
 export type Props = {
-  name: AnimationNames;
+  animation: Object;
   width?: number;
   height?: number;
   loop?: boolean;
@@ -13,11 +13,11 @@ export type Props = {
   className?: string;
 };
 
-export const Animation = ({ name, width = 80, height = 80, loop = false, autoplay = true, className }: Props) => {
+export const Animation = ({ animation, width = 80, height = 80, loop = false, autoplay = true, className }: Props) => {
   const defaultOptions = {
     loop,
     autoplay,
-    animationData: Animations[name],
+    animationData: animation,
     rendererSettings: {
       preserveAspectRatio: 'xMidYMid slice',
     },
