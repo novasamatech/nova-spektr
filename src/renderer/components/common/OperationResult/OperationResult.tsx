436d2748f80394766d4fd629b486a9c41781e1a9
egor0798 <egorbirukov1234@gmail.com>
2023-07-17 17:46:06 +0400

    fix: Animation leads to app stuck (#969)
    * fix: [wip] removed animation from operation result
    
    * fix: animation memory leak fix
    
    ---------
    
    Co-authored-by: Egor <egor@novawallet.io>

--- src/renderer/components/common/OperationResult/OperationResult.tsx
+++ src/renderer/components/common/OperationResult/OperationResult.tsx
@@ -1,11 +1,12 @@
-import { Fragment, PropsWithChildren } from 'react';
+import { Fragment, PropsWithChildren, useEffect, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
 import { ModalBackdrop, ModalTransition } from '@renderer/components/ui-redesign/Modals/common';
 import { FootnoteText, SmallTitleText } from '@renderer/components/ui-redesign';
 import { Animation } from '@renderer/components/ui-redesign/Animation/Animation';
-import { VariantAnimations, VariantAnimationProps } from './common/constants';
+import { VariantAnimationProps } from './common/constants';
 import { Variant } from './common/types';
+import Animations from '@renderer/components/ui-redesign/Animation/Data';
 
 type Props = {
   title: string;
@@ -22,28 +23,39 @@ export const OperationResult = ({
   isOpen,
   children,
   onClose,
-}: PropsWithChildren<Props>) => (
-  <Transition appear show={isOpen} as={Fragment}>
-    <Dialog as="div" className="relative z-50" onClose={onClose}>
-      <ModalBackdrop />
+}: PropsWithChildren<Props>) => {
+  const [animation, setAnimation] = useState();
 
-      <div className="fixed inset-0 overflow-hidden flex min-h-full w-full items-center justify-center text-center">
-        <ModalTransition>
-          {/* TODO: change bg color */}
-          <Dialog.Panel className="w-[240px] max-w-md transform flex flex-col items-center justify-center rounded-lg bg-white p-4 align-middle shadow-card-shadow transition-all">
-            <Animation name={VariantAnimations[variant]} {...VariantAnimationProps[variant]} />
-            <Dialog.Title className="font-semibold mb-2">
-              <SmallTitleText align="center">{title}</SmallTitleText>
-            </Dialog.Title>
-            {description && (
-              <FootnoteText className="text-text-tertiary" align="center">
-                {description}
-              </FootnoteText>
-            )}
-            <div className="mt-3">{children}</div>
-          </Dialog.Panel>
-        </ModalTransition>
-      </div>
-    </Dialog>
-  </Transition>
-);
+  useEffect(() => {
+    if (isOpen) {
+      // using same animation repeatedly without deep clone lead to memory leak
+      // https://github.com/airbnb/lottie-web/issues/1159
+      setAnimation(JSON.parse(JSON.stringify(Animations[variant])));
+    }
+  }, [isOpen, variant]);
+
+  return (
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className="relative z-50" onClose={onClose}>
+        <ModalBackdrop />
+
+        <div className="fixed inset-0 overflow-hidden flex min-h-full w-full items-center justify-center text-center">
+          <ModalTransition>
+            <Dialog.Panel className="w-[240px] max-w-md transform flex flex-col items-center justify-center rounded-lg bg-white p-4 align-middle shadow-card-shadow transition-all">
+              {animation && <Animation animation={animation} {...VariantAnimationProps[variant]} />}
+              <Dialog.Title className="font-semibold mb-2">
+                <SmallTitleText align="center">{title}</SmallTitleText>
+              </Dialog.Title>
+              {description && (
+                <FootnoteText className="text-text-tertiary" align="center">
+                  {description}
+                </FootnoteText>
+              )}
+              <div className="mt-3">{children}</div>
+            </Dialog.Panel>
+          </ModalTransition>
+        </div>
+      </Dialog>
+    </Transition>
+  );
+};
