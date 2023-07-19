374dc1d18ebc43628e39aceaa33777fafe287e93
asmadek <alma2610@ya.ru>
2023-07-19 10:37:06 +0500

    chore: remove console log

--- src/renderer/context/MultisigChainContext/MultisigChainContext.tsx
+++ src/renderer/context/MultisigChainContext/MultisigChainContext.tsx
@@ -100,8 +100,6 @@ export const MultisigChainProvider = ({ children }: PropsWithChildren) => {
         txIndex: indexCreated,
       });
 
-      console.log('purr', events);
-
       const pendingEvent = events.find(
         (event) => pendingEventStatuses.includes(event.status) && event.accountId === accountId,
       );
