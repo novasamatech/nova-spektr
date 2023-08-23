f63e1d1c8968fcd8bd1740e94cbb0fa2c301785a
Egor <egor@novawallet.io>
2023-08-23 13:33:21 +0400

    chore: added comment

--- src/renderer/features/operation/init/model/errors.ts
+++ src/renderer/features/operation/init/model/errors.ts
@@ -3,7 +3,7 @@ import { ObjectValues } from '@renderer/domain/utility';
 export const OperationError = {
   INVALID_FEE: 'staking.notEnoughBalanceForFeeError',
   INVALID_DEPOSIT: 'staking.notEnoughBalanceForDepositError',
-  EMPTY_ERROR: ' ',
+  EMPTY_ERROR: ' ', // error for invalid state with no hint text
 } as const;
 
 export type OperationErrorType = ObjectValues<typeof OperationError>;
