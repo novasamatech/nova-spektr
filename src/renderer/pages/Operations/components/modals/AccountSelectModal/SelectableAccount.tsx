e441ff582a246c5e6e27894fd2cb40c2b6693007
asmadek <alma2610@ya.ru>
2023-09-13 16:15:44 +0500

    chore: remove account info on myself input

--- src/renderer/pages/Operations/components/modals/AccountSelectModal/SelectableAccount.tsx
+++ src/renderer/pages/Operations/components/modals/AccountSelectModal/SelectableAccount.tsx
@@ -1,5 +1,5 @@
-import { AccountAddress, getAddress, AccountAddressProps, useAddressInfo } from '@renderer/entities/account';
-import { InfoPopover, Icon } from '@renderer/shared/ui';
+import { AccountAddress, AccountAddressProps } from '@renderer/entities/account';
+import { Icon } from '@renderer/shared/ui';
 import { Explorer } from '@renderer/entities/chain';
 import { ChainId } from '@renderer/domain/shared-kernel';
 
@@ -19,18 +19,12 @@ export const SelectableAccount = <T extends any>({
   name,
   ...addressProps
 }: Props<T>) => {
-  const address = getAddress(addressProps);
-  const popoverItems = useAddressInfo(address, explorers, true);
-
   return (
     <button
       className="group flex items-center cursor-pointer hover:bg-action-background-hover px-2 py-1.5 rounded w-full text-text-secondary active:text-text-primary"
       onClick={() => onSelected(value)}
     >
       <AccountAddress addressFont="text-body text-inherit" size={size} name={name} {...addressProps} />
-      <InfoPopover data={popoverItems}>
-        <Icon name="info" size={14} className="ml-2 mr-auto" />
-      </InfoPopover>
       <Icon name="right" className="ml-auto" size={16} />
     </button>
   );
