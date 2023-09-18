a551f41c45d421698ce0f435ab39506723663a9e
Aleksandr Makhnev <alma2610@ya.ru>
2023-09-18 20:07:17 +0500

    fix: transfer modal close (#1074)

--- src/renderer/pages/Assets/SendAsset/SendAsset.tsx
+++ src/renderer/pages/Assets/SendAsset/SendAsset.tsx
@@ -1,15 +1,11 @@
-import { useNavigate } from 'react-router-dom';
-
 import { AssetRouteGuard } from '@renderer/features/assets';
 import { Paths } from '@renderer/app/providers';
 import { SendAssetModal } from '@renderer/widgets';
 
 export const SendAsset = () => {
-  const navigate = useNavigate();
-
   return (
     <AssetRouteGuard redirectPath={Paths.ASSETS}>
-      {(chain, asset) => <SendAssetModal chain={chain} asset={asset} onClose={() => navigate(Paths.ASSETS)} />}
+      {(chain, asset) => <SendAssetModal chain={chain} asset={asset} />}
     </AssetRouteGuard>
   );
 };
