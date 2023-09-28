10a9367183f2d7c5d27d1350eabc85d0751fd406
Egor B <egor@novawallet.io>
2023-09-28 17:13:14 +0200

    feat: added fiat values to staking operations

--- src/renderer/pages/Operations/components/TransactionAmount.tsx
+++ src/renderer/pages/Operations/components/TransactionAmount.tsx
@@ -1,11 +1,8 @@
-import { useUnit } from 'effector-react';
-
 import { DecodedTransaction, Transaction } from '@renderer/entities/transaction';
 import { chainsService } from '@renderer/entities/network';
 import { AssetBalance } from '@renderer/entities/asset';
 import { cnTw, getAssetById } from '@renderer/shared/lib/utils';
 import { getTransactionAmount } from '../common/utils';
-import { priceProviderModel } from '@renderer/entities/price';
 import { AssetFiatBalance } from '@renderer/entities/price/ui/AssetFiatBalance';
 
 type Props = {
@@ -14,16 +11,10 @@ type Props = {
 };
 
 export const TransactionAmount = ({ tx, className }: Props) => {
-  const fiatFlag = useUnit(priceProviderModel.$fiatFlag);
-
   const asset = tx && getAssetById(tx.args.asset, chainsService.getChainById(tx.chainId)?.assets);
   const value = getTransactionAmount(tx);
 
   if (!asset || !value) return null;
-
-  if (!fiatFlag) {
-    return <AssetBalance value={value} asset={asset} className={className} />;
-  }
 
   return (
     <div className={cnTw('flex flex-col gap-y-1 items-center')}>
