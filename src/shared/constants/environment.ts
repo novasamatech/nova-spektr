fabb96083422e0d139eb18c121b5b3015a1b2c02
Stepan Lavrentev <lawrentievsv@gmail.com>
2023-09-28 09:55:13 +0300

    fix: eslinter

--- src/shared/constants/environment.ts
+++ src/shared/constants/environment.ts
@@ -1,5 +1,5 @@
 export const ENVIRONMENT = {
   IS_DEV: process.env.NODE_ENV === 'development',
   IS_STAGE: process.env.NODE_ENV === 'staging',
-  IS_FORCE_ELECTRON: process.env.FORCE_ELECTRON === 'true'
+  IS_FORCE_ELECTRON: process.env.FORCE_ELECTRON === 'true',
 };
