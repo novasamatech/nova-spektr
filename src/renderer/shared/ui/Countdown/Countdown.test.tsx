8ca875e7a44afcd1c44d3203878db8963a64017d
asmadek <alma2610@ya.ru>
2023-10-16 15:26:02 +0500

    chore: fix tests

--- src/renderer/shared/ui/Countdown/Countdown.test.tsx
+++ src/renderer/shared/ui/Countdown/Countdown.test.tsx
@@ -11,9 +11,18 @@ jest.mock('@renderer/pages/Onboarding/WalletConnect/model/wc-onboarding-model', 
   wcOnboardingModel: { events: {} },
 }));
 
+jest.mock('@renderer/app/providers', () => ({
+  useI18n: jest.fn().mockReturnValue({
+    t: (key: string) => key,
+  }),
+}));
+
 describe('ui/Countdown', () => {
   test('should render component', () => {
     render(<Countdown countdown={10} />);
+
+    const title = screen.getByText('signing.qrCountdownTitle');
+    expect(title).toBeInTheDocument();
 
     const timer = screen.getByText('0:10');
     expect(timer).toBeInTheDocument();
