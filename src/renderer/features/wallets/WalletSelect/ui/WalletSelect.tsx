ae58708d8729d76c23f6903eaf95688cbbad07c9
egor0798 <egorbirukov1234@gmail.com>
2023-10-27 13:31:08 +0200

    fix: wallet select overlap, extra divider on multisig staking confirm (#1185)
    Co-authored-by: Egor B <egor@novawallet.io>

--- src/renderer/features/wallets/WalletSelect/ui/WalletSelect.tsx
+++ src/renderer/features/wallets/WalletSelect/ui/WalletSelect.tsx
@@ -33,17 +33,21 @@ export const WalletSelect = ({ action }: Props) => {
 
   return (
     <Popover className="relative">
-      <WalletButton wallet={activeWallet} />
-      <Transition
-        enter="transition ease-out duration-200"
-        enterFrom="opacity-0 translate-y-1"
-        enterTo="opacity-100 translate-y-0"
-        leave="transition ease-in duration-150"
-        leaveFrom="opacity-100 translate-y-0"
-        leaveTo="opacity-0 translate-y-1"
-      >
-        <WalletPanel action={action} />
-      </Transition>
+      {({ close }) => (
+        <>
+          <WalletButton wallet={activeWallet} />
+          <Transition
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <WalletPanel action={action} onClose={close} />
+          </Transition>
+        </>
+      )}
     </Popover>
   );
 };
