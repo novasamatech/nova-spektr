4b6de48734195b07ec0f888edf4530b86a4f000e
Aleksandr Makhnev <alma2610@ya.ru>
2023-11-01 18:14:14 +0500

    Fix: reject signing
    * fix: reject signing
    * fix: wallet connect refresh

--- src/renderer/features/operation/sign/model/wallet-connect-sign-model.ts
+++ src/renderer/features/operation/sign/model/wallet-connect-sign-model.ts
@@ -1,5 +1,7 @@
-import { createEvent, createStore, forward, sample } from 'effector';
+import { combine, createEffect, createEvent, createStore, forward, sample } from 'effector';
 import { combineEvents } from 'patronum';
+import Client from '@walletconnect/sign-client';
+import { EngineTypes } from '@walletconnect/types';
 
 import { walletConnectModel, type InitReconnectParams } from '@renderer/entities/walletConnect';
 import { toAccountId } from '@renderer/shared/lib/utils';
@@ -7,16 +9,61 @@ import { chainsService } from '@renderer/entities/network';
 import { WalletConnectAccount } from '@renderer/shared/core';
 import { ReconnectStep } from '../lib/constants';
 import { walletModel } from '@renderer/entities/wallet';
-import { isConnectedStep, isReconnectingStep, isTopicExists } from '../lib/utils';
+import { isConnectedStep, isReconnectingStep, isRejectedStep, isTopicExists } from '../lib/utils';
 import { signModel } from './sign-model';
+import { SignResponse } from '../lib/types';
+
+type SignParams = {
+  client: Client;
+  payload: EngineTypes.RequestParams;
+};
 
 const reset = createEvent();
 const reconnectModalShown = createEvent();
 const reconnectStarted = createEvent<InitReconnectParams>();
 const reconnectAborted = createEvent();
 const reconnectDone = createEvent();
+const signingStarted = createEvent<SignParams>();
 
 const $reconnectStep = createStore(ReconnectStep.NOT_STARTED).reset(reset);
+const $isSigningRejected = createStore(false).reset(reset);
+const $signature = createStore('').reset(reset);
+
+const $isStatusShown = combine(
+  {
+    reconnectStep: $reconnectStep,
+    isSigningRejected: $isSigningRejected,
+  },
+  ({ reconnectStep, isSigningRejected }): boolean => {
+    return (
+      isReconnectingStep(reconnectStep) ||
+      isConnectedStep(reconnectStep) ||
+      isRejectedStep(reconnectStep) ||
+      isSigningRejected
+    );
+  },
+);
+
+const signFx = createEffect(({ client, payload }: SignParams): Promise<SignResponse> => {
+  return client.request(payload);
+});
+
+forward({
+  from: signingStarted,
+  to: signFx,
+});
+
+sample({
+  clock: signFx.doneData,
+  fn: ({ signature }) => signature,
+  target: $signature,
+});
+
+sample({
+  clock: signFx.fail,
+  fn: () => true,
+  target: $isSigningRejected,
+});
 
 sample({
   clock: reconnectModalShown,
@@ -94,15 +141,18 @@ sample({
   target: $reconnectStep,
 });
 
-sample({
-  clock: [reconnectAborted, reconnectDone],
-  fn: () => ReconnectStep.NOT_STARTED,
-  target: $reconnectStep,
+forward({
+  from: [reconnectAborted, reconnectDone],
+  to: reset,
 });
 
 export const walletConnectSignModel = {
   $reconnectStep,
+  $isSigningRejected,
+  $signature,
+  $isStatusShown,
   events: {
+    signingStarted,
     reset,
     reconnectModalShown,
     reconnectStarted,
