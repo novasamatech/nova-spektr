5b665ddfecdd9e147dc46766a44be80948674d0d
Egor B <egor@novawallet.io>
2023-12-11 18:42:52 +0100

    chore: linter err fix

--- src/renderer/shared/ui/Dropdowns/common/types.ts
+++ src/renderer/shared/ui/Dropdowns/common/types.ts
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
-import { IconNames } from "@shared/ui/Icon/data";
+
+import { IconNames } from '@shared/ui/Icon/data';
 
 export type Position = 'up' | 'down' | 'auto';
 export type Theme = 'dark' | 'light';
