445b12f0f9c07cb69f014f378fd13d44f49bb0dc
Egor B <egor@novawallet.io>
2023-12-13 14:19:08 +0100

    chore: updated tests

--- src/renderer/features/wallets/RenameWallet/model/__tests__/rename-wallet-model.test.ts
+++ src/renderer/features/wallets/RenameWallet/model/__tests__/rename-wallet-model.test.ts
@@ -3,7 +3,7 @@ import { fork, allSettled } from 'effector';
 import { walletMock } from './mocks/wallet-mock';
 import { renameWalletModel } from '../rename-wallet-model';
 import { walletModel } from '@entities/wallet';
-import { walletSelectModel } from '@features/wallets';
+import { storageService } from '@shared/api/storage';
 
 describe('entities/wallet/model/wallet-model', () => {
   afterEach(() => {
@@ -23,18 +23,20 @@ describe('entities/wallet/model/wallet-model', () => {
     expect(scope.getState(renameWalletModel.$walletForm.$isValid)).toEqual(false);
   });
 
-  test('should updated $walletForDetails and $wallets after name change', async () => {
-    const wallets = [walletMock.wallet1, walletMock.wallet2];
+  test('should updated wallet name after form submit', async () => {
     const newName = 'New wallet name';
+    const updatedWallet = { ...walletMock.wallet1, name: newName };
+
+    jest.spyOn(storageService.wallets, 'update').mockResolvedValue(updatedWallet.id);
+
     const scope = fork({
-      values: new Map().set(walletModel.$wallets, wallets).set(walletSelectModel.$walletForDetails, walletMock.wallet1),
+      values: new Map().set(walletModel.$wallets, [walletMock.wallet1]),
     });
 
     await allSettled(renameWalletModel.events.formInitiated, { scope, params: walletMock.wallet1 });
-    await allSettled(renameWalletModel.$walletForm.setForm, { scope, params: { name: newName } });
+    await allSettled(renameWalletModel.$walletForm.fields.name.onChange, { scope, params: newName });
     await allSettled(renameWalletModel.$walletForm.submit, { scope });
 
-    expect(scope.getState(walletSelectModel.$walletForDetails)?.name).toEqual(newName);
-    expect(scope.getState(walletModel.$wallets).find((w) => w.id === walletMock.wallet1.id)?.name).toEqual(newName);
+    expect(scope.getState(walletModel.$wallets).find((w) => w.id === updatedWallet.id)).toEqual(updatedWallet);
   });
 });
