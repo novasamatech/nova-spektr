9353d6f8b7655ef1ac452ede35533908c2e35757
Egor B <egor@novawallet.io>
2023-12-22 17:04:01 +0100

    chore: added accordion hover

--- src/renderer/entities/wallet/ui/MultishardAccountsList/MultishardAccountsList.tsx
+++ src/renderer/entities/wallet/ui/MultishardAccountsList/MultishardAccountsList.tsx
@@ -23,9 +23,9 @@ export const MultishardAccountsList = ({ accounts, chains, className }: Props) =
   return (
     <div className={cnTw('flex flex-col overflow-y-auto', className)}>
       {accountList.map(([baseAccount, chainMap]) => (
-        <div key={baseAccount.id} className="flex flex-col pl-5 pr-2">
+        <div key={baseAccount.id} className="flex flex-col pl-5">
           <ContactItem
-            className="sticky top-0 bg-white z-10 py-4"
+            className="sticky top-0 bg-white z-10 py-4 pr-2"
             addressFont="text-text-secondary"
             size={28}
             name={baseAccount.name}
@@ -39,8 +39,8 @@ export const MultishardAccountsList = ({ accounts, chains, className }: Props) =
             if (!chainMap[chain.chainId]) return;
 
             return (
-              <Token key={chain.chainId} isDefaultOpen className="pl-10">
-                <Token.Button buttonClass="px-0">
+              <Token key={chain.chainId} isDefaultOpen className="pl-8">
+                <Token.Button>
                   <div className="flex gap-x-2">
                     <ChainTitle fontClass="text-text-primary" chain={chain} />
                     <FootnoteText className="text-text-tertiary">{chainMap[chain.chainId].length}</FootnoteText>
@@ -48,7 +48,7 @@ export const MultishardAccountsList = ({ accounts, chains, className }: Props) =
                 </Token.Button>
                 <Token.Content>
                   {chainMap[chain.chainId].map((account) => (
-                    <div key={account.id} className="flex items-center py-1.5 mb-2">
+                    <div key={account.id} className="flex items-center py-1.5 mb-2 px-2">
                       <ContactItem
                         addressFont="text-text-secondary"
                         name={account.name}
