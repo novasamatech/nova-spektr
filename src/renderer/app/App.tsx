6c26e87a325d888871ed1e75c3a0cb2499cb0c8d
asmadek <alma2610@ya.ru>
2024-01-29 15:08:42 +0500

    fix: don't switch to assets after add proxied

--- src/renderer/app/App.tsx
+++ src/renderer/app/App.tsx
@@ -17,6 +17,7 @@ import {
   GraphqlProvider,
   MultisigChainProvider,
 } from './providers';
+import { usePrevious } from '../shared/lib/hooks';
 
 const SPLASH_SCREEN_DELAY = 450;
 
@@ -27,6 +28,8 @@ export const App = () => {
   const wallets = useUnit(walletModel.$wallets);
   const isLoadingWallets = useUnit(walletModel.$isLoadingWallets);
 
+  const previousWallets = usePrevious(wallets);
+
   const [splashScreenLoading, setSplashScreenLoading] = useState(true);
 
   useEffect(() => {
@@ -34,7 +37,8 @@ export const App = () => {
   }, []);
 
   useEffect(() => {
-    if (isLoadingWallets) return;
+    const alreadyAddNewWallet = previousWallets?.length > 0 && wallets.length > 0;
+    if (isLoadingWallets || alreadyAddNewWallet) return;
 
     const path = wallets.length > 0 ? Paths.ASSETS : Paths.ONBOARDING;
     navigate(path, { replace: true });
