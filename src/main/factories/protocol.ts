baa9abac57edbf08b273d0595d09dff7c50af571
Yaroslav Grachev <tuulwq@gmail.com>
2024-02-19 15:06:19 +0300

    Fix: Revert scheme (#1482)

--- src/main/factories/protocol.ts
+++ src/main/factories/protocol.ts
@@ -1,43 +1,14 @@
 import path from 'path';
-import { pathToFileURL } from 'url';
-import { protocol, net, app, BrowserWindow } from 'electron';
+import { app, BrowserWindow } from 'electron';
 
 import { APP_CONFIG } from '../../../app.config';
 
-export function registerCustomProtocol() {
+export function registerDeepLinkProtocol() {
   if (!process.defaultApp) {
     app.setAsDefaultProtocolClient(APP_CONFIG.ELECTRON_PROTOCOL);
   } else if (process.argv.length > 1) {
     app.setAsDefaultProtocolClient(APP_CONFIG.ELECTRON_PROTOCOL, process.execPath, [path.resolve(process.argv[1])]);
   }
-}
-
-export function registerSchema() {
-  protocol.registerSchemesAsPrivileged([
-    {
-      scheme: APP_CONFIG.ELECTRON_PROTOCOL,
-      privileges: {
-        standard: true,
-        secure: true,
-        supportFetchAPI: true,
-      },
-    },
-  ]);
-}
-
-/**
- * Handle everything that's being requested with novaspektr:// protocol
- * every mainWindow.openURL() leads here
- */
-export function registerSchemaHandler() {
-  protocol.handle(APP_CONFIG.ELECTRON_PROTOCOL, async (req) => {
-    const url = new URL(req.url);
-
-    const pathname = path.posix.normalize(url.pathname);
-    const fileUrl = pathToFileURL(path.join(__dirname, pathname === '/' ? 'index.html' : pathname));
-
-    return net.fetch(fileUrl.toString());
-  });
 }
 
 export function processUrl(url: string, mainWindow?: BrowserWindow) {
@@ -46,5 +17,5 @@ export function processUrl(url: string, mainWindow?: BrowserWindow) {
   const parsed = new URL(url);
   if (parsed.protocol !== `${APP_CONFIG.ELECTRON_PROTOCOL}:`) return;
 
-  mainWindow.loadURL(parsed.href);
+  mainWindow.loadURL('file://' + __dirname + '/index.html' + parsed.search);
 }
--- src/main/factories/window.ts
+++ src/main/factories/window.ts
@@ -7,7 +7,7 @@ import { APP_CONFIG } from '../../../app.config';
 import { ENVIRONMENT } from '../shared/constants/environment';
 
 export function createWindow(): BrowserWindow {
-  const { MAIN, TITLE, ELECTRON_PROTOCOL } = APP_CONFIG;
+  const { MAIN, TITLE } = APP_CONFIG;
 
   const mainWindowState = windowStateKeeper({
     defaultWidth: MAIN.WINDOW.WIDTH,
@@ -31,7 +31,7 @@ export function createWindow(): BrowserWindow {
     },
   });
 
-  window.loadURL(`${ELECTRON_PROTOCOL}://webapp/`);
+  window.loadURL('file://' + __dirname + '/index.html');
 
   ENVIRONMENT.IS_DEV && window.webContents.openDevTools({ mode: 'bottom' });
 
