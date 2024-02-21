79cc70a5723c4623a1abf03c92b264da840c8019
Yaroslav Grachev <tuulwq@gmail.com>
2024-02-21 17:28:15 +0300

    fix: localhost scheme

--- src/main/shared/constants/environment.ts
+++ src/main/shared/constants/environment.ts
@@ -1,5 +1,4 @@
 export const ENVIRONMENT = {
   IS_DEV: process.env.NODE_ENV === 'development',
   IS_STAGE: process.env.NODE_ENV === 'stage',
-  IS_FORCE_ELECTRON: process.env.FORCE_ELECTRON === 'true',
 };
