81a277dd722e81c822f71a273c0bd557d36e31e1
Thibaut Sardan <github@thib.top>
2024-03-04 10:48:25 +0000

    fix: await allsettled, duplicate scope, remove allInitiated

--- src/renderer/features/contacts/ContactFilter/model/__tests__/contact-filter.test.tsx
+++ src/renderer/features/contacts/ContactFilter/model/__tests__/contact-filter.test.tsx
@@ -3,60 +3,72 @@ import { allSettled, fork } from 'effector';
 import { filterModel } from '../contact-filter';
 import { contactModel } from '@entities/contact';
 
-const mockContact1 = {
-  id: 1,
-  name: 'name 1',
-  address: '222',
-  accountId: '0x333',
-  matrixId: '@444',
-};
-
-const mockContact2 = {
-  id: 5,
-  name: 'name 6',
-  address: '777',
-  accountId: '0x888',
-  matrixId: '@999',
-};
+const contacts = [
+  {
+    id: 1,
+    name: 'name 1',
+    address: '222',
+    accountId: '0x333',
+    matrixId: '@444',
+  },
+  {
+    id: 5,
+    name: 'name 6',
+    address: '777',
+    accountId: '0x888',
+    matrixId: '@999',
+  },
+];
 
 describe('features/contacts/model/contact-filter-model', () => {
-  const scope = fork({
-    values: new Map().set(contactModel.$contacts, [mockContact1, mockContact2]),
-  });
-
   test('should return all contacts if no search query', async () => {
-    allSettled(filterModel.events.formInitiated, { scope });
+    const scope = fork({
+      values: new Map().set(contactModel.$contacts, contacts),
+    });
+
     expect(scope.getState(filterModel.$contactsFiltered)).toHaveLength(2);
   });
 
   test('should return nothing if there is no match', async () => {
-    allSettled(filterModel.events.formInitiated, { scope });
-    allSettled(filterModel.events.queryChanged, { scope, params: 'nothing' });
+    const scope = fork({
+      values: new Map().set(contactModel.$contacts, contacts),
+    });
+
+    await allSettled(filterModel.events.queryChanged, { scope, params: 'nothing' });
 
     expect(scope.getState(filterModel.$contactsFiltered)).toHaveLength(0);
   });
 
   test('should search by name', async () => {
-    allSettled(filterModel.events.formInitiated, { scope });
-    allSettled(filterModel.events.queryChanged, { scope, params: mockContact1.name });
+    const scope = fork({
+      values: new Map().set(contactModel.$contacts, contacts),
+    });
+
+    await allSettled(filterModel.events.queryChanged, { scope, params: contacts[0].name });
 
     expect(scope.getState(filterModel.$contactsFiltered)).toHaveLength(1);
-    expect(scope.getState(filterModel.$contactsFiltered)[0].id).toBe(mockContact1.id);
+    expect(scope.getState(filterModel.$contactsFiltered)[0].id).toEqual(contacts[0].id);
   });
 
   test('should search by address', async () => {
-    allSettled(filterModel.events.formInitiated, { scope });
-    allSettled(filterModel.events.queryChanged, { scope, params: mockContact1.address });
+    const scope = fork({
+      values: new Map().set(contactModel.$contacts, contacts),
+    });
+
+    await allSettled(filterModel.events.queryChanged, { scope, params: contacts[0].address });
 
     expect(scope.getState(filterModel.$contactsFiltered)).toHaveLength(1);
-    expect(scope.getState(filterModel.$contactsFiltered)[0].id).toBe(mockContact1.id);
+    expect(scope.getState(filterModel.$contactsFiltered)[0].id).toEqual(contacts[0].id);
   });
 
   test('should search by matrix id', async () => {
-    allSettled(filterModel.events.formInitiated, { scope });
-    allSettled(filterModel.events.queryChanged, { scope, params: mockContact2.matrixId });
+    const scope = fork({
+      values: new Map().set(contactModel.$contacts, contacts),
+    });
+
+    await allSettled(filterModel.events.queryChanged, { scope, params: contacts[1].matrixId });
 
     expect(scope.getState(filterModel.$contactsFiltered)).toHaveLength(1);
-    expect(scope.getState(filterModel.$contactsFiltered)[0].id).toBe(mockContact2.id);
+    expect(scope.getState(filterModel.$contactsFiltered)[0].id).toEqual(contacts[1].id);
   });
 });
