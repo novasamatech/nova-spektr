09922f911948da7a66ad467eb1f610a8bb9333f3
Yaroslav Grachev <tuulwq@gmail.com>
2024-04-03 13:44:23 +0300

    chore: test commit

--- src/renderer/features/network/NetworkSelector/index.ts
+++ src/renderer/features/network/NetworkSelector/index.ts
@@ -1,3 +1,4 @@
 export { NetworkSelector } from './ui/NetworkSelector';
+export type { ConnectionOptions, SelectorPayload } from './lib/types';
 export { networkSelectorUtils } from './lib/network-selector-utils';
 export { networkSelectorModel } from './model/network-selector-model';
