e1beacf042038f29aa78cf5eaf2c2977f173b4ed
Yaroslav Grachev <tuulwq@gmail.com>
2024-04-09 09:42:34 +0300

    chore: linter

--- src/renderer/pages/index.tsx
+++ src/renderer/pages/index.tsx
@@ -8,7 +8,7 @@ import { Operations } from './Operations/Operations';
 import { Notifications } from './Notifications/Notifications';
 import { Contacts, CreateContact, EditContact } from './AddressBook';
 import { Overview as Settings, Matrix, Currency, Networks } from './Settings';
-import { Overview as Staking, ChangeValidators,  Restake, Destination, StakeMore } from './Staking';
+import { Overview as Staking, ChangeValidators, Restake, Destination, StakeMore } from './Staking';
 
 // React routes v6 hint:
 // https://github.com/remix-run/react-router/blob/main/docs/upgrading/v5.md#use-useroutes-instead-of-react-router-config
