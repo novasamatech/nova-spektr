ab7a2c06ffa42a40a65e192574aca6113d37cd2e
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-04-22 17:05:04 +0300

    fix: change proxy-worker to entries

--- src/renderer/features/proxies/workers/proxy-worker.ts
+++ src/renderer/features/proxies/workers/proxy-worker.ts
@@ -129,15 +129,13 @@ async function getProxies({
   }
 
   try {
-    const keys = await api.query.proxy.proxies.keys();
-
-    const proxiesRequests = keys.map(async (key) => {
+    const entries = await api.query.proxy.proxies.entries();
+    entries.forEach(([key, value]) => {
       try {
-        const proxyData = (await api.rpc.state.queryStorageAt([key])) as any;
-
+        const proxyData = value.toHuman() as any;
         const proxiedAccountId = key.args[0].toHex();
 
-        proxyData[0][0].toHuman().forEach((account: any) => {
+        proxyData[0].forEach((account: any) => {
           const newProxy: NoID<ProxyAccount> = {
             chainId,
             proxiedAccountId,
@@ -171,11 +169,11 @@ async function getProxies({
           }
 
           if (needToAddProxiedAccount) {
-            deposits.deposits[proxiedAccountId] = proxyData[0][1].toHuman();
+            deposits.deposits[proxiedAccountId] = proxyData[1];
           }
         });
 
-        proxyData[0][0].toHuman().forEach((account: any) => {
+        proxyData[0].forEach((account: any) => {
           const newProxy: NoID<ProxyAccount> = {
             chainId,
             proxiedAccountId,
@@ -199,7 +197,7 @@ async function getProxies({
           }
 
           if (needToAddProxyAccount) {
-            deposits.deposits[proxiedAccountId] = proxyData[0][1].toHuman();
+            deposits.deposits[proxiedAccountId] = proxyData[1];
           }
         });
       } catch (e) {
@@ -207,7 +205,6 @@ async function getProxies({
       }
     });
 
-    await Promise.all(proxiesRequests);
   } catch (e) {
     console.log(`proxy-worker ${api.genesisHash}: error in getProxies`, e);
   }
