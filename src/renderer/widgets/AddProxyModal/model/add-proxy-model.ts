c404332ba1ff0aece478861d4b8e27bfc2aad972
sokolova-an <dev.anastasis@gmail.com>
2024-04-23 11:36:39 +0200

    fix: proxy worker trigger

--- src/renderer/widgets/AddProxyModal/model/add-proxy-model.ts
+++ src/renderer/widgets/AddProxyModal/model/add-proxy-model.ts
@@ -5,6 +5,7 @@ import { Transaction } from '@entities/transaction';
 import { signModel } from '@features/operations/OperationSign/model/sign-model';
 import { submitModel } from '@features/operations/OperationSubmit';
 import { walletSelectModel } from '@features/wallets';
+import { proxiesModel } from '@features/proxies';
 import { walletModel } from '@entities/wallet';
 import { balanceSubModel } from '@features/balances';
 import { Step, AddProxyStore } from '../lib/types';
@@ -132,6 +133,8 @@ sample({
 
 sample({
   clock: delay(submitModel.output.formSubmitted, 2000),
+  source: $step,
+  filter: (step) => step === Step.SUBMIT,
   target: flowFinished,
 });
 
@@ -148,6 +151,11 @@ sample({
   },
   fn: ({ walletDetails }) => walletDetails!,
   target: balanceSubModel.events.walletToUnsubSet,
+});
+
+sample({
+  clock: flowFinished,
+  target: proxiesModel.events.workerStarted,
 });
 
 sample({
