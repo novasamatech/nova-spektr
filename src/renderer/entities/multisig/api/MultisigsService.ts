d599b2b20c06586969d00964fae30618d3438ecb
Thibaut Sardan <github@thib.top>
2024-04-30 17:15:58 +0100

    feat: multisig query

---
+++ src/renderer/entities/multisig/api/MultisigsService.ts
@@ -0,0 +1,26 @@
+import { GraphQLClient } from 'graphql-request';
+
+import { FILTER_MULTISIG_ACCOUNT_IDS } from './graphql/queries/multisigs';
+import type { AccountId } from '@shared/core';
+
+export const multisigService = {
+  filterMultisigsAccountIds,
+};
+
+export type MultisigResult = {
+  accountId: AccountId;
+  threshold: number;
+  signatories: AccountId[];
+};
+
+async function filterMultisigsAccountIds(client: GraphQLClient, accountIds: AccountId[]): Promise<MultisigResult[]> {
+  const data = await client.request(FILTER_MULTISIG_ACCOUNT_IDS, { accountIds });
+
+  const filteredMultisigIds = (data as any)?.accounts?.nodes?.map(({ id, threshold, signatories }: any) => ({
+    accountId: id,
+    threshold,
+    signatories: signatories.nodes.map(({ signatory }: any) => signatory.id),
+  }));
+
+  return filteredMultisigIds || [];
+}
