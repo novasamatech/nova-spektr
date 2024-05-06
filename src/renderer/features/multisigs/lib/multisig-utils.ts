4c92ecfc0c7591d907cfcf5053d6c337907a1e6d
Thibaut Sardan <github@thib.top>
2024-05-06 12:07:57 +0100

    chore: cleanup

--- src/renderer/features/multisigs/lib/multisig-utils.ts
+++
@@ -1,9 +0,0 @@
-import { Chain, ChainOptions } from '@shared/core';
-
-export const multisigUtils = {
-  isMultisigSupported,
-};
-
-function isMultisigSupported(chain: Chain): boolean {
-  return Boolean(chain.options?.includes(ChainOptions.MULTISIG));
-}
