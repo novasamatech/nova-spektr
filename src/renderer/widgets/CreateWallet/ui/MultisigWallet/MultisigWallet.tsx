7f1686b2265210ec520a55d1b2af401c148bf2c9
Thibaut Sardan <github@thib.top>
2024-05-29 11:38:39 +0200

    chore: description back

--- src/renderer/widgets/CreateWallet/ui/MultisigWallet/MultisigWallet.tsx
+++ src/renderer/widgets/CreateWallet/ui/MultisigWallet/MultisigWallet.tsx
@@ -1,12 +1,11 @@
 import { useForm } from 'effector-forms';
-import { ComponentProps, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useUnit } from 'effector-react';
 import noop from 'lodash/noop';
 
-import { BaseModal, HeaderTitleText, Button, IconButton } from '@shared/ui';
+import { BaseModal, HeaderTitleText, IconButton } from '@shared/ui';
 import { useI18n } from '@app/providers';
 import { useToggle } from '@shared/lib/hooks';
-import { OperationResult } from '@entities/transaction';
 import { ConfirmationStep, NameThresholdStep } from './components';
 import { DEFAULT_TRANSITION } from '@shared/lib/utils';
 import { flowModel } from '../../model/flow-model';
@@ -16,7 +15,7 @@ import { SelectSignatoriesForm } from './components/SelectSignatoriesForm';
 import { Step } from '../../lib/types';
 import { OperationSign } from '@features/operations';
 
-type OperationResultProps = Pick<ComponentProps<typeof OperationResult>, 'variant' | 'description'>;
+// type OperationResultProps = Pick<ComponentProps<typeof OperationResult>, 'variant' | 'description'>;
 
 type Props = {
   isOpen: boolean;
@@ -31,8 +30,8 @@ export const MultisigWallet = ({ isOpen, onClose, onComplete }: Props) => {
   const {
     fields: { chain },
   } = useForm(formModel.$createMultisigForm);
-  const isLoading = useUnit(flowModel.$isLoading);
-  const error = useUnit(flowModel.$error);
+  // const isLoading = useUnit(flowModel.$isLoading);
+  // const error = useUnit(flowModel.$error);
   const activeStep = useUnit(flowModel.$step);
   const accountSignatories = useUnit(formModel.$accountSignatories);
   const contactSignatories = useUnit(formModel.$contactSignatories);
@@ -61,12 +60,12 @@ export const MultisigWallet = ({ isOpen, onClose, onComplete }: Props) => {
     setTimeout(params?.complete ? onComplete : params?.closeAll ? onClose : noop, DEFAULT_TRANSITION);
   };
 
-  const getResultProps = (): OperationResultProps => {
-    if (isLoading) return { variant: 'loading' };
-    if (error) return { variant: 'error', description: error };
+  // const getResultProps = (): OperationResultProps => {
+  //   if (isLoading) return { variant: 'loading' };
+  //   if (error) return { variant: 'error', description: error };
 
-    return { variant: 'success', description: t('createMultisigAccount.successMessage') };
-  };
+  //   return { variant: 'success', description: t('createMultisigAccount.successMessage') };
+  // };
 
   const modalTitle = (
     <div className="flex justify-between items-center px-5 py-3 w-[464px] bg-white rounded-tl-lg rounded-tr-lg">
@@ -95,14 +94,14 @@ export const MultisigWallet = ({ isOpen, onClose, onComplete }: Props) => {
           <OperationSign onGoBack={() => flowModel.events.stepChanged(Step.CONFIRM)} />
         )}
 
-        <OperationResult
+        {/* <OperationResult
           {...getResultProps()}
           title={t('createMultisigAccount.createionStatusTitle')}
           isOpen={isModalOpen && isResultModalOpen}
           onClose={() => closeMultisigModal({ complete: true })}
         >
           {error && <Button onClick={toggleResultModal}>{t('createMultisigAccount.closeButton')}</Button>}
-        </OperationResult>
+        </OperationResult> */}
         <IconButton
           name="close"
           size={20}
