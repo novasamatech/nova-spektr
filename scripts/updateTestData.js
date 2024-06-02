93fcec8ffe5001c0dbf0e4acb6af0b9bdd8578b3
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-06-02 18:59:11 +0300

    fix: format

--- scripts/updateTestData.js
+++ scripts/updateTestData.js
@@ -6,9 +6,12 @@ const updateChainsList = () => {
   const chainsListPath = path.resolve(__dirname, '../tests/system/data/chains/chainsList.ts');
 
   const chainsJson = JSON.parse(fs.readFileSync(chainsJsonPath, 'utf-8'));
-  const chainNames = chainsJson.map(chain => ({ name: chain.name }));
+  const chainNames = chainsJson.map((chain) => ({ name: chain.name }));
 
-  const chainsListContent = `export const chainsList = ${JSON.stringify(chainNames, null, 2)};`;
+  const chainsListContent = `export const chainsList = ${JSON.stringify(chainNames, null, 2)
+    .replace(/"name":/g, 'name:')
+    .replace(/"/g, "'")
+    .replace(/,\n\s+}/g, '\n  }')};`;
 
   fs.writeFileSync(chainsListPath, chainsListContent, 'utf-8');
   console.log('chainsList.ts has been updated.');
