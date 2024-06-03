5c2392b177cd1e0a82068cbe3f26fa04e4ede3e2
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2024-06-03 12:58:18 +0300

    feat: add db interaction in system tests (#1824)
    * feat: add db interaction in system tests
    
    fix: update ci for system tests
    
    fix: test run
    
    fix: report style
    
    fix: report status
    
    fix: report status
    
    fix: report status
    
    fix: run command
    
    fix: adding waiting until fully load
    
    fix: add cache for playwright
    
    fix: run in CI
    
    fix: change button approach
    
    ci: temp remove telegram notific
    
    fix: playwright version parsing
    
    fix: wait loading the page
    
    fix: echo command
    
    fix: run tests
    
    fix: increase timeout
    
    fix: database interaction timeout
    
    fix: remove filter undefined
    
    fix: makr test as a slow
    
    * feat: fee loading test
    
    * fix: refactoring
    
    * fix: format
    
    * fix: format
    
    * fix: split substrate and eth chains
    
    * fix: linter
    
    * fix: linter
    
    * fix: linter
    
    * feat: adding tags for tests
    
    * fix: revert update playwright
    
    * fix: upgrade playwright
    
    * fix: playwright path
    
    * fix: remove redundant selector
    
    * fix: function name
    
    * fix: playwright path
    
    * fix: lint
    
    * fix: run_type_info

---
+++ tests/system/data/db/dynamicDerivations/dynamicDerivationsWallets.ts
@@ -0,0 +1,38 @@
+import { IndexedDBData } from '../../../utils/interactWithDatabase';
+
+export const vaultDPPolkadotTestWallet: IndexedDBData = {
+  database: 'spektr',
+  table: 'wallets',
+  injectingData: [
+    { id: 1, isActive: true, name: 'vaultDynamicDerivationsPolkadot', signingType: 'signing_pv', type: 'wallet_pv' },
+  ],
+};
+
+export const vaultDPPolkadotTestAccount: IndexedDBData = {
+  database: 'spektr',
+  table: 'accounts',
+  injectingData: [
+    {
+      id: 1,
+      accountId: '0x7a28037947ecebe0dd86dc0e910911cb33185fd0714b37b75943f67dcf9b6e7c',
+      chainType: 0,
+      cryptoType: 0,
+      name: '',
+      type: 'base',
+      walletId: 1,
+    },
+    {
+      accountId: '0x2a8f1ce5d56835b49e439e447a38aa1f22295d4093df756cab5e5bb39bd9f563',
+      baseId: 1,
+      chainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+      chainType: 0,
+      cryptoType: 0,
+      derivationPath: '//polkadot',
+      id: 2,
+      keyType: 'main',
+      name: 'Main',
+      type: 'chain',
+      walletId: 1,
+    },
+  ],
+};
---
+++ tests/system/tests/assets/load.fee.test.ts
@@ -0,0 +1,42 @@
+import { chromium, Browser, Page, BrowserContext } from 'playwright';
+import { test } from '@playwright/test';
+
+import { BaseLoginPage } from '../../pages/loginPage/BaseLoginPage';
+import { LoginPageElements } from '../../pages/_elements/LoginPageElements';
+import { substrateChains } from '../../data/chains/chainsList';
+import { VaultAssetsPage } from '../../pages/assetsPage/VaultAssetsPage';
+
+test.describe(
+  'Load Transfer fee as Substrate Polkadot Vault',
+  {
+    tag: '@fee-test',
+  },
+  () => {
+    let browser: Browser;
+    let context: BrowserContext;
+    let page: Page;
+    let loginPage: BaseLoginPage;
+    let vaultWallet: VaultAssetsPage;
+
+    test.beforeAll(async () => {
+      browser = await chromium.launch();
+      context = await browser.newContext({ ignoreHTTPSErrors: true, permissions: [] });
+      page = await context.newPage();
+      loginPage = new BaseLoginPage(page, new LoginPageElements());
+      await loginPage.gotoOnboarding();
+      vaultWallet = await loginPage.createVaultSubstrateWallet();
+    });
+
+    test.afterAll(async () => {
+      await browser.close();
+    });
+
+    substrateChains.forEach((chain) => {
+      test(`Can load fee for ${chain.name}`, async () => {
+        test.slow();
+        const assetsPage = await vaultWallet.gotoMain();
+        await assetsPage.checkTransferFee(chain);
+      });
+    });
+  },
+);
