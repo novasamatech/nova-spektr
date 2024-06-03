1dbee21a0a988f2c5d1c5fd3da66e2c62c619cdf
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2024-06-03 12:58:18 +0300

    feat: add db interaction in system tests (#1824)
    * feat: add db interaction in system tests
    
    fix: update ci for system tests
    
    fix: test run
    
    fix: report style
    
    fix: report status
    
    fix: report status
    
    fix: report status
    
    fix: run command
    
    fix: adding waiting until fully load
    
    fix: add cache for playwright
    
    fix: run in CI
    
    fix: change button approach
    
    ci: temp remove telegram notific
    
    fix: playwright version parsing
    
    fix: wait loading the page
    
    fix: echo command
    
    fix: run tests
    
    fix: increase timeout
    
    fix: database interaction timeout
    
    fix: remove filter undefined
    
    fix: makr test as a slow
    
    * feat: fee loading test
    
    * fix: refactoring
    
    * fix: format
    
    * fix: format
    
    * fix: split substrate and eth chains
    
    * fix: linter
    
    * fix: linter
    
    * fix: linter
    
    * feat: adding tags for tests
    
    * fix: revert update playwright
    
    * fix: upgrade playwright
    
    * fix: playwright path
    
    * fix: remove redundant selector
    
    * fix: function name
    
    * fix: playwright path
    
    * fix: lint
    
    * fix: run_type_info

---
+++ tests/system/utils/interactWithDatabase.ts
@@ -0,0 +1,38 @@
+import { Page } from 'playwright';
+
+export interface IndexedDBData {
+  database: string;
+  table: string;
+  injectingData: any[];
+}
+
+/**
+ * Injects data into an IndexedDB database within a given page context.
+ *
+ * @param {Page} page - The Playwright page context in which to operate.
+ * @param {IndexedDBData} data - The data to be injected, including database name, table name, and the data itself.
+ * @returns {Promise<void>} A promise that resolves when the data has been successfully injected.
+ */
+export async function injectDataInDatabase(page: Page, data: IndexedDBData): Promise<void> {
+  await page.evaluate(async (data) => {
+    if (!('indexedDB' in window)) {
+      console.log("This browser doesn't support IndexedDB.");
+    } else {
+      const { database, table, injectingData } = data;
+      const dbPromise = window.indexedDB.open(database);
+
+      while (dbPromise.readyState == 'pending') {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1_000);
+        });
+        console.log('waiting');
+      }
+      const tx = dbPromise.result.transaction(table, 'readwrite');
+      console.log(tx);
+      const store = tx.objectStore(table);
+      injectingData.forEach((item) => {
+        store.put(item);
+      });
+    }
+  }, data);
+}
