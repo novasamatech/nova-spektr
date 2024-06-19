ad4ec1278626a1df986ac8d77a6527d418d75f55
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-06-19 10:34:32 +0300

    feat: adding tests for text parser

---
+++ src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-utils-text.test.ts
@@ -0,0 +1,106 @@
+import { importKeysUtils } from '../import-keys-utils';
+
+describe('entities/dynamicDerivations/import-keys-utils/parseTextFile', () => {
+  test('should return null for invalid version', () => {
+    const fileContent = `version: 2\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef`;
+    expect(importKeysUtils.parseTextFile(fileContent)).toBeNull();
+  });
+
+  test('should return null for invalid public address', () => {
+    const fileContent = `version: 1\npublic address: invalid_address`;
+    expect(importKeysUtils.parseTextFile(fileContent)).toBeNull();
+  });
+
+  test('should return null for missing derivation paths', () => {
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef`;
+    expect(importKeysUtils.parseTextFile(fileContent)).toBeNull();
+  });
+
+  test('should parse text file with regular path', () => {
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//path: name [type]`;
+    const parsedData = importKeysUtils.parseTextFile(fileContent);
+    expect(parsedData).toEqual({
+      version: '1',
+      publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+      derivationPaths: [
+        {
+          derivationPath: '//path',
+          name: 'name',
+          type: 'type',
+          chainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+        },
+      ],
+    });
+  });
+
+  test('should parse text file with soft and hard path', () => {
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//hard/soft//hard/soft: name [type]`;
+    const parsedData = importKeysUtils.parseTextFile(fileContent);
+    expect(parsedData).toEqual({
+      version: '1',
+      publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+      derivationPaths: [
+        {
+          derivationPath: '//hard/soft//hard/soft',
+          name: 'name',
+          type: 'type',
+          chainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+        },
+      ],
+    });
+  });
+
+  test('should parse text file with sharded keys', () => {
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//path//0...10: name [type]`;
+    const parsedData = importKeysUtils.parseTextFile(fileContent);
+    expect(parsedData).toEqual({
+      version: '1',
+      publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+      derivationPaths: [
+        {
+          derivationPath: '//path',
+          sharded: '10',
+          name: 'name',
+          type: 'type',
+          chainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+        },
+      ],
+    });
+  });
+
+  test('should parse text file with sharded keys and different derivations', () => {
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//hard/soft//hard//0...10: name [type]`;
+    const parsedData = importKeysUtils.parseTextFile(fileContent);
+    expect(parsedData).toEqual({
+      version: '1',
+      publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+      derivationPaths: [
+        {
+          derivationPath: '//hard/soft//hard',
+          sharded: '10',
+          name: 'name',
+          type: 'type',
+          chainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+        },
+      ],
+    });
+  });
+
+  test('should parse text file with difficult symbols in dp', () => {
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//polkadot//~!@#$%^&*(*)_+QWE'1234567890-=//0...10: name [type]`;
+    const parsedData = importKeysUtils.parseTextFile(fileContent);
+    expect(parsedData).toEqual({
+      version: '1',
+      publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+      derivationPaths: [
+        {
+          derivationPath: `//polkadot//~!@#$%^&*(*)_+QWE'1234567890-=`,
+          sharded: '10',
+          name: 'name',
+          type: 'type',
+          chainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+        },
+      ],
+    });
+  });
+});
