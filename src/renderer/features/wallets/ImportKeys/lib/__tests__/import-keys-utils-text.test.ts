8c421f0b416c27be884f413d5f5123ca4643b2a6
Stepan Lavrentev <lawrentievsv@gmail.com>
2024-06-19 11:08:40 +0300

    fix: soft \ hard naming

--- src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-utils-text.test.ts
+++ src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-utils-text.test.ts
@@ -51,14 +51,14 @@ describe('entities/dynamicDerivations/import-keys-utils/parseTextFile', () => {
   });
 
   test('should parse text file with soft path and shards', () => {
-    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//soft_path//0...10: name [type]`;
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n/soft_path//0...10: name [type]`;
     const parsedData = importKeysUtils.parseTextFile(fileContent);
     expect(parsedData).toEqual({
       version: '1',
       publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
       derivationPaths: [
         {
-          derivationPath: '//soft_path',
+          derivationPath: '/soft_path',
           sharded: '10',
           name: 'name',
           type: 'type',
@@ -69,14 +69,14 @@ describe('entities/dynamicDerivations/import-keys-utils/parseTextFile', () => {
   });
 
   test('should parse text file with hard path and shards', () => {
-    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n/hard_path//0...10: name [type]`;
+    const fileContent = `version: 1\npublic address: 0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef\ngenesis: 0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3\n//hard_path//0...10: name [type]`;
     const parsedData = importKeysUtils.parseTextFile(fileContent);
     expect(parsedData).toEqual({
       version: '1',
       publicAddress: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
       derivationPaths: [
         {
-          derivationPath: '/hard_path',
+          derivationPath: '//hard_path',
           sharded: '10',
           name: 'name',
           type: 'type',
