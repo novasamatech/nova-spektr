fe513b2a169019be27e39515b69a04d414c31ad1
sokolova-an <dev.anastasis@gmail.com>
2024-07-02 12:38:28 +0200

    feat: update asset empty state

--- src/renderer/entities/asset/ui/index.ts
+++ src/renderer/entities/asset/ui/index.ts
@@ -2,3 +2,4 @@ export { AssetBalance } from './AssetBalance/AssetBalance';
 export { AssetDetails } from './AssetDetails/AssetDetails';
 export { AssetIcon } from './AssetIcon/AssetIcon';
 export { AssetLinks } from './AssetLinks/AssetLinks';
+export { EmptyAssetsState } from './EmptyAssetsState/EmptyAssetsState';
