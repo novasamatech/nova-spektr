e63381963ff5ddbdbf566afc4c333ac42797bcfe
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-07-10 20:09:37 +0300

    feat: added vote dialog placeholder

--- src/renderer/shared/ui/Typography/common/types.ts
+++ src/renderer/shared/ui/Typography/common/types.ts
@@ -1,6 +1,20 @@
 import { PropsWithChildren } from 'react';
 
-export type Tags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p' | 'span' | 'small' | 'em' | 'strong' | 'dt' | 'dd' | 'li';
+export type Tags =
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'p'
+  | 'span'
+  | 'small'
+  | 'em'
+  | 'strong'
+  | 'dt'
+  | 'dd'
+  | 'li'
+  | 'div';
 
 export type Align = 'left' | 'right' | 'center' | 'balance';
 
