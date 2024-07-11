553873cc4a3fb0819831220f3d9456bf540d0cb0
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-07-11 11:56:25 +0300

    feat: cleanup

--- src/renderer/shared/ui/Typography/common/types.ts
+++ src/renderer/shared/ui/Typography/common/types.ts
@@ -1,20 +1,6 @@
 import { PropsWithChildren } from 'react';
 
-export type Tags =
-  | 'h1'
-  | 'h2'
-  | 'h3'
-  | 'h4'
-  | 'h5'
-  | 'p'
-  | 'span'
-  | 'small'
-  | 'em'
-  | 'strong'
-  | 'dt'
-  | 'dd'
-  | 'li'
-  | 'div';
+export type Tags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p' | 'span' | 'small' | 'em' | 'strong' | 'dt' | 'dd' | 'li';
 
 export type Align = 'left' | 'right' | 'center' | 'balance';
 
