f6ef278f5da396ec483b6fe96a9d6200799f390e
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-07-18 02:52:38 +0300

    chore: up eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -174,6 +174,7 @@ module.exports = {
         '@typescript-eslint/no-empty-interface': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'warn',
         // TODO enable
         '@typescript-eslint/no-unnecessary-type-constraint': 'error',
         // it took around 4 seconds to check this single rule
