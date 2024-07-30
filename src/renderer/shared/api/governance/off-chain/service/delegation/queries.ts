712a47db98cf46a7e3189e9bb5b9c6074747a78c
asmadek <alma2610@ya.ru>
2024-07-30 15:10:52 +0500

    chore: add link to delegation subquery

--- src/renderer/shared/api/governance/off-chain/service/delegation/queries.ts
+++ src/renderer/shared/api/governance/off-chain/service/delegation/queries.ts
@@ -1,5 +1,8 @@
 import { gql } from '@apollo/client';
 
+// Schema may be checked at
+// https://github.com/novasamatech/subquery-governance/blob/master/schema.graphql
+
 export const GET_DELEGATE_LIST = gql`
   query DelegateList($activityStartBlock: Int) {
     delegates {
