b3ebb6703f2476e381ef0e1e22675d7a89cb6e70
asmadek <alma2610@ya.ru>
2024-07-30 15:12:39 +0500

    chore: add comment

--- src/renderer/shared/api/governance/off-chain/service/delegation/queries.ts
+++ src/renderer/shared/api/governance/off-chain/service/delegation/queries.ts
@@ -2,6 +2,7 @@ import { gql } from '@apollo/client';
 
 // Schema may be checked at
 // https://github.com/novasamatech/subquery-governance/blob/master/schema.graphql
+// TODO: Add generated types
 
 export const GET_DELEGATE_LIST = gql`
   query DelegateList($activityStartBlock: Int) {
