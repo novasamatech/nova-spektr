031e439b8a2c956a5ddbd9e90857059cf6c0288a
asmadek <alma2610@ya.ru>
2024-07-30 14:28:42 +0500

    chore: fix typo

--- src/renderer/features/governance/aggregates/delegation.ts
+++ src/renderer/features/governance/aggregates/delegation.ts
@@ -15,7 +15,7 @@ const $totalDelegations = combine(
       .reduce((acc, value) => {
         const voting = Object.values(value).find(votingService.isDelegating);
 
-        return voting ? acc.iadd(voting.delegating.balance) : acc;
+        return voting ? acc.add(voting.delegating.balance) : acc;
       }, new BN(0))
       .toString();
   },
