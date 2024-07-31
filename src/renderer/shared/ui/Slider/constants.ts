285a866ace2a82902e4c1017492b05e98a657e6f
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2024-07-31 15:04:28 +0300

    feat: implemented Slider component (#2006)
    * feat: implement Slider component
    
    ---------
    
    Co-authored-by: Sergey Zhuravlev <s.zhuravlev@elama.ru>

---
+++ src/renderer/shared/ui/Slider/constants.ts
@@ -0,0 +1,1 @@
+export const maxIndicationDensity = 10;
---
+++ src/renderer/shared/ui/Slider/helpers.ts
@@ -0,0 +1,6 @@
+export const countSteps = (min: number, max: number, stepSize: number) => {
+  const steps = max - min;
+  const remainder = stepSize ? steps % stepSize : 0;
+
+  return remainder ? steps : steps / stepSize + 1;
+};
