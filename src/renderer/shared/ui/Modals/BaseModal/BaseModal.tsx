b768027aac8ee76efe71165e1d2f2aca17f74c4c
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-08-08 14:28:29 +0300

    fix: change BaseModal implementation to radix Dialog

--- src/renderer/shared/ui/Modals/BaseModal/BaseModal.tsx
+++ src/renderer/shared/ui/Modals/BaseModal/BaseModal.tsx
@@ -1,11 +1,9 @@
-import { Dialog, Transition } from '@headlessui/react';
-import { Fragment, type PropsWithChildren, type ReactNode } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+import { type PropsWithChildren, type ReactNode } from 'react';
 
 import { cnTw } from '@shared/lib/utils';
 import { IconButton } from '../../Buttons';
 import { HeaderTitleText } from '../../Typography';
-import { ModalBackdrop } from '../common/ModalBackdrop';
-import { ModalTransition } from '../common/ModalTransition';
 
 type Props = {
   isOpen: boolean;
@@ -36,40 +34,51 @@ export const BaseModal = ({
   const headerExist = title || actionButton || closeButton;
 
   return (
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className={cnTw('relative', zIndex)} onClose={() => onClose()}>
-        <ModalBackdrop />
-
-        <div className="fixed inset-0 overflow-hidden flex min-h-full items-center justify-center p-4">
-          <ModalTransition>
-            <Dialog.Panel
-              style={panelStyle}
-              className={cnTw(
-                'transform rounded-lg bg-white text-left align-middle shadow-modal transition-all w-[440px]',
-                panelClass,
-              )}
-            >
-              {headerExist && (
+    <Dialog.Root open={isOpen} onOpenChange={(open) => !open && onClose()}>
+      <Dialog.Portal>
+        <Dialog.Overlay
+          className={cnTw(
+            'fixed inset-0 overflow-hidden flex min-h-full items-center justify-center p-4 ',
+            'bg-dim-background',
+            'animate-in fade-in duration-300',
+            zIndex,
+          )}
+        >
+          <Dialog.Content
+            style={panelStyle}
+            className={cnTw(
+              'transform rounded-lg bg-white text-left align-middle shadow-modal transition-all w-[440px]',
+              'animate-in fade-in zoom-in-95 duration-300',
+              panelClass,
+            )}
+          >
+            {headerExist && (
+              <Dialog.Title asChild>
                 <header className={cnTw('flex items-center justify-between', headerClass)}>
                   {title && typeof title === 'string' && (
-                    <Dialog.Title as={HeaderTitleText} className="text-text-primary font-bold truncate py-1">
-                      {title}
-                    </Dialog.Title>
+                    <HeaderTitleText className="text-text-primary font-bold truncate py-1">{title}</HeaderTitleText>
                   )}
 
                   {title && typeof title !== 'string' && title}
+
                   <div className="flex items-center gap-x-4 h-7.5 z-20">
                     {actionButton}
-                    {closeButton && <IconButton name="close" size={20} className="m-1" onClick={() => onClose()} />}
+
+                    {closeButton && (
+                      <Dialog.Close asChild>
+                        <IconButton name="close" size={20} className="m-1" onClick={() => onClose()} />
+                      </Dialog.Close>
+                    )}
                   </div>
                 </header>
-              )}
-
+              </Dialog.Title>
+            )}
+            <Dialog.Description asChild>
               <section className={contentClass}>{children}</section>
-            </Dialog.Panel>
-          </ModalTransition>
-        </div>
-      </Dialog>
-    </Transition>
+            </Dialog.Description>
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
