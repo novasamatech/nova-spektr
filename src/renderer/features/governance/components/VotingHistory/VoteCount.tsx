050602cd4458e7015eb03aafd2067ee49e213347
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2024-08-09 15:33:10 +0300

    fix: hide vote counter until history loaded
    Co-authored-by: Sergey Zhuravlev <s.zhuravlev@elama.ru>

---
+++ src/renderer/features/governance/components/VotingHistory/VoteCount.tsx
@@ -0,0 +1,18 @@
+import { FootnoteText, Shimmering } from '@shared/ui';
+
+type Props = {
+  count: number;
+  loading: boolean;
+};
+
+export const VoteCount = ({ count, loading }: Props) => {
+  if (loading) {
+    return <Shimmering height="1em" width="1ch" />;
+  }
+
+  return (
+    <FootnoteText as="span" className="text-text-tertiary">
+      {count}
+    </FootnoteText>
+  );
+};
