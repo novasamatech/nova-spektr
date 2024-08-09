63fd6b7e6cfa2d9d00fbdb1abd20155f3f326edc
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-08-09 15:20:58 +0300

    fix: hide vote counter until history loaded

--- src/renderer/shared/ui/Shimmering/Shimmering.tsx
+++ src/renderer/shared/ui/Shimmering/Shimmering.tsx
@@ -1,18 +1,25 @@
+import { isNumber } from 'lodash';
+
 import { cnTw } from '@shared/lib/utils';
 
 import './Shimmering.css';
 
 type Props = {
-  width?: number;
-  height?: number;
+  width?: number | string;
+  height?: number | string;
   circle?: boolean;
   className?: string;
 };
 
-export const Shimmering = ({ width, height, circle, className }: Props) => (
-  <span
-    className={cnTw('h-full w-full block spektr-shimmer', circle ? 'rounded-full' : 'rounded-[10px]', className)}
-    style={{ width: `${width}px`, height: `${circle ? width : height}px` }}
-    data-testid="shimmer"
-  />
-);
+export const Shimmering = ({ width, height, circle, className }: Props) => {
+  const formattedWidth = isNumber(width) ? `${width}px` : width;
+  const formattedHeight = isNumber(height) ? `${height}px` : height;
+
+  return (
+    <span
+      className={cnTw('h-full w-full block spektr-shimmer', circle ? 'rounded-full' : 'rounded-[10px]', className)}
+      style={{ width: formattedWidth, height: circle ? formattedWidth : formattedHeight }}
+      data-testid="shimmer"
+    />
+  );
+};
