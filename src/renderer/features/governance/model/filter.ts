adb7596e46e36e3b10ee6cd11c956ac5811f7837
Anastasiia Sokolova <63446892+sokolova-an@users.noreply.github.com>
2024-08-19 10:23:46 +0200

    feat: add clear all button

--- src/renderer/features/governance/model/filter.ts
+++ src/renderer/features/governance/model/filter.ts
@@ -1,4 +1,4 @@
-import { createEvent, createStore, restore, sample } from 'effector';
+import { combine, createEvent, createStore, restore, sample } from 'effector';
 import { debounce } from 'patronum';
 
 import { type DropdownResult } from '@shared/ui/types';
@@ -6,11 +6,16 @@ import { type DropdownResult } from '@shared/ui/types';
 const queryChanged = createEvent<string>();
 const selectedTracksChanged = createEvent<DropdownResult[]>();
 const selectedVoteChanged = createEvent<DropdownResult>();
+const filtersReset = createEvent();
 
 const $selectedTrackIds = createStore<string[]>([]);
 const $selectedVoteId = createStore<string>('');
 const $query = restore<string>(queryChanged, '');
 const $debouncedQuery = restore<string>(debounce(queryChanged, 100), '');
+
+const $isFiltersSelected = combine($selectedTrackIds, $selectedVoteId, (tracks, voteId) => {
+  return tracks.length > 0 || voteId !== '';
+});
 
 sample({
   clock: selectedTracksChanged,
@@ -31,14 +36,22 @@ sample({
   target: $selectedVoteId,
 });
 
+sample({
+  clock: filtersReset,
+  target: [$selectedVoteId.reinit, $selectedTrackIds.reinit],
+});
+
 export const filterModel = {
   $query,
   $debouncedQuery,
   $selectedTrackIds,
   $selectedVoteId,
+  $isFiltersSelected,
+
   events: {
     queryChanged,
     selectedTracksChanged,
     selectedVoteChanged,
+    filtersReset,
   },
 };
