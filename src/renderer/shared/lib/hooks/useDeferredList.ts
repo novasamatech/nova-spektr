e61ac04f51f9d9365b319fb9eae230e7e6f36699
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-08-19 11:22:46 +0300

    fix: save

--- src/renderer/shared/lib/hooks/useDeferredList.ts
+++ src/renderer/shared/lib/hooks/useDeferredList.ts
@@ -5,6 +5,14 @@ type Params<T> = {
   isLoading?: boolean;
 };
 
+/**
+ * Hook solves problem with intermediate state between loading finished and
+ * deferred list rendering finished. This problem appears on large lists.
+ *
+ * @returns {boolean} Field list - deferred data for rendering.
+ * @returns {boolean} Field isLoading - isLoading parameter + delay, introduced
+ *   by deferred rendering.
+ */
 export const useDeferredList = <T>({ list, isLoading }: Params<T>) => {
   const deferred = useDeferredValue(list);
   const isDeferred = deferred.length === 0 && list.length !== 0;
