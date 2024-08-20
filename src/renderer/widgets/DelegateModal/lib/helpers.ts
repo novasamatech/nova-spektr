f527859287b61f141f08d0814e02970596149541
sokolova-an <dev.anastasis@gmail.com>
2024-08-20 09:49:38 +0200

    fix: delegation tracks

--- src/renderer/widgets/DelegateModal/lib/helpers.ts
+++ src/renderer/widgets/DelegateModal/lib/helpers.ts
@@ -1,1 +1,13 @@
-export const helpers = {};
+import { type TFunction } from 'i18next';
+
+import { type Asset } from '@/shared/core';
+
+import { treasurySpendsDescription } from './constants';
+
+export const getTreasuryTrackDescription = (asset: Asset | null, description: string, t: TFunction) => {
+  if (asset && treasurySpendsDescription[description][asset.symbol]) {
+    return t(description, { value: treasurySpendsDescription[description][asset.symbol], asset: asset.symbol });
+  }
+
+  return t(`${description}General`);
+};
