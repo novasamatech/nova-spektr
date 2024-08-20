24920fdfc57901e51673bceaa4deb8b9f9fcaa1d
sokolova-an <dev.anastasis@gmail.com>
2024-08-20 10:06:28 +0200

    fix: review

--- src/renderer/widgets/DelegateModal/lib/helpers.ts
+++ src/renderer/widgets/DelegateModal/lib/helpers.ts
@@ -1,11 +1,12 @@
 import { type TFunction } from 'i18next';
 
 import { type Asset } from '@/shared/core';
+import { nonNullable } from '@shared/lib/utils';
 
 import { treasurySpendsDescription } from './constants';
 
 export const getTreasuryTrackDescription = (asset: Asset | null, description: string, t: TFunction) => {
-  if (asset && treasurySpendsDescription[description][asset.symbol]) {
+  if (nonNullable(asset) && treasurySpendsDescription[description][asset.symbol]) {
     return t(description, { value: treasurySpendsDescription[description][asset.symbol], asset: asset.symbol });
   }
 
