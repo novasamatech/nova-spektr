e7a8ea01fe32934d639b7d21f83adfdc27888e41
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-09-03 12:29:51 +0300

    feat: referenda as multiple pallets

--- src/renderer/shared/pallet/referenda/state.ts
+++ src/renderer/shared/pallet/referenda/state.ts
@@ -3,16 +3,18 @@ import { type ApiPromise } from '@polkadot/api';
 import { type ReferendumId } from '@/shared/core';
 import { pjsSchema } from '@/shared/polkadotjsSchemas';
 
-import { referendaReferendumInfoConvictionVotingTally, trackId } from './schema';
+import { getPalletName } from './helpers';
+import { type TrackId, referendaReferendumInfoConvictionVotingTally, referendumId, trackId } from './schema';
+import { type PalletType } from './types';
 
-const getQuery = (api: ApiPromise, name: string) => {
-  const referenda = api.query['referenda'];
-  if (!referenda) {
-    throw new TypeError(`referenda pallet not found in ${api.runtimeChain.toString()} chain`);
+const getQuery = (type: PalletType, api: ApiPromise, name: string) => {
+  const palletName = getPalletName(type);
+  const pallet = api.query[palletName];
+  if (!pallet) {
+    throw new TypeError(`${palletName} pallet not found in ${api.runtimeChain.toString()} chain`);
   }
 
-  const query = referenda[name];
-
+  const query = pallet[name];
   if (!query) {
     throw new TypeError(`${name} query not found`);
   }
@@ -24,16 +26,16 @@ export const state = {
   /**
    * The number of referenda being decided currently.
    */
-  decidingCount(api: ApiPromise) {
+  decidingCount(type: PalletType, api: ApiPromise) {
     const schema = pjsSchema.vec(pjsSchema.tuppleMap(['track', trackId], ['decidingCount', pjsSchema.u32]));
 
-    return getQuery(api, 'decidingCount').entries().then(schema.parse);
+    return getQuery(type, api, 'decidingCount').entries().then(schema.parse);
   },
 
   /**
    * Information concerning any given referendum.
    */
-  referendumInfoFor(api: ApiPromise, ids?: ReferendumId[]) {
+  referendumInfoFor(type: PalletType, api: ApiPromise, ids?: ReferendumId[]) {
     const schema = pjsSchema.vec(
       pjsSchema.tuppleMap(
         ['id', pjsSchema.storageKey(pjsSchema.u32).transform(keys => keys[0])],
@@ -42,9 +44,29 @@ export const state = {
     );
 
     if (ids) {
-      return getQuery(api, 'referendumInfoFor').entries(ids).then(schema.parse);
+      return getQuery(type, api, 'referendumInfoFor').entries(ids).then(schema.parse);
     } else {
-      return getQuery(api, 'referendumInfoFor').entries().then(schema.parse);
+      return getQuery(type, api, 'referendumInfoFor').entries().then(schema.parse);
     }
+  },
+
+  /**
+   * The next free referendum index, aka the number of referenda started so far.
+   */
+  referendumCount(type: PalletType, api: ApiPromise) {
+    return getQuery(type, api, 'referendumCount')().then(pjsSchema.u32.parse);
+  },
+
+  /**
+   * The sorted list of referenda ready to be decided but not yet being decided,
+   * ordered by conviction-weighted approvals.
+   *
+   * This should be empty if `DecidingCount` is less than
+   * `TrackInfo::max_deciding`.
+   */
+  trackQueue(type: PalletType, api: ApiPromise, track: TrackId) {
+    const schema = pjsSchema.vec(pjsSchema.tuppleMap(['approval', pjsSchema.u32], ['referendum', referendumId]));
+
+    return getQuery(type, api, 'trackQueue')(track).then(schema.parse);
   },
 };
