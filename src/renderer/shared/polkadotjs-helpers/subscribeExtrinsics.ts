935ab0f8b1d723f4b362ffeb148f485d049dfc9c
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-09-17 12:43:49 +0300

    fix: code review

--- src/renderer/shared/polkadotjs-helpers/subscribeExtrinsics.ts
+++ src/renderer/shared/polkadotjs-helpers/subscribeExtrinsics.ts
@@ -7,12 +7,12 @@ type Params = {
 };
 
 export const subscribeExtrinsics = ({ api, name }: Params, fn: (extrinsic: GenericExtrinsic) => unknown) => {
-  const methodsMap = Object.fromEntries(name.map((x) => [x, true]));
+  const methodsMap = new Set(name);
 
   return api.rpc.chain.subscribeAllHeads((header) => {
     api.rpc.chain.getBlock(header.hash).then((body) => {
       for (const extrinsic of body.block.extrinsics) {
-        if (extrinsic.meta.name.toString() in methodsMap) {
+        if (methodsMap.has(extrinsic.meta.name.toString())) {
           fn(extrinsic);
         }
       }
