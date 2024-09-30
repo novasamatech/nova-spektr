e52091651c3afe739543b6831861fad0bd5c3e4e
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2024-09-30 13:49:40 +0300

    fix: replaced custom contacts address implementation with one from ui-entities
    Co-authored-by: Sergey Zhuravlev <s.zhuravlev@elama.ru>

--- src/renderer/entities/contact/ui/ContactRow.tsx
+++ src/renderer/entities/contact/ui/ContactRow.tsx
@@ -1,8 +1,8 @@
 import { type PropsWithChildren } from 'react';
 
 import { type Contact } from '@shared/core';
-import { copyToClipboard } from '@shared/lib/utils';
-import { BodyText, IconButton, Identicon, Plate, Truncate } from '@shared/ui';
+import { BodyText, Plate } from '@shared/ui';
+import { Address } from '@shared/ui-entities';
 
 type Props = {
   contact: Contact;
@@ -11,15 +11,8 @@ type Props = {
 export const ContactRow = ({ contact, children }: PropsWithChildren<Props>) => {
   return (
     <Plate className="grid grid-cols-[250px,250px,1fr] items-center p-0">
-      <div className="flex items-center gap-x-2 p-3">
-        <Identicon address={contact.address} size={20} background={false} />
-        <div className="overflow-hidden">
-          <BodyText className="truncate">{contact.name}</BodyText>
-          <div className="flex items-center gap-1 text-text-tertiary">
-            <Truncate className="text-help-text" ellipsis="..." start={4} end={4} text={contact.address} />
-            <IconButton name="copy" onClick={() => copyToClipboard(contact.address)} />
-          </div>
-        </div>
+      <div className="flex items-center gap-x-2 p-3 text-body">
+        <Address address={contact.address} showIcon iconSize={20} variant="truncate" title={contact.name} />
       </div>
       <BodyText className="truncate p-3 text-text-primary">{contact.matrixId || '-'}</BodyText>
       {children}
