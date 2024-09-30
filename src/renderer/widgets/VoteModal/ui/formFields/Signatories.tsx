0098a8826a50167c34b81aaf07330d8173a64396
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2024-09-30 13:52:04 +0300

    fix: correct calculation of available balance in voting form (#2383)
    Co-authored-by: Sergey Zhuravlev <s.zhuravlev@elama.ru>

--- src/renderer/widgets/VoteModal/ui/formFields/Signatories.tsx
+++ src/renderer/widgets/VoteModal/ui/formFields/Signatories.tsx
@@ -1,13 +1,14 @@
-import { type BN } from '@polkadot/util';
+import { BN_ZERO } from '@polkadot/util';
 import { useMemo } from 'react';
 
-import { useI18n } from '@app/providers';
-import { type Account, type Asset, type Chain } from '@shared/core';
-import { SignatorySelector } from '@entities/operations';
+import { useI18n } from '@/app/providers';
+import { type Account, type Asset, type Balance, type Chain } from '@/shared/core';
+import { SignatorySelector } from '@/entities/operations';
+import { locksService } from '@entities/governance';
 
 type Props = {
   value: Account | null;
-  signatories: { account: Account; balance: BN }[];
+  signatories: { account: Account; balance: Balance | null }[];
   hasError: boolean;
   errorText: string;
   asset: Asset;
@@ -17,14 +18,16 @@ type Props = {
 
 export const Signatories = ({ value, asset, chain, signatories, hasError, errorText, onChange }: Props) => {
   const { t } = useI18n();
-  const fixedSignatories = useMemo(
-    () =>
-      signatories.map(({ account, balance }) => ({
+  const fixedSignatories = useMemo(() => {
+    return signatories.map(({ account, balance }) => {
+      const availableBalance = balance ? locksService.getAvailableBalance(balance) : BN_ZERO;
+
+      return {
         signer: account,
-        balance,
-      })),
-    [signatories],
-  );
+        balance: availableBalance,
+      };
+    });
+  }, [signatories]);
 
   return (
     <SignatorySelector
