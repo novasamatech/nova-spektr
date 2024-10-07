221e9416c7f44e58d6097cd14afed23a90f86d89
Sergey Zhuravlev <s.zhuravlev@elama.ru>
2024-10-07 18:39:15 +0200

    feat: up deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -157,7 +157,7 @@ module.exports = {
       parser: '@typescript-eslint/parser',
       parserOptions: {
         sourceType: 'module',
-        project: './tsconfig.json',
+        projectService: true,
         tsconfigRootDir: __dirname,
         createDefaultProgram: true,
       },
