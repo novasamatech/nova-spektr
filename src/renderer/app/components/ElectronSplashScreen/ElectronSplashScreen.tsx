4885e6483abfef9d253397cb3f035946684221b7
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2024-10-10 00:59:30 +0200

    fix: separated lottie chunk

--- src/renderer/app/components/ElectronSplashScreen/ElectronSplashScreen.tsx
+++ src/renderer/app/components/ElectronSplashScreen/ElectronSplashScreen.tsx
@@ -2,7 +2,7 @@ import logo from './assets/logo.svg';
 
 export const ElectronSplashScreen = () => {
   return (
-    <div className="duration-400 flex h-screen w-screen items-center justify-center animate-in fade-in">
+    <div className="flex h-screen w-screen items-center justify-center duration-500 animate-in fade-in">
       <img src={logo} />
     </div>
   );
--- src/renderer/shared/ui/Animation/Animation.tsx
+++ src/renderer/shared/ui/Animation/Animation.tsx
@@ -1,10 +1,10 @@
-import { useLottie } from 'lottie-react';
-import { useEffect, useState } from 'react';
+import { Suspense, lazy, memo, useEffect, useState } from 'react';
 
-import { Images } from './common/constants';
+import { cnTw } from '@/shared/lib/utils';
+import { Shimmering } from '../Shimmering/Shimmering';
 
 type Props = {
-  variant: keyof typeof Images;
+  variant: 'error' | 'success' | 'loading';
   width?: number;
   height?: number;
   loop?: boolean;
@@ -12,25 +12,62 @@ type Props = {
   className?: string;
 };
 
-export const Animation = ({ variant, width = 80, height = 80, loop = false, autoplay = true, className }: Props) => {
-  const [animation, setAnimation] = useState();
+const LazyAnimation = lazy(async () => {
+  const [{ default: error }, { default: loading }, { default: success }] = await Promise.all([
+    import('@shared/assets/animations/alert.json'),
+    import('@shared/assets/animations/spinner.json'),
+    import('@shared/assets/animations/success.json'),
+  ]);
 
-  const defaultOptions = {
-    loop,
-    autoplay,
-    animationData: animation,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
+  const images: Record<Props['variant'], unknown> = {
+    error,
+    success,
+    loading,
   };
 
-  const { View } = useLottie(defaultOptions, { width, height });
+  const { useLottie } = await import('lottie-react');
 
-  useEffect(() => {
-    // HINT: using same animation repeatedly without deep clone leads to memory leak
-    // https://github.com/airbnb/lottie-web/issues/1159
-    setAnimation(JSON.parse(JSON.stringify(Images[variant])));
-  }, [variant]);
+  const Component = ({ variant, width, height, loop, autoplay, className }: Props) => {
+    const [animation, setAnimation] = useState();
 
-  return <div className={className}>{View}</div>;
-};
+    const defaultOptions = {
+      loop,
+      autoplay,
+      animationData: animation,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice',
+      },
+    };
+
+    const { View } = useLottie(defaultOptions, { width, height });
+
+    useEffect(() => {
+      // HINT: using same animation repeatedly without deep clone leads to memory leak
+      // https://github.com/airbnb/lottie-web/issues/1159
+      setAnimation(JSON.parse(JSON.stringify(images[variant])));
+    }, [variant]);
+
+    return <div className={cnTw(className, 'animate-in fade-in')}>{View}</div>;
+  };
+
+  return { default: Component };
+});
+
+export const Animation = memo<Props>(
+  ({ variant, width = 80, height = 80, loop = false, autoplay = true, className }) => {
+    const fallback = <Shimmering width={width} height={height} circle={width === height} />;
+
+    return (
+      <Suspense fallback={fallback}>
+        <LazyAnimation
+          className={className}
+          variant={variant}
+          width={width}
+          height={height}
+          loop={loop}
+          autoplay={autoplay}
+        />
+      </Suspense>
+    );
+  },
+);
