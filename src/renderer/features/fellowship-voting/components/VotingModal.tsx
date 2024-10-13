ebeacd97b45401e4f836c22ec60261e0e887676f
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2024-10-14 00:35:42 +0200

    feat: carousel component

--- src/renderer/features/fellowship-voting/components/VotingModal.tsx
+++ src/renderer/features/fellowship-voting/components/VotingModal.tsx
@@ -1,11 +1,12 @@
 import { useUnit } from 'effector-react';
-import { type PropsWithChildren } from 'react';
+import { type PropsWithChildren, useState } from 'react';
 
 import { useI18n } from '@app/providers';
-import { Box, Modal } from '@/shared/ui-kit';
+import { Box, Carousel, Modal } from '@/shared/ui-kit';
 import { nonNullable, nullable } from '@shared/lib/utils';
-import { Button } from '@shared/ui';
 import { OperationTitle } from '@/entities/chain';
+import { SignButton } from '@/entities/operations';
+import { walletUtils } from '@entities/wallet';
 import { votingFeatureStatus } from '../model/status';
 import { votingModel } from '../model/voting';
 
@@ -16,15 +17,20 @@ type Props = PropsWithChildren<{
 }>;
 
 export const VotingModal = ({ children, vote }: Props) => {
+  const [step] = useState('confirm');
   const { t } = useI18n();
 
   const input = useUnit(votingFeatureStatus.input);
   const account = useUnit(votingModel.$votingAccount);
   const member = useUnit(votingModel.$currectMember);
 
-  if (nullable(input) || nullable(member)) {
+  if (nullable(input) || nullable(member) || nullable(account)) {
     return null;
   }
+
+  const wallet = walletUtils.getWalletFilteredAccounts(input.wallets, {
+    accountFn: a => a.accountId === account.accountId,
+  });
 
   return (
     <Modal size="md">
@@ -35,14 +41,30 @@ export const VotingModal = ({ children, vote }: Props) => {
       <Modal.Content>
         <Box gap={4} padding={[4, 5]}>
           {nonNullable(account) ? (
-            <VoteConfirm chain={input.chain} wallets={input.wallets} account={account} vote={vote} rank={member.rank} />
+            <Carousel item={step}>
+              <Carousel.Item id="confirm">
+                <VoteConfirm
+                  chain={input.chain}
+                  wallets={input.wallets}
+                  account={account}
+                  vote={vote}
+                  rank={member.rank}
+                />
+              </Carousel.Item>
+              <Carousel.Item id="sign">
+                <VoteConfirm
+                  chain={input.chain}
+                  wallets={input.wallets}
+                  account={account}
+                  vote={vote}
+                  rank={member.rank}
+                />
+              </Carousel.Item>
+            </Carousel>
           ) : null}
         </Box>
       </Modal.Content>
-      <Modal.Footer>
-        {/* eslint-disable-next-line i18next/no-literal-string */}
-        <Button>Sign</Button>
-      </Modal.Footer>
+      <Modal.Footer>{step === 'confirm' && nonNullable(wallet) && <SignButton type={wallet.type} />}</Modal.Footer>
     </Modal>
   );
 };
