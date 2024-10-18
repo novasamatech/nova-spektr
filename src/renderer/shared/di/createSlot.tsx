9f93f3dd12be74cb6447c857ec717197feb0b3bc
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2024-10-18 18:05:28 +0200

    fix: types

--- src/renderer/shared/di/createSlot.tsx
+++ src/renderer/shared/di/createSlot.tsx
@@ -46,7 +46,6 @@ export const createSlot = <Props,>(config?: { name: string }): SlotIdentifier<Pr
 type IsVoid<T> = T extends void ? true : false;
 
 type SlotProps = Record<string, unknown> | void;
-
 type SlotOptions<Props extends SlotProps> = IsVoid<Props> extends true ? { props?: void } : { props: Props };
 
 export type UseSlotArguments<Props extends SlotProps = void> =
