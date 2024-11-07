f920ba8f10bb27e990492e131c7f9e046f18f6ef
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2024-11-07 11:00:19 +0100

    feat: proxy pallet

---
+++ src/renderer/shared/pallet/proxy/schema.ts
@@ -0,0 +1,30 @@
+import { type z } from 'zod';
+
+import { pjsSchema } from '@/shared/polkadotjs-schemas';
+
+export type KitchensinkRuntimeProxyType = z.infer<typeof kitchensinkRuntimeProxyType>;
+export const kitchensinkRuntimeProxyType = pjsSchema.enumType(
+  'Any',
+  'NonTransfer',
+  'Governance',
+  'Staking',
+  // All types below can be absent, need some research
+  'Auction',
+  'CancelProxy',
+  'IdentityJudgement',
+  'NominationPools',
+);
+
+export type ProxyProxyDefinition = z.infer<typeof proxyProxyDefinition>;
+export const proxyProxyDefinition = pjsSchema.object({
+  delegate: pjsSchema.accountId,
+  delay: pjsSchema.u32,
+  proxyType: kitchensinkRuntimeProxyType,
+});
+
+export type ProxyAnnouncement = z.infer<typeof proxyAnnouncement>;
+export const proxyAnnouncement = pjsSchema.object({
+  real: pjsSchema.accountId,
+  callHash: pjsSchema.dataString,
+  height: pjsSchema.blockHeight,
+});
