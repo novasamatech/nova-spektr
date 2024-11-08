8679ad94ec5c67f09c32e5d3abc85ae60f0f5916
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2024-11-08 17:59:59 +0300

    fix: now electron app handle macos app reopening correctly (perhaps)

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,11 @@
 import 'source-map-support/register';
 
-import { type BrowserWindow, app } from 'electron';
-
-import { APP_CONFIG } from '../../app.config';
+import { type BrowserWindow, app, session } from 'electron';
+import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 import { runAppSingleInstance } from './factories/instance';
 import { setupLogger } from './factories/logs';
 import { processUrl, registerDeepLinkProtocol } from './factories/protocol';
-import { setupApplication } from './factories/setup';
 import { setupAutoUpdater } from './factories/updater';
 import { createWindow } from './factories/window';
 import { ENVIRONMENT } from './shared/constants/environment';
@@ -18,12 +16,12 @@ runAppSingleInstance(async () => {
   setupAutoUpdater();
   registerDeepLinkProtocol();
 
+  app.commandLine.appendSwitch('force-color-profile', 'srgb');
+  app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
   if (ENVIRONMENT.IS_DEV || ENVIRONMENT.IS_STAGE) {
     app.commandLine.appendSwitch('ignore-certificate-errors');
   }
-  app.commandLine.appendSwitch('force-color-profile', 'srgb');
 
-  app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
   process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
   delete process.env.ELECTRON_ENABLE_SECURITY_WARNINGS;
 
@@ -31,34 +29,55 @@ runAppSingleInstance(async () => {
     app.disableHardwareAcceleration();
   }
 
-  // eslint-disable-next-line prefer-const
-  let mainWindow: BrowserWindow | undefined;
+  await app.whenReady();
+
+  let mainWindow: BrowserWindow | null = createWindow();
 
   if (PLATFORM.IS_MAC) {
     // Protocol handler for macos
     app.on('open-url', (event, url) => {
       event.preventDefault();
-      processUrl(url, mainWindow);
+      if (mainWindow) {
+        processUrl(url, mainWindow);
+      }
     });
   }
 
   if (PLATFORM.IS_WINDOWS || PLATFORM.IS_LINUX) {
     // Protocol handler for win32/Linux
-    app.on('second-instance', (_, commandLine) => {
+    app.on('second-instance', () => {
       if (mainWindow) {
         if (mainWindow.isMinimized()) mainWindow.restore();
         mainWindow.focus();
       }
-
-      const url = commandLine[commandLine.length - 1];
-      if (!url.startsWith(APP_CONFIG.ELECTRON_PROTOCOL + '://')) return;
-
-      processUrl(url, mainWindow);
     });
   }
 
-  await app.whenReady();
+  app.on('activate', async () => {
+    if (mainWindow === null) {
+      mainWindow = createWindow();
+    }
+  });
 
-  mainWindow = createWindow();
-  await setupApplication(mainWindow);
+  app.on('web-contents-created', (_, contents) =>
+    contents.on('will-navigate', (event) => !ENVIRONMENT.IS_DEV && event.preventDefault()),
+  );
+
+  app.on('window-all-closed', () => {
+    if (!PLATFORM.IS_MAC) {
+      app.quit();
+    }
+
+    mainWindow?.destroy();
+    mainWindow = null;
+  });
+
+  if (ENVIRONMENT.IS_DEV) {
+    await installExtension(REACT_DEVELOPER_TOOLS);
+
+    // Reloading extensions for correct initialization in dev tools
+    session.defaultSession.getAllExtensions().map((e) => {
+      session.defaultSession.loadExtension(e.path);
+    });
+  }
 });
