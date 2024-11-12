23d508408c4a8ec899472b7e6c205eff7de5d23d
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2024-11-12 15:03:21 +0100

    fix: series helper description

--- src/renderer/shared/effector/helpers/series.ts
+++ src/renderer/shared/effector/helpers/series.ts
@@ -2,12 +2,29 @@ import { type Effect, type Event, type EventCallable, createEvent, createStore, 
 
 import { nonNullable } from '@/shared/lib/utils';
 
-export const series = <T>(target: EventCallable<T> | Effect<T, any>): EventCallable<T[]> => {
+/**
+ * Triggers target unit on each element of the input list.
+ *
+ * ```ts
+ * const $store = createStore<number[]>([]);
+ * const event = createEvent<number>();
+ *
+ * sample({
+ *   clock: $store,
+ *   target: series(event),
+ * });
+ *
+ * $store.set([0, 1, 3]);
+ * // event will be called 3 times, direct equivalent of
+ * // event(0); event(1); event(2)
+ * ```
+ */
+export const series = <T>(target: EventCallable<T> | Effect<T, any>) => {
   const pop = createEvent();
-  const push = createEvent<T[]>();
+  const push = createEvent<Iterable<T> | ArrayLike<T>>();
 
   const $queue = createStore<T[]>([])
-    .on(push, (state, payload) => state.concat(payload))
+    .on(push, (state, payload) => state.concat(Array.from(payload)))
     .on(pop, ([, ...rest]) => rest);
   const $head = $queue.map((queue) => queue.at(0) ?? null);
   const nextHeadRetrieved = $head.updates.filter({ fn: nonNullable }) as Event<T>;
