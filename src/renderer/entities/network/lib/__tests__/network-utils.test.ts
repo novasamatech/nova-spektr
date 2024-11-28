9fcdd6c68ab22ce0fdb71db54488686c40eb0f0b
asmadek <alma2610@ya.ru>
2024-11-28 17:59:15 +0500

    fix: tests for chain name converter

---
+++ src/renderer/entities/network/lib/__tests__/network-utils.test.ts
@@ -0,0 +1,20 @@
+import { networkUtils } from '../network-utils';
+
+describe('entities/network/lib/network-utils', () => {
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  test.each([
+    ['Polkadot', 'polkadot'],
+    ['Kusama', 'kusama'],
+    ['Novasama Testnet - Governance', 'novasama-testnet-governance'],
+    ['Name with (braces)', 'name-with-braces'],
+    ['Name with  spaces', 'name-with-spaces'],
+    ['Name with /.,.&!@#$', 'name-with'],
+  ])('should convert chain name to url', (firstArg, expectedResult) => {
+    const url = networkUtils.chainNameToUrl(firstArg);
+
+    expect(url).toEqual(expectedResult);
+  });
+});
