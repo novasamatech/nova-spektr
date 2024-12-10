bd17e6fd088af8cda2094e6668e269d36249e2aa
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2024-12-10 17:28:06 +0100

    fix: xcm toke amount validation

--- src/renderer/features/operations/OperationsValidation/lib/validation/balance.ts
+++ src/renderer/features/operations/OperationsValidation/lib/validation/balance.ts
@@ -39,7 +39,7 @@ function insufficientBalanceForFee(
     isNative ? (config.withFormatAmount ? formatAmount(amount, asset.precision) : amount) : ZERO_BALANCE,
   );
   const feeBN = new BN(isProxy || isMultisig ? ZERO_BALANCE : fee);
-  const xcmFeeBN = new BN(isXcm ? xcmFee : ZERO_BALANCE);
+  const xcmFeeBN = new BN(isXcm && isNative ? xcmFee : ZERO_BALANCE);
   const value = amountBN.add(feeBN).add(xcmFeeBN);
 
   return isLteThanBalance(value, balance);
