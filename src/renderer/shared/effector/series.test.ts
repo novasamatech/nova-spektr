d47b13a65e60d6c3e52c6a3fa09eb0337cb30f0f
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-01-22 16:14:46 +0100

    chore: more tests for series helper

--- src/renderer/shared/effector/series.test.ts
+++ src/renderer/shared/effector/series.test.ts
@@ -83,4 +83,55 @@ describe('series', () => {
     expect(spy).toHaveBeenCalledTimes(3);
     expect(spy.mock.calls).toEqual([[1], [2], [3]]);
   });
+
+  it('should collect doneData from wrapped effect', async () => {
+    const scope = fork();
+    const target = createEffect((n: number) => n + 1);
+    const wrapped = series(target);
+
+    const res = await allSettled(wrapped, { scope, params: [1, 2, 3] });
+
+    expect(res.status).toBe('done');
+    expect(res.status === 'done' ? res.value : []).toEqual([2, 3, 4]);
+  });
+
+  it('should not shuffle values with parallel option', async () => {
+    const scope = fork();
+    const target = createEffect((ttl: number) => new Promise(r => setTimeout(() => r(ttl), ttl)));
+    const wrapped = series(target, { parallel: true });
+
+    const res = await allSettled(wrapped, { scope, params: [3, 2, 1] });
+
+    expect(res.status).toBe('done');
+    expect(res.status === 'done' ? res.value : []).toEqual([3, 2, 1]);
+  });
+
+  it('should respect skipErrors option', async () => {
+    const scope = fork();
+    const target = createEffect(({ fail, value }: { value: number; fail: boolean }) => {
+      return new Promise((resolve, reject) => (fail ? reject() : resolve(value)));
+    });
+    const wrapped = series(target, { skipErrors: true });
+
+    const res = await allSettled(wrapped, {
+      scope,
+      params: [
+        {
+          value: 1,
+          fail: false,
+        },
+        {
+          value: 2,
+          fail: true,
+        },
+        {
+          value: 3,
+          fail: false,
+        },
+      ],
+    });
+
+    expect(res.status).toBe('done');
+    expect(res.status === 'done' ? res.value : []).toEqual([1, 3]);
+  });
 });
