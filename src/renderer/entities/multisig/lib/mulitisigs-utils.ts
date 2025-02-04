b889263c93c69f98aad5aaaf639a5937fa3ac7e7
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-02-04 22:43:11 +0100

    fix: correct sorting of signatories

--- src/renderer/entities/multisig/lib/mulitisigs-utils.ts
+++ src/renderer/entities/multisig/lib/mulitisigs-utils.ts
@@ -28,10 +28,23 @@ function getOtherSignatories(
 ) {
   const signerAddress = toAddress(signer, { prefix: addressPrefix });
 
-  return Array.from(account.signatories)
-    .sort((a, b) => a.accountId.localeCompare(b.accountId))
-    .map((s) => toAddress(s.accountId, { prefix: addressPrefix }))
-    .filter((address) => address !== signerAddress);
+  return (
+    Array.from(account.signatories)
+      /**
+       * Public keys of signers' wallets are compared byte-for-byte and sorted
+       * ascending before being used to generate the multisig address. For
+       * example, consider the scenario with three addresses, A, B, and C,
+       * starting with 5FUGT, 5HMfS, and 5GhKJ. If we build the ABC multisig
+       * with the accounts in that specific order (i.e. first A, then B, and C),
+       * the real order of the accounts in the multisig will be ACB. If, in the
+       * Extrinsic tab, we initiate a multisig call with C, the order of the
+       * other signatories will be first A, then B. If we put first B, then A,
+       * the transaction will fail.
+       */
+      .sort((a, b) => a.accountId.localeCompare(b.accountId))
+      .map((s) => toAddress(s.accountId, { prefix: addressPrefix }))
+      .filter((address) => address !== signerAddress)
+  );
 }
 
 function isMultisigSupported(chain: Chain) {
