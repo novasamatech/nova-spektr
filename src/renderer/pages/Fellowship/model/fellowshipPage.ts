1fb687c1ef083a528d8fefc08c9bd5ccc2a4c459
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-02-06 14:48:29 +0300

    fix: update chain id

--- src/renderer/pages/Fellowship/model/fellowshipPage.ts
+++ src/renderer/pages/Fellowship/model/fellowshipPage.ts
@@ -8,7 +8,7 @@ export const COLLECTIVES_CHAIN_ID: ChainId = '0x46ee89aa2eedd13e988962630ec9fb75
 export const COLLECTIVES_WESTEND_CHAIN_ID: ChainId =
   '0x713daf193a6301583ff467be736da27ef0a72711b248927ba413f573d2b38e44';
 export const COLLECTIVES_NOVASAMA_CHAIN_ID: ChainId =
-  '0xa35b4d0bf5767fcc3a4795aca93ef3b152c7c280d558d49eea535cae56a3a93e';
+  '0x5647d37b8ac3ea2259c6697ec255347d6f473d052bfec59c641d751538d43442';
 const flow = createGate<{ chainId: ChainId }>();
 
 // We want to show main fellowship on specific chain - Polkadot Collectives
