e5244cc932aad059ba9e8606f88fabb778c1234e
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-02-07 13:14:14 +0100

    feat: wip

--- src/renderer/features/fellowship-tasks/model/referendums.ts
+++ src/renderer/features/fellowship-tasks/model/referendums.ts
@@ -3,7 +3,7 @@ import { combine, createEvent, sample } from 'effector';
 import { attachToFeatureInput } from '@/shared/feature';
 import { dictionary, nullable } from '@/shared/lib/utils';
 import { type AccountId } from '@/shared/polkadotjs-schemas';
-import { evidence, referendumService } from '@/domains/collectives';
+import { evidence, referendumService, tracks, tracksService } from '@/domains/collectives';
 
 import { fellowshipTasksFeature } from './feature';
 import { fellowshipModel } from './fellowship';
@@ -11,6 +11,7 @@ import { member } from './member';
 
 const requestEvidence = createEvent<AccountId>();
 
+const $maxRank = fellowshipModel.$store.map(x => x?.maxRank ?? 0);
 const $voting = fellowshipModel.$store.map(store => store?.voting ?? []);
 const $tracks = fellowshipModel.$store.map(store => store?.tracks ?? []);
 const $evidences = fellowshipModel.$store.map(store => store?.evidence ?? []);
@@ -20,6 +21,24 @@ const $ongoing = $referendums.map(referendumService.getOngoingReferendums);
 const $memberVoting = combine(member.$member, $voting, (member, voting) => {
   if (nullable(member)) return [];
   return voting.filter(v => v.accountId === member.accountId);
+});
+
+const $notVotedReferendumns = combine(
+  { maxRank: $maxRank, member: member.$member, ongoing: $ongoing, voting: $memberVoting },
+  ({ maxRank, member, ongoing, voting }) => {
+    if (nullable(member)) return [];
+    const votingMap = dictionary(voting, 'referendumId');
+    return ongoing.filter(
+      referendum =>
+        !(referendum.id in votingMap) &&
+        tracksService.rankSatisfiesVotingThreshold(member.rank, maxRank, referendum.track),
+    );
+  },
+);
+
+sample({
+  clock: fellowshipTasksFeature.running,
+  target: [tracks.request],
 });
 
 sample({
@@ -33,11 +52,6 @@ sample({
     };
   },
   target: evidence.request,
-});
-
-const $notVotedReferendumns = combine($ongoing, $memberVoting, (ongoing, voting) => {
-  const votingMap = dictionary(voting, 'referendumId');
-  return ongoing.filter(referendum => !(referendum.id in votingMap));
 });
 
 export const referendumList = {
