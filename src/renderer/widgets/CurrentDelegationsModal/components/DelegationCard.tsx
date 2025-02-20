51b0d18ee475324d88b22f9b20d8a3d4ba35dfbc
Yaroslav Grachev <tuulwq@gmail.com>
2025-02-20 16:42:51 +0300

    feat: voting power for delegates modal

--- src/renderer/widgets/CurrentDelegationsModal/components/DelegationCard.tsx
+++ src/renderer/widgets/CurrentDelegationsModal/components/DelegationCard.tsx
@@ -1,72 +1,76 @@
 import { type BN, BN_ZERO } from '@polkadot/util';
-import { useUnit } from 'effector-react';
+import { useMemo } from 'react';
 
 import { type DelegateAccount } from '@/shared/api/governance';
-import { type Conviction } from '@/shared/core';
+import { type Asset, type Conviction } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
+import { cnTw } from '@/shared/lib/utils';
 import { BodyText, FootnoteText } from '@/shared/ui';
 import { AssetBalance } from '@/entities/asset';
 import { votingService } from '@/entities/governance';
-import { DelegateName, networkSelectorModel } from '@/features/governance';
+import { DelegateName } from '@/features/governance';
 
 type Props = {
+  asset: Asset;
   delegate: DelegateAccount;
   votes?: {
     conviction: Conviction;
     balance: BN;
   }[];
-  tracks?: string[];
+  onClick: () => void;
 };
 
-export const DelegationCard = ({ delegate, votes, tracks }: Props) => {
+export const DelegationCard = ({ asset, delegate, votes = [], onClick }: Props) => {
   const { t } = useI18n();
 
-  const chain = useUnit(networkSelectorModel.$governanceChain);
+  const { totalVotes, totalVotingPower } = useMemo(() => {
+    const result = {
+      totalVotes: BN_ZERO,
+      totalVotingPower: BN_ZERO,
+    };
 
-  const totalVotes = votes?.reduce(
-    (acc, { balance, conviction }) => acc.add(votingService.calculateVotingPower(balance, conviction)),
-    BN_ZERO,
-  );
+    for (const { balance, conviction } of votes) {
+      result.totalVotes = result.totalVotes.add(balance);
+      result.totalVotingPower = result.totalVotingPower.add(votingService.calculateVotingPower(balance, conviction));
+    }
+
+    return result;
+  }, [votes.length]);
 
   return (
-    <li className="rounded border border-container-border bg-white p-4 shadow-card-shadow">
+    <button
+      className={cnTw(
+        'w-full rounded border border-container-border bg-white p-4 transition-shadow',
+        'shadow-shadow-1 hover:shadow-shadow-2 focus:shadow-shadow-2',
+      )}
+      onClick={onClick}
+    >
       <div className="flex flex-col gap-4">
-        <DelegateName
-          delegate={delegate}
-          asset={chain?.assets[0]}
-          votes={totalVotes}
-          tracks={tracks}
-          titleClassName="max-w-[200px]"
-        />
+        <DelegateName delegate={delegate} titleClassName="max-w-[200px]" />
         <div className="flex flex-col gap-2.5">
-          {delegate.shortDescription && <FootnoteText>{delegate.shortDescription}</FootnoteText>}
+          <FootnoteText>{delegate.shortDescription}</FootnoteText>
 
-          <div className="flex gap-6 divide-x divide-divider">
+          <div className="grid grid-cols-2">
             <div className="flex flex-col gap-1">
               <FootnoteText className="text-text-secondary">
-                {t('governance.addDelegation.card.delegations')}
+                {t('governance.addDelegation.card.lockedAmount')}
               </FootnoteText>
-              <BodyText>{delegate.delegators || '0'}</BodyText>
-            </div>
-
-            <div className="flex flex-col gap-1 pl-6">
-              <FootnoteText className="text-text-secondary">{t('governance.addDelegation.card.votes')}</FootnoteText>
               <BodyText>
-                <AssetBalance
-                  showSymbol={false}
-                  value={delegate.delegatorVotes?.toString() || '0'}
-                  asset={chain?.assets[0]}
-                />
+                <AssetBalance value={totalVotes} asset={asset} />
               </BodyText>
             </div>
 
-            <div className="flex flex-col gap-1 pl-6">
-              <FootnoteText className="text-text-secondary">{t('governance.addDelegation.card.voted')}</FootnoteText>
-              <BodyText>{delegate.delegateVotesMonth || '0'}</BodyText>
+            <div className="flex flex-col gap-1 divide-divider border-l pl-5">
+              <FootnoteText className="text-text-secondary">
+                {t('governance.addDelegation.card.votingPower')}
+              </FootnoteText>
+              <BodyText>
+                <AssetBalance value={totalVotingPower} asset={asset} />
+              </BodyText>
             </div>
           </div>
         </div>
       </div>
-    </li>
+    </button>
   );
 };
--- src/renderer/widgets/DelegationModal/components/DelegationCard.tsx
+++ src/renderer/widgets/DelegationModal/components/DelegationCard.tsx
@@ -1,53 +1,55 @@
-import { useUnit } from 'effector-react';
-
 import { type DelegateAccount } from '@/shared/api/governance';
+import { type Asset } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
+import { cnTw } from '@/shared/lib/utils';
 import { BodyText, FootnoteText } from '@/shared/ui';
 import { AssetBalance } from '@/entities/asset';
-import { DelegateName, networkSelectorModel } from '@/features/governance';
+import { DelegateName } from '@/features/governance';
 
 type Props = {
+  asset: Asset;
   delegate: DelegateAccount;
+  onClick: () => void;
 };
 
-export const DelegationCard = ({ delegate }: Props) => {
+export const DelegationCard = ({ asset, delegate, onClick }: Props) => {
   const { t } = useI18n();
 
-  const chain = useUnit(networkSelectorModel.$governanceChain);
-
   return (
-    <li className="rounded border border-container-border bg-white p-4 shadow-card-shadow">
+    <button
+      className={cnTw(
+        'w-full rounded border border-container-border bg-white p-4 transition-shadow',
+        'shadow-shadow-1 hover:shadow-shadow-2 focus:shadow-shadow-2',
+      )}
+      onClick={onClick}
+    >
       <div className="flex flex-col gap-4">
         <DelegateName delegate={delegate} titleClassName="max-w-[200px]" />
         <div className="flex flex-col gap-2.5">
-          {delegate.shortDescription && <FootnoteText>{delegate.shortDescription}</FootnoteText>}
+          <FootnoteText>{delegate.shortDescription}</FootnoteText>
 
-          <div className="flex gap-6 divide-x divide-divider">
+          <div className="grid grid-cols-[1fr,100px,128px] gap-x-5">
             <div className="flex flex-col gap-1">
+              <FootnoteText className="text-text-secondary">{t('governance.addDelegation.card.votes')}</FootnoteText>
+              <BodyText>
+                <AssetBalance value={delegate.delegatorVotes?.toString() || '0'} asset={asset} />
+              </BodyText>
+            </div>
+
+            <div className="flex flex-col gap-1 divide-divider border-l pl-5">
               <FootnoteText className="text-text-secondary">
                 {t('governance.addDelegation.card.delegations')}
               </FootnoteText>
               <BodyText>{delegate.delegators || '0'}</BodyText>
             </div>
 
-            <div className="flex flex-col gap-1 pl-6">
-              <FootnoteText className="text-text-secondary">{t('governance.addDelegation.card.votes')}</FootnoteText>
-              <BodyText>
-                <AssetBalance
-                  showSymbol={false}
-                  value={delegate.delegatorVotes?.toString() || '0'}
-                  asset={chain?.assets[0]}
-                />
-              </BodyText>
-            </div>
-
-            <div className="flex flex-col gap-1 pl-6">
+            <div className="flex flex-col gap-1 divide-divider border-l pl-5">
               <FootnoteText className="text-text-secondary">{t('governance.addDelegation.card.voted')}</FootnoteText>
               <BodyText>{delegate.delegateVotesMonth || '0'}</BodyText>
             </div>
           </div>
         </div>
       </div>
-    </li>
+    </button>
   );
 };
