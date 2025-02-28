b06b11053bc90f374a5c8724508c2e1ac25f667a
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2025-02-28 13:24:08 +0100

    feat: unlock basket for single shard pv accounts

--- src/renderer/entities/basket/lib/basket-utils.ts
+++ src/renderer/entities/basket/lib/basket-utils.ts
@@ -3,7 +3,7 @@ import { type AnyAccount, accountService } from '@/domains/network';
 import { walletUtils } from '@/entities/wallet';
 
 function isBasketAvailable(wallet: Wallet): boolean {
-  return walletUtils.isPolkadotVault(wallet) || walletUtils.isMultiShard(wallet);
+  return walletUtils.isPolkadotVault(wallet) || walletUtils.isMultiShard(wallet) || walletUtils.isSingleShard(wallet);
 }
 
 function isBasketAvailableForAccount(account: AnyAccount): boolean {
