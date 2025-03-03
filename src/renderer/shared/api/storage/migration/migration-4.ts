586a06ab0a92ee58f7f5fe69c52920b826827529
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-03-03 16:01:46 +0100

    fix: migration

--- src/renderer/shared/api/storage/migration/migration-4.ts
+++ src/renderer/shared/api/storage/migration/migration-4.ts
@@ -59,6 +59,6 @@ export async function migrateMultisigAccounts(t: Transaction): Promise<void> {
   }
 
   await t.table('accounts2').bulkPut(accountsToAdd);
-  await t.table('accounts2').bulkDelete(accountsToDelete);
+  await t.table('accounts2').bulkDelete(accountsToDelete.map((a) => a.accountId));
   await t.table('wallets').bulkDelete(walletsToDelete);
 }
