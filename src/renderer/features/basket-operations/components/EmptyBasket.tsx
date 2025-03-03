389f99aaf08d6742f5b5515abbdc72fa053e3221
Anton <aaa_eee@icloud.com>
2025-03-03 12:31:40 +0100

    fix: show proper empty search msg

--- src/renderer/features/basket-operations/components/EmptyBasket.tsx
+++ src/renderer/features/basket-operations/components/EmptyBasket.tsx
@@ -1,14 +1,22 @@
+import { Trans } from 'react-i18next';
+
 import { useI18n } from '@/shared/i18n';
 import { BodyText } from '@/shared/ui';
 import { Graphics } from '@/shared/ui-kit';
 
-export const EmptyBasket = () => {
+type Props = {
+  text: string;
+};
+
+export const EmptyBasket = ({ text }: Props) => {
   const { t } = useI18n();
 
   return (
     <div className="flex h-full flex-col items-center justify-center gap-y-4">
       <Graphics name="emptyList" alt={t('basket.noOperationsLabel')} size={178} />
-      <BodyText className="w-[300px] text-center text-text-tertiary">{t('basket.noOperationsDescription')}</BodyText>
+      <BodyText className="w-[300px] text-center text-text-tertiary">
+        <Trans t={t} i18nKey={text} />
+      </BodyText>
     </div>
   );
 };
