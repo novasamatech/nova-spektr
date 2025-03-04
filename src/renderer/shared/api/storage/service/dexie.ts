293e9777e68696359d385f7f5d7465d6e3a3cd24
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2025-03-04 16:04:54 +0300

    feat: add connections table to import\export (#3280)
    * fix: import tables
    
    * feat: export connections too

--- src/renderer/shared/api/storage/service/dexie.ts
+++ src/renderer/shared/api/storage/service/dexie.ts
@@ -156,7 +156,7 @@ const dexie = new DexieStorage();
 export const exportDb = async () => {
   const blob = await exportDB(dexie, {
     prettyJson: true,
-    skipTables: ['metadata', 'balances', 'proxies', 'proxyGroups', 'basketTransactions', 'connections'],
+    skipTables: ['metadata', 'balances', 'proxies', 'proxyGroups', 'basketTransactions'],
   });
 
   return { blob, fileName: 'spektr-database.json' };
