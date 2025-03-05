a0404f475450a9ed4c44b261b241b6b6ef9e21fc
asmadek <alma2610@ya.ru>
2025-03-05 17:04:16 +0500

    chore: fix

--- src/renderer/pages/Operations/Operations.tsx
+++ src/renderer/pages/Operations/Operations.tsx
@@ -1,6 +1,7 @@
 import { useI18n } from '@/shared/i18n';
 import { Header } from '@/shared/ui';
-import { Operations as OperationsList } from '@/features/multisig-operations';
+// eslint-disable-next-line boundaries/entry-point
+import { Operations as OperationsList } from '@/features/multisig-operations/components/Operations';
 
 export const Operations = () => {
   const { t } = useI18n();
