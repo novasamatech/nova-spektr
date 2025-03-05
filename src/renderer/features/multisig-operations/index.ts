006a5052383ebf5fc6580c3361dfc95e0318f050
asmadek <alma2610@ya.ru>
2025-03-05 18:04:04 +0500

    chore: fix tests

--- src/renderer/features/multisig-operations/index.ts
+++ src/renderer/features/multisig-operations/index.ts
@@ -2,6 +2,6 @@
 // export { operationTitleSlot } from './components/Operation';
 // export { operationDetailsSlot } from './components/OperationFullInfo';
 // export { confirmTransactionInfoSlot } from './components/ActionSteps/Confirmation';
-export { Operations } from './components/Operations';
+// export { Operations } from './components/Operations';
 export { multisigOperations } from './model/model';
 // export { SignatorySelectModal } from './components/modals/SignatorySelectModal';
