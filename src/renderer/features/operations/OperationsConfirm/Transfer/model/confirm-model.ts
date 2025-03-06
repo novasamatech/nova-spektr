9f6f4711a4b5e355b7e977b27fffcad2efcef611
asmadek <alma2610@ya.ru>
2025-03-06 15:30:25 +0500

    chore: fix tests

--- src/renderer/features/operations/OperationsConfirm/Transfer/model/confirm-model.ts
+++ src/renderer/features/operations/OperationsConfirm/Transfer/model/confirm-model.ts
@@ -15,7 +15,9 @@ import { balanceModel, balanceUtils } from '@/entities/balance';
 import { networkModel } from '@/entities/network';
 import { operationsUtils } from '@/entities/operations';
 import { walletModel, walletUtils } from '@/entities/wallet';
-import { multisigOperations } from '@/features/multisig-operations';
+// TODO: Fix circular dependencies
+// eslint-disable-next-line boundaries/entry-point
+import { multisigOperations } from '@/features/multisig-operations/model/model';
 import {
   type BalanceMap,
   type NetworkStore,
