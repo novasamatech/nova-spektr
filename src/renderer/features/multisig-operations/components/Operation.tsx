40babe989d945393432b0012b78656fe1df034f4
asmadek <alma2610@ya.ru>
2025-03-10 16:21:32 +0500

    chore: add comparator for memoized operation

--- src/renderer/features/multisig-operations/components/Operation.tsx
+++ src/renderer/features/multisig-operations/components/Operation.tsx
@@ -1,3 +1,4 @@
+import { isEqual } from 'lodash';
 import { memo } from 'react';
 
 import { type MultisigAccount } from '@/shared/core';
@@ -7,6 +8,13 @@ import { type MultisigOperation } from '@/domains/multisig';
 import { OperationTitleDate, OperationTitleStatus } from '@/entities/operations';
 
 import { OperationFullInfo } from './OperationFullInfo';
+
+const compareTxWithoutDeposit = (oldTx: MultisigOperation, newTx: MultisigOperation) => {
+  const { deposit: _oldDeposit, ...oldWithoutDeposit } = oldTx;
+  const { deposit: _newDeposit, ...newWithoutDeposit } = newTx;
+
+  return isEqual(oldWithoutDeposit, newWithoutDeposit);
+};
 
 type Props = {
   tx: MultisigOperation;
@@ -19,21 +27,29 @@ type SlotProps = {
 
 export const operationTitleSlot = createSlot<SlotProps>();
 
-const Operation = memo(({ tx, account }: Props) => {
-  return (
-    <Accordion className="rounded bg-block-background-default transition-shadow hover:shadow-card-shadow focus-visible:shadow-card-shadow">
-      <Accordion.Button buttonClass="px-2" iconWrapper="px-1.5">
-        <div className="flex h-[52px] w-full items-center gap-x-4 overflow-hidden">
-          <OperationTitleDate operation={tx} />
-          <Slot id={operationTitleSlot} props={{ operation: tx }} />
-          <OperationTitleStatus operation={tx} />
-        </div>
-      </Accordion.Button>
-      <Accordion.Content className="border-t border-divider">
-        <OperationFullInfo tx={tx} account={account} />
-      </Accordion.Content>
-    </Accordion>
-  );
-});
+const Operation = memo(
+  ({ tx, account }: Props) => {
+    return (
+      <Accordion className="rounded bg-block-background-default transition-shadow hover:shadow-card-shadow focus-visible:shadow-card-shadow">
+        <Accordion.Button buttonClass="px-2" iconWrapper="px-1.5">
+          <div className="flex h-[52px] w-full items-center gap-x-4 overflow-hidden">
+            <OperationTitleDate operation={tx} />
+            <Slot id={operationTitleSlot} props={{ operation: tx }} />
+            <OperationTitleStatus operation={tx} />
+          </div>
+        </Accordion.Button>
+        <Accordion.Content className="border-t border-divider">
+          <OperationFullInfo tx={tx} account={account} />
+        </Accordion.Content>
+      </Accordion>
+    );
+  },
+  (previousePros, props) => {
+    const equalTx = compareTxWithoutDeposit(previousePros.tx, props.tx);
+    const equalAccount = isEqual(previousePros.account, props.account);
+
+    return equalTx && equalAccount;
+  },
+);
 
 export default Operation;
