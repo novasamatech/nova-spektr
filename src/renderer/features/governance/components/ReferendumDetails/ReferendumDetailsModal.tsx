8f9e49d6b44c23336e5fec522273b7518f0f9b22
Yaroslav Grachev <tuulwq@gmail.com>
2025-03-13 16:19:39 +0300

    feat: removed conviction from vote history

--- src/renderer/features/governance/components/ReferendumDetails/ReferendumDetailsModal.tsx
+++ src/renderer/features/governance/components/ReferendumDetails/ReferendumDetailsModal.tsx
@@ -8,7 +8,8 @@ import { useToggle } from '@/shared/lib/hooks';
 import { Button, IconButton, Plate } from '@/shared/ui';
 import { Box, Modal } from '@/shared/ui-kit';
 import { referendumService } from '@/entities/governance';
-import { walletModel } from '@/entities/wallet';
+import { walletUtils } from '@/entities/wallet';
+import { walletSelect } from '@/aggregates/wallet-select';
 import { detailsAggregate } from '../../aggregates/details';
 import { proposerIdentityAggregate } from '../../aggregates/proposerIdentity';
 import { listService } from '../../lib/listService';
@@ -54,7 +55,7 @@ export const ReferendumDetailsModal = ({
 
   const canVote = useUnit(detailsAggregate.$canVote);
   const hasAccount = useUnit(detailsAggregate.$hasAccount);
-  const wallet = useUnit(walletModel.$activeWallet);
+  const wallet = useUnit(walletSelect.$selectedWallet);
 
   const [showWalletVotes, toggleShowWalletVotes] = useToggle();
   const [showVoteHistory, toggleShowVoteHistory] = useToggle();
@@ -90,10 +91,12 @@ export const ReferendumDetailsModal = ({
                 <DetailsCard>
                   <Box direction="row" verticalAlign="center" horizontalAlign="space-between">
                     <VotedBy
+                      variant="rows"
                       asset={asset}
                       identity={identity}
                       delegates={referendum.votedByDelegates}
                       castingVotes={referendum.voting.votes}
+                      conviction={walletUtils.isPolkadotVault(wallet) || walletUtils.isMultiShard(wallet)}
                     />
                     <IconButton name="info" onClick={toggleShowWalletVotes} />
                   </Box>
