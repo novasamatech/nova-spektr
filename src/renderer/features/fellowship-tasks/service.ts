147a83dcbae4836e192638d39f7ba0d05e94b75c
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-03-24 10:19:23 +0100

    fix: correct sorting for urgent tasks

--- src/renderer/features/fellowship-tasks/service.ts
+++ src/renderer/features/fellowship-tasks/service.ts
@@ -75,13 +75,13 @@ function getUserImportanceScore(maximumAvailableVotingWeight: number, userVoting
  */
 function getSortingScope(isUrgent: boolean, isControversial: boolean, isImportantVote: boolean) {
   if (isUrgent && isImportantVote) {
-    return 4;
-  } else if (isUrgent && isControversial) {
-    return 3;
-  } else if (isImportantVote) {
-    return 2;
-  } else if (isControversial) {
     return 1;
+  } else if (isUrgent && isControversial) {
+    return 0.75;
+  } else if (isImportantVote || isUrgent) {
+    return 0.5;
+  } else if (isControversial) {
+    return 0.25;
   }
 
   return 0;
