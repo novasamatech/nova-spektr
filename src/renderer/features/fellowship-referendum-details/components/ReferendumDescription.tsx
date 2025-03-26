b81cf273d5a6d6af7d0efbc8bac04854405c4d56
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-03-26 12:36:59 +0100

    fix: fellowship fixes

--- src/renderer/features/fellowship-referendum-details/components/ReferendumDescription.tsx
+++ src/renderer/features/fellowship-referendum-details/components/ReferendumDescription.tsx
@@ -1,33 +1,44 @@
 import { useUnit } from 'effector-react';
 
-import { nullable } from '@/shared/lib/utils';
-import { HeaderTitleText, Markdown } from '@/shared/ui';
+import { nonNullable, nullable } from '@/shared/lib/utils';
+import { Alert, HeaderTitleText, Markdown } from '@/shared/ui';
 import { Box, Skeleton } from '@/shared/ui-kit';
+import { referendumService, trackService } from '@/domains/collectives';
 import { details } from '../model/details';
 
 import { ProposerName } from './ProposerName';
 
 export const ReferendumDescription = () => {
+  const referendum = useUnit(details.$referendum);
   const referendumMeta = useUnit(details.$referendumMeta);
   const pendingReferendumMeta = useUnit(details.$pendingMeta);
   const pendingEvidence = useUnit(details.$pendingEvidence);
   const evidence = useUnit(details.$evidence);
   const description = useUnit(details.$description);
 
-  const metaLoadingState = pendingReferendumMeta && nullable(referendumMeta);
+  const shouldRenderTitle = pendingReferendumMeta && nullable(referendumMeta);
+  const canHaveEvidence =
+    nonNullable(referendum) &&
+    nonNullable(referendumMeta) &&
+    referendumService.isOngoing(referendum) &&
+    (trackService.isPromotionTrack(referendumMeta.track) || trackService.isRetentionTrack(referendumMeta.track));
+  const shouldRenderEvidence = nonNullable(evidence) && !pendingEvidence;
+  const shouldRenderEvidencePending = nullable(evidence) && pendingEvidence;
+  const shouldRenderEvidenceAlert = canHaveEvidence && nullable(evidence) && !pendingEvidence;
 
   return (
     <Box padding={6} gap={4}>
       <ProposerName />
       <HeaderTitleText className="text-balance">
-        {metaLoadingState ? <Skeleton height="1lh" width="80%" /> : referendumMeta?.title}
+        {shouldRenderTitle ? <Skeleton height="1lh" width="80%" /> : referendumMeta?.title}
       </HeaderTitleText>
 
       {pendingReferendumMeta ? <Skeleton height="1lh" width="100%" /> : null}
       {!pendingReferendumMeta && description ? <Markdown>{description}</Markdown> : null}
 
-      {pendingEvidence ? <Skeleton height="8lh" width="100%" /> : null}
-      {!pendingEvidence && evidence ? <Markdown>{evidence ?? ''}</Markdown> : null}
+      {shouldRenderEvidencePending ? <Skeleton height="8lh" width="100%" /> : null}
+      {shouldRenderEvidence ? <Markdown>{evidence ?? ''}</Markdown> : null}
+      {shouldRenderEvidenceAlert ? <Alert active variant="warn" title="Evidence not provided"></Alert> : null}
     </Box>
   );
 };
--- src/renderer/features/fellowship-tasks/components/tasks/CompletedReferendumVoting.tsx
+++ src/renderer/features/fellowship-tasks/components/tasks/CompletedReferendumVoting.tsx
@@ -1,16 +1,32 @@
 import { useStoreMap } from 'effector-react';
+import { type TFunction } from 'i18next';
 import { memo } from 'react';
 
 import { useI18n } from '@/shared/i18n';
-import { SmallTitleText } from '@/shared/ui';
+import { FootnoteText, Icon, type IconNames, SmallTitleText } from '@/shared/ui';
 import { Box, Label } from '@/shared/ui-kit';
-import { type CompletedReferendum, type Referendum } from '@/domains/collectives';
+import { type CompletedReferendum } from '@/domains/collectives';
 import { referendums } from '../../model/referendums';
 import { votes } from '../../model/voting';
 
+const getStatusLabel = (type: CompletedReferendum['type'], t: TFunction): { icon: IconNames; label: string } => {
+  switch (type) {
+    case 'Approved':
+      return { icon: 'votingCheckFilled', label: t('governance.referendums.approved') };
+    case 'TimedOut':
+      return { icon: 'clock', label: t('governance.referendums.timedOut') };
+    case 'Rejected':
+      return { icon: 'votingCheckFilled', label: t('governance.referendums.rejected') };
+    case 'Cancelled':
+      return { icon: 'votingCheckFilled', label: t('governance.referendums.canceled') };
+    case 'Killed':
+      return { icon: 'votingCheckFilled', label: t('governance.referendums.killed') };
+  }
+};
+
 type Props = {
   referendum: CompletedReferendum;
-  onReferendumSelect(referendum: Referendum): void;
+  onReferendumSelect(referendum: CompletedReferendum): void;
 };
 
 export const CompletedReferendumVoting = memo(({ referendum, onReferendumSelect }: Props) => {
@@ -26,14 +42,22 @@ export const CompletedReferendumVoting = memo(({ referendum, onReferendumSelect 
     keys: [referendum.id],
     fn: (votes, [id]) => votes.find(v => v.referendumId === id) ?? null,
   });
+  const type = referendum.type;
+  const label = getStatusLabel(type, t);
 
   return (
     <button className="block w-full appearance-none" onClick={() => onReferendumSelect(referendum)}>
       <Box direction="row" fillContainer padding={4} gap={3}>
         {vote ? <Label variant="gray">{t('governance.voted')}</Label> : null}
-        <SmallTitleText>
-          {meta?.title || t('governance.referendums.referendumTitle', { index: referendum.id })}
-        </SmallTitleText>
+        <Box grow={1}>
+          <SmallTitleText>
+            {meta?.title || t('governance.referendums.referendumTitle', { index: referendum.id })}
+          </SmallTitleText>
+        </Box>
+        <Box direction="row" verticalAlign="center" gap={1}>
+          <Icon className="text-icon-hover" name={label.icon} size={16} />
+          <FootnoteText className="text-text-secondary">{label.label}</FootnoteText>
+        </Box>
       </Box>
     </button>
   );
