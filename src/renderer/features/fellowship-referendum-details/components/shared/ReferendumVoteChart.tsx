dfdbb3e3019b9e150edde2b3688321bf3310d4b1
Anastasiia Sokolova <63446892+sokolova-an@users.noreply.github.com>
2025-03-26 13:12:28 +0300

    fix: improve fellowship vote chart (#3426)

--- src/renderer/features/fellowship-referendum-details/components/shared/ReferendumVoteChart.tsx
+++ src/renderer/features/fellowship-referendum-details/components/shared/ReferendumVoteChart.tsx
@@ -1,34 +1,26 @@
-import { BN_MILLION } from '@polkadot/util';
-import { useGate, useStoreMap } from 'effector-react';
+import { useGate } from 'effector-react';
 import { memo } from 'react';
 
 import { useI18n } from '@/shared/i18n';
-import { nullable } from '@/shared/lib/utils';
+import { nonNullable, nullable } from '@/shared/lib/utils';
 import { HelpText } from '@/shared/ui';
 import { DynamicVoteChart } from '@/shared/ui-entities';
-import { Box, Skeleton, Tooltip } from '@/shared/ui-kit';
+import { Box, Skeleton } from '@/shared/ui-kit';
 import { type Referendum, referendumService } from '@/domains/collectives';
 import { fellowshipReferendumsDetailsFeature } from '../../model/feature';
-import { thresholdsModel } from '../../model/thresholds';
 
 type Props = {
   referendum: Referendum | null;
   pending: boolean;
-  descriptionPosition: 'tooltip' | 'bottom';
   votes?: number | null;
-  highlight?: 'aye' | 'nay' | null;
+  highlight?: 'Aye' | 'Nay' | null;
+  voted?: 'Aye' | 'Nay';
 };
 
-export const ReferendumVoteChart = memo<Props>(({ referendum, pending, descriptionPosition, votes, highlight }) => {
+export const ReferendumVoteChart = memo<Props>(({ referendum, voted, pending, votes, highlight }) => {
   useGate(fellowshipReferendumsDetailsFeature.gate);
 
   const { t } = useI18n();
-
-  const thresholds = useStoreMap({
-    store: thresholdsModel.$thresholds,
-    keys: [referendum?.id],
-    fn: (thresholds, [id]) => (id ? (thresholds[id] ?? null) : null),
-  });
 
   if (nullable(referendum)) {
     if (pending) {
@@ -42,53 +34,39 @@ export const ReferendumVoteChart = memo<Props>(({ referendum, pending, descripti
     }
   }
 
-  if (nullable(thresholds) || referendumService.isCompleted(referendum)) {
+  if (referendumService.isCompleted(referendum)) {
     return null;
   }
 
+  const showDiff = nonNullable(votes) && highlight;
+
   const total = referendum.tally.ayes + referendum.tally.nays;
-  const aye = (referendum.tally.ayes * 100_000) / total / 1000;
-  const nay = (referendum.tally.nays * 100_000) / total / 1000;
-  const votesImpact = votes && highlight ? (votes * 100_000) / total / 1000 : 0;
-  const threshold = thresholds.approval.threshold.div(BN_MILLION).toNumber() / 10;
+  const aye =
+    voted === 'Aye' && showDiff
+      ? ((referendum.tally.ayes - votes) / (total - votes)) * 100
+      : (referendum.tally.ayes / total) * 100;
+
+  // if user wants to revote then votes diff for the referendum x2
+  const voteDiff = showDiff && (nullable(voted) || voted === highlight ? votes : votes * 2);
+  const votesImpact = voteDiff ? (voteDiff / (voted ? total : total + votes)) * 100 : 0;
+
   const disabled = referendum.tally.ayes === 0 && referendum.tally.nays === 0;
 
   const chartNode = (
-    <DynamicVoteChart value={aye} disabled={disabled} votesImpact={highlight === 'nay' ? -votesImpact : votesImpact} />
+    <DynamicVoteChart value={aye} disabled={disabled} votesImpact={highlight === 'Nay' ? -votesImpact : votesImpact} />
   );
 
-  if (descriptionPosition === 'tooltip') {
-    return (
-      <Tooltip side="top">
-        <Tooltip.Trigger>
-          <div>{chartNode}</div>
-        </Tooltip.Trigger>
-        <Tooltip.Content>
-          <span className="text-inherit">{`${t('voteChart.toPass')} ${threshold.toFixed(2)}%`}</span>
-          <br />
-          <span className="text-inherit">{`${t('voteChart.aye')} ${aye.toFixed(2)}%`}</span>
-          <br />
-          <span className="text-inherit">{`${t('voteChart.nay')} ${nay.toFixed(2)}%`}</span>
-        </Tooltip.Content>
-      </Tooltip>
-    );
-  }
-
-  if (descriptionPosition === 'bottom') {
-    return (
-      <div className="flex w-full flex-col">
-        {chartNode}
-        <Box direction="row" horizontalAlign="space-between">
-          <HelpText className="text-text-secondary">
-            {t('voteChart.aye')}: {referendum.tally.ayes}
-          </HelpText>
-          <HelpText className="text-text-secondary">
-            {t('voteChart.nay')}: {referendum.tally.nays}
-          </HelpText>
-        </Box>
-      </div>
-    );
-  }
-
-  return chartNode;
+  return (
+    <div className="flex w-full flex-col">
+      {chartNode}
+      <Box direction="row" horizontalAlign="space-between">
+        <HelpText className="text-text-secondary">
+          {t('voteChart.nay')}: {referendum.tally.nays}
+        </HelpText>
+        <HelpText className="text-text-secondary">
+          {t('voteChart.aye')}: {referendum.tally.ayes}
+        </HelpText>
+      </Box>
+    </div>
+  );
 });
