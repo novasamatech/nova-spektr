8ae623199948e597ef2a1810f41faba8c8becc2f
Yaroslav Grachev <tuulwq@gmail.com>
2025-03-27 16:49:13 +0300

    feat: vote confirms

--- src/renderer/domains/collectives/tracks/service.ts
+++ src/renderer/domains/collectives/tracks/service.ts
@@ -8,18 +8,18 @@ import { type Tally } from '../referendum/types';
 
 import { type Track, type VotingCurve, type VotingThreshold } from './types';
 
-function isRetentionTrack(track: TrackId) {
+const isRetentionTrack = (track: TrackId) => {
   return track >= 11 && track <= 16;
-}
+};
 
-function isPromotionTrack(track: TrackId) {
+const isPromotionTrack = (track: TrackId) => {
   return (
     // Promotion
     (track >= 21 && track <= 26) ||
     // Fast promotion
     (track >= 31 && track <= 36)
   );
-}
+};
 
 /**
  * @see https://github.com/paritytech/polkadot-sdk/blob/master/cumulus/parachains/runtimes/collectives/collectives-westend/src/fellowship/tracks.rs#L63
--- src/renderer/features/fellowship-voting/components/VotingConfirmation.tsx
+++ src/renderer/features/fellowship-voting/components/VotingConfirmation.tsx
@@ -2,18 +2,21 @@ import { type BN } from '@polkadot/util';
 
 import { type Asset, type Chain, type Wallet } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
-import { formatAsset } from '@/shared/lib/utils';
+import { formatAsset, nonNullable, toRomanNumeral } from '@/shared/lib/utils';
 import { DetailRow, Icon, Separator } from '@/shared/ui';
 import { TransactionDetails } from '@/shared/ui-entities';
 import { Box } from '@/shared/ui-kit';
-import { type OngoingReferendum, trackService } from '@/domains/collectives';
+import { type Member, type OngoingReferendum, type Track, trackService } from '@/domains/collectives';
 import { type AnyAccount } from '@/domains/network';
 
 type Props = {
   account: AnyAccount;
   rank: number;
   maxRank: number;
+  currentTrack: Track;
+  nextTrack: Track | null;
   referendum: OngoingReferendum;
+  proposer: Member | null;
   wallets: Wallet[];
   chain: Chain;
   asset: Asset;
@@ -21,10 +24,27 @@ type Props = {
   fee: BN;
 };
 
-export const VotingConfirmation = ({ fee, account, wallets, chain, asset, vote, rank, maxRank, referendum }: Props) => {
+export const VotingConfirmation = ({
+  fee,
+  account,
+  wallets,
+  currentTrack,
+  nextTrack,
+  chain,
+  asset,
+  vote,
+  rank,
+  maxRank,
+  referendum,
+  proposer,
+}: Props) => {
   const { t } = useI18n();
 
   const votes = trackService.getVoteWeight({ pallet: 'fellowship', rank, maxRank, track: referendum.track });
+
+  const isPromotionTrack = trackService.isPromotionTrack(referendum.track);
+  const isRetentionTrack = trackService.isRetentionTrack(referendum.track);
+  const isAnotherTrack = !isRetentionTrack && !isPromotionTrack;
 
   return (
     <Box gap={6}>
@@ -32,14 +52,29 @@ export const VotingConfirmation = ({ fee, account, wallets, chain, asset, vote, 
         <Icon className="text-icon-default" name="voteMst" size={60} />
 
         <span className="font-manrope text-[32px] font-bold leading-[36px] text-text-primary">
-          {t('governance.referendum.votes', {
-            votes: votes,
-            count: votes,
-          })}
+          {t('governance.referendum.votes', { votes, count: votes })}
         </span>
       </Box>
 
       <TransactionDetails wallets={wallets} chain={chain} initiator={[account]} signatory={null}>
+        <DetailRow label={t('fellowship.voting.confirmation.referendumID')}>{referendum.id}</DetailRow>
+        <DetailRow label={t('fellowship.voting.confirmation.referendumType')}>
+          {isPromotionTrack && t('fellowship.voting.confirmation.promotionTrack')}
+          {isRetentionTrack && t('fellowship.voting.confirmation.retentionTrack')}
+          {isAnotherTrack && currentTrack.name}
+        </DetailRow>
+        {nonNullable(proposer) && nonNullable(nextTrack) && isPromotionTrack && (
+          <DetailRow label={t('fellowship.voting.confirmation.rank')}>
+            {`${toRomanNumeral(proposer.rank)} ${currentTrack.name.replace(/s$/, '')}`}
+            &nbsp;{'→'}&nbsp;
+            {`${toRomanNumeral(proposer.rank + 1)} ${nextTrack.name.replace(/s$/, '')}`}
+          </DetailRow>
+        )}
+        {nonNullable(proposer) && isRetentionTrack && (
+          <DetailRow label={t('fellowship.voting.confirmation.rank')}>
+            {`${toRomanNumeral(proposer.rank)} ${currentTrack.name.replace(/s$/, '')}`}
+          </DetailRow>
+        )}
         <DetailRow label={t('fellowship.voting.confirmation.vote')}>{t(`fellowship.voting.${vote}`)}</DetailRow>
         <Separator className="border-filter-border" />
         <DetailRow label={t('fellowship.voting.confirmation.fee')}>{formatAsset(fee, asset)}</DetailRow>
