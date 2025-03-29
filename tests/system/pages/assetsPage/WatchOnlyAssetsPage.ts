a39e46a52a0a8ccd8bd0efbd6dd66389c94f9f9c
Fedor Kiselev <fedor@novasama.io>
2025-03-29 18:06:00 +0200

    Added tests for transfer urls

--- tests/system/pages/assetsPage/WatchOnlyAssetsPage.ts
+++ tests/system/pages/assetsPage/WatchOnlyAssetsPage.ts
@@ -6,6 +6,9 @@ import { SettingsPageElements } from '../_elements/SettingsPageElements';
 import { WalletModalElements } from '../_elements/WalletModalElements';
 import { WalletModalWindow } from '../modals/WalletModalWindow';
 import { BaseSettingsPage } from '../settingsPage/BaseSettingsPage';
+import { TransferModalWindow } from '../modals/TransferModalWindow';
+import { ChainModel } from 'tests/system/data/chains/testChainModel';
+import { TransferModalElements } from '../_elements/TransferModalElements';
 
 export class WatchOnlyAssetsPage extends BasePage<AssetsPageElements> {
   public async goToSettingsPage(): Promise<BaseSettingsPage> {
@@ -20,8 +23,19 @@ export class WatchOnlyAssetsPage extends BasePage<AssetsPageElements> {
 
   public async checkTransferButtonNotExists(): Promise<WatchOnlyAssetsPage> {
     const sendButton = this.page.getByTestId(TEST_IDS.ASSETS.SEND_ARROW_ICON).first();
-    await expect(sendButton).toHaveCount(0);
+    await expect(sendButton).not.toBeVisible();
 
     return this;
+  }
+
+  public async checkReceiveButtonNotExists(): Promise<WatchOnlyAssetsPage> {
+    const receiveButton = this.page.getByTestId(TEST_IDS.ASSETS.RECEIVE_ARROW_ICON).first();
+    await expect(receiveButton).not.toBeVisible();
+
+    return this;
+  }
+
+  public async openTransferByUrl(chain: ChainModel, assetId: number): Promise<TransferModalWindow> {
+    return new TransferModalWindow(this.page, new TransferModalElements(), this, chain, assetId).openTransferModalByUrl();
   }
 }
--- tests/system/tests/transfers/watch.only.transfer.system.test.ts
+++ tests/system/tests/transfers/watch.only.transfer.system.test.ts
@@ -1,17 +1,67 @@
 import * as allure from 'allure-js-commons';
-import { test } from '../../utils/baseRegularFixture';
-
+import { test, expect } from '../../utils/baseRegularFixture';
+import { getChainByName } from '../../utils/readConfig';
+import { substrateChains } from '../../data/chains/chainsList';
 
 const feature = 'Wallets. Watch-only. Single wallet';
 const story = 'Transfers';
 
+const transferTestCases = [
+  {
+    chainName: 'Polkadot',
+    assetId: 0,
+    amount: '0.1',
+    recipient: '13mAjFVjFDpfa42k2dLdSnUyrSzK8vAySsoudnxX2EKVtfaq',
+  },
+  {
+    chainName: 'Kusama',
+    assetId: 0,
+    amount: '0.01',
+    recipient: 'FLVFEaY1oa7tAqfqh6gCb1q9RGuFHS1pkvAsAF7wwWUTFxY',
+  },
+  {
+    chainName: 'Polkadot Asset Hub',
+    assetId: 0,
+    amount: '0.01',
+    recipient: '13mAjFVjFDpfa42k2dLdSnUyrSzK8vAySsoudnxX2EKVtfaq',
+  },
+  {
+    chainName: 'Hydration',
+    assetId: 0,
+    amount: '0.01',
+    recipient: '7LMj1kc8TYvTQkWy6Am8EZEka1zqbTqmL8iBPrUVC6nDcoo6',
+  },
+];
+
 test.describe('Watch-only transfers', { tag: ['@watch-only-transfers', '@regress'] }, () => {
-  test('Watch-only transfer', async ({ loginPage }) => {
+  test('Watch-only transfer buttons should not be visible', async ({ loginPage }) => {
     await allure.feature(feature);
     await allure.story(story);
     const watchOnlyWallet = await loginPage.createBaseWatchOnlyWallet();
     const assetsPage = await watchOnlyWallet.gotoMain();
-    await assetsPage.checkTransferButtonNotExists();
     
+    try {
+      await assetsPage.checkTransferButtonNotExists();
+    } catch (error) {
+      throw new Error('Transfer button should not be visible for watch-only wallet');
+    }
+
+    try {
+      await assetsPage.checkReceiveButtonNotExists(); 
+    } catch (error) {
+      throw new Error('Receive button should not be visible for watch-only wallet');
+    }
   });
+
+  for (const { chainName, assetId} of transferTestCases) {
+    test(`Should not be able to open transfer modal on ${chainName} in watch-only mode`, async ({ loginPage }) => {
+      await allure.feature(feature);
+      await allure.story(story);
+      const watchOnlyWallet = await loginPage.createBaseWatchOnlyWallet();
+      const assetsPage = await watchOnlyWallet.gotoMain();      
+      const chain = getChainByName(substrateChains, chainName);
+      const transferModal = await assetsPage.openTransferByUrl(chain, assetId);
+      await transferModal.transferModalIsNotVisible();
+    });
+  }
 });
