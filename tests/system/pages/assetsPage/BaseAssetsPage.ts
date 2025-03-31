d7fa41a22fd525d194fc9d5b7dc875e40fb68fdd
Fedor Kiselev <fedor@novasama.io>
2025-03-31 21:11:45 +0300

    Added tests for watch-only transfers + fixes

--- tests/system/pages/assetsPage/BaseAssetsPage.ts
+++ tests/system/pages/assetsPage/BaseAssetsPage.ts
@@ -1,4 +1,27 @@
+import { ChainModel } from 'tests/system/data/chains/testChainModel';
 import { BasePage } from '../BasePage';
 import { type AssetsPageElements } from '../_elements/AssetsPageElements';
+import { TransferModalElements } from '../_elements/TransferModalElements';
+import { TransferModalWindow } from '../modals/TransferModalWindow';
+import { SettingsPageElements } from '../_elements/SettingsPageElements';
+import { BaseSettingsPage } from '../settingsPage/BaseSettingsPage';
+import { WalletModalElements } from '../_elements/WalletModalElements';
+import { WalletModalWindow } from '../modals/WalletModalWindow';
 
-export class BaseAssetsPage extends BasePage<AssetsPageElements> {}
+export class BaseAssetsPage extends BasePage<AssetsPageElements> {
+    public async openTransfer(chain: ChainModel, assetId: number): Promise<TransferModalWindow> {
+        return new TransferModalWindow(this.page, new TransferModalElements(), this, chain, assetId).openTransferModal();
+      }
+    
+    public async goToSettingsPage(): Promise<BaseSettingsPage> {
+        return new BaseSettingsPage(this.page, new SettingsPageElements()).gotoMain();
+      }
+    
+    public async openWalletManagement(): Promise<WalletModalWindow> {
+        await this.click(this.pageElements.accountButton);
+    
+        return new WalletModalWindow(this.page, new WalletModalElements(), this);
+      }
+
+    
+}
