8dc92e42295d086ecb96947d70459eb93064d887
Fedor Kiselev <fedor@novasama.io>
2025-03-31 21:11:45 +0300

    Added tests for watch-only transfers + fixes

--- tests/system/pages/assetsPage/WatchOnlyAssetsPage.ts
+++ tests/system/pages/assetsPage/WatchOnlyAssetsPage.ts
@@ -1,26 +1,11 @@
 import { expect } from '@playwright/test';
 import { TEST_IDS } from '@/shared/constants/testIds';
-import { BasePage } from '../BasePage';
-import { type AssetsPageElements } from '../_elements/AssetsPageElements';
-import { SettingsPageElements } from '../_elements/SettingsPageElements';
-import { WalletModalElements } from '../_elements/WalletModalElements';
-import { WalletModalWindow } from '../modals/WalletModalWindow';
-import { BaseSettingsPage } from '../settingsPage/BaseSettingsPage';
 import { TransferModalWindow } from '../modals/TransferModalWindow';
 import { ChainModel } from 'tests/system/data/chains/testChainModel';
 import { TransferModalElements } from '../_elements/TransferModalElements';
+import { BaseAssetsPage } from './BaseAssetsPage';
 
-export class WatchOnlyAssetsPage extends BasePage<AssetsPageElements> {
-  public async goToSettingsPage(): Promise<BaseSettingsPage> {
-    return new BaseSettingsPage(this.page, new SettingsPageElements()).gotoMain();
-  }
-
-  public async openWalletManagement(): Promise<WalletModalWindow> {
-    await this.click(this.pageElements.accountButton);
-
-    return new WalletModalWindow(this.page, new WalletModalElements(), this);
-  }
-
+export class WatchOnlyAssetsPage extends BaseAssetsPage { 
   public async checkTransferButtonNotExists(): Promise<WatchOnlyAssetsPage> {
     const sendButton = this.page.getByTestId(TEST_IDS.ASSETS.SEND_ARROW_ICON).first();
     await expect(sendButton).not.toBeVisible();
@@ -34,8 +19,9 @@ export class WatchOnlyAssetsPage extends BasePage<AssetsPageElements> {
 
     return this;
   }
-
-  public async openTransferByUrl(chain: ChainModel, assetId: number): Promise<TransferModalWindow> {
-    return new TransferModalWindow(this.page, new TransferModalElements(), this, chain, assetId).openTransferModalByUrl();
+  
+  public override async openTransfer(chain: ChainModel, assetId: number): Promise<TransferModalWindow> {
+      return new TransferModalWindow(this.page, new TransferModalElements(), this, chain, assetId).openTransferModalByUrl();
   }
+  
 }
--- tests/system/tests/transfers/watch.only.transfers.system.test.ts
+++ tests/system/tests/transfers/watch.only.transfers.system.test.ts
@@ -1,37 +1,11 @@
 import * as allure from 'allure-js-commons';
-import { test, expect } from '../../utils/baseRegularFixture';
+import { test } from '../../utils/baseRegularFixture';
 import { getChainByName } from '../../utils/readConfig';
 import { substrateChains } from '../../data/chains/chainsList';
+import { transferTestCases } from '../../utils/transferTestCases';
 
 const feature = 'Wallets. Watch-only. Single wallet';
 const story = 'Transfers';
-
-const transferTestCases = [
-  {
-    chainName: 'Polkadot',
-    assetId: 0,
-    amount: '0.1',
-    recipient: '13mAjFVjFDpfa42k2dLdSnUyrSzK8vAySsoudnxX2EKVtfaq',
-  },
-  {
-    chainName: 'Kusama',
-    assetId: 0,
-    amount: '0.01',
-    recipient: 'FLVFEaY1oa7tAqfqh6gCb1q9RGuFHS1pkvAsAF7wwWUTFxY',
-  },
-  {
-    chainName: 'Polkadot Asset Hub',
-    assetId: 0,
-    amount: '0.01',
-    recipient: '13mAjFVjFDpfa42k2dLdSnUyrSzK8vAySsoudnxX2EKVtfaq',
-  },
-  {
-    chainName: 'Hydration',
-    assetId: 0,
-    amount: '0.01',
-    recipient: '7LMj1kc8TYvTQkWy6Am8EZEka1zqbTqmL8iBPrUVC6nDcoo6',
-  },
-];
 
 test.describe('Watch-only transfers', { tag: ['@watch-only-transfers', '@regress'] }, () => {
   test('Watch-only transfer buttons should not be visible', async ({ loginPage }) => {
@@ -39,7 +13,7 @@ test.describe('Watch-only transfers', { tag: ['@watch-only-transfers', '@regress
     await allure.story(story);
     const watchOnlyWallet = await loginPage.createBaseWatchOnlyWallet();
     const assetsPage = await watchOnlyWallet.gotoMain();
-    
+
     try {
       await assetsPage.checkTransferButtonNotExists();
     } catch (error) {
@@ -47,20 +21,20 @@ test.describe('Watch-only transfers', { tag: ['@watch-only-transfers', '@regress
     }
 
     try {
-      await assetsPage.checkReceiveButtonNotExists(); 
+      await assetsPage.checkReceiveButtonNotExists();
     } catch (error) {
       throw new Error('Receive button should not be visible for watch-only wallet');
     }
   });
 
-  for (const { chainName, assetId} of transferTestCases) {
+  for (const { chainName, assetId } of transferTestCases) {
     test(`Should not be able to open transfer modal on ${chainName} in watch-only mode`, async ({ loginPage }) => {
       await allure.feature(feature);
       await allure.story(story);
       const watchOnlyWallet = await loginPage.createBaseWatchOnlyWallet();
-      const assetsPage = await watchOnlyWallet.gotoMain();      
+      const assetsPage = await watchOnlyWallet.gotoMain();
       const chain = getChainByName(substrateChains, chainName);
-      const transferModal = await assetsPage.openTransferByUrl(chain, assetId);
+      const transferModal = await assetsPage.openTransfer(chain, assetId);
       await transferModal.transferModalIsNotVisible();
     });
   }
