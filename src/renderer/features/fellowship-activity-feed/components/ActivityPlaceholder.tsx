c7bd4d3823e080f082a1ed9e52534bab9aea8187
Anton <aaa_eee@icloud.com>
2025-04-01 14:10:50 +0200

    feat: activity list modal
    * feat: activity list modal
    
    * refactor: add titles and placeholders
    
    * refactor: add sorts
    
    * refactor: add account info
    
    * Update src/renderer/features/fellowship-activity-feed/components/ActivityModal.tsx
    
    Co-authored-by: Yaroslav Grachev <tuulwq@gmail.com>
    
    * refactor: reduce nesting
    
    * refactor: use account
    
    ---------
    
    Co-authored-by: Yaroslav Grachev <tuulwq@gmail.com>

---
+++ src/renderer/features/fellowship-activity-feed/components/ActivityPlaceholder.tsx
@@ -0,0 +1,22 @@
+import { BodyText, FootnoteText, HelpText } from '@/shared/ui';
+import { Skeleton } from '@/shared/ui-kit';
+
+export const ActivityPlaceholder = () => {
+  return (
+    <div className="flex flex-col gap-1 px-4 pt-2">
+      <div className="flex items-center gap-1 text-button-small">
+        <div className="flex min-w-0 grow items-center">
+          <BodyText>
+            <Skeleton width="10ch" height="1lh" />
+          </BodyText>
+        </div>
+        <HelpText className="h-fit">
+          <Skeleton height="1em" width="5ch" />
+        </HelpText>
+      </div>
+      <FootnoteText>
+        <Skeleton width="100%" height="1lh" />
+      </FootnoteText>
+    </div>
+  );
+};
