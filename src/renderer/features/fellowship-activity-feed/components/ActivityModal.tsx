2ca65ba9d85f248f0f5c89ecf69f9009ff7a77be
anton.e <anton.efanov@novasama.io>
2025-04-02 15:38:06 +0200

    refactor: search by accountId

--- src/renderer/features/fellowship-activity-feed/components/ActivityModal.tsx
+++ src/renderer/features/fellowship-activity-feed/components/ActivityModal.tsx
@@ -5,7 +5,7 @@ import { type PropsWithChildren, useState } from 'react';
 import { Slot } from '@/shared/di';
 import { useI18n } from '@/shared/i18n';
 import { useDeferredList } from '@/shared/lib/hooks';
-import { nonNullable, performSearch } from '@/shared/lib/utils';
+import { nonNullable, performSearch, truncate } from '@/shared/lib/utils';
 import { Duration, EmptyList, FootnoteText, HelpText } from '@/shared/ui';
 import { Account } from '@/shared/ui-entities';
 import { Modal, SearchInput, Select } from '@/shared/ui-kit';
@@ -50,6 +50,8 @@ export const ActivityModal = ({ children }: PropsWithChildren) => {
     };
   });
 
+  console.log({ records });
+
   const filteredList = performSearch({
     records,
     query,
@@ -58,6 +60,7 @@ export const ActivityModal = ({ children }: PropsWithChildren) => {
       type: 0.5,
       wish: 0.5,
       name: 1,
+      accountId: 1,
     },
   });
 
@@ -110,7 +113,9 @@ export const ActivityModal = ({ children }: PropsWithChildren) => {
           {isLoading && Array.from({ length: 5 }).map((_, i) => <ActivityPlaceholder key={i} />)}
 
           {isNothingFound && (
-            <EmptyList message={t('fellowship.activityFeed.activityModal.nothing-found', { query })} />
+            <EmptyList
+              message={t('fellowship.activityFeed.activityModal.nothing-found', { query: truncate(query, 10, 10) })}
+            />
           )}
 
           {sortedList.map(record => (
