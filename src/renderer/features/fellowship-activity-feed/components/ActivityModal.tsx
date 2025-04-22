3de28dbb11d54895fbae1010678ea8c5e12c715c
anton.e <anton.efanov@novasama.io>
2025-04-22 12:25:41 +0200

    feat: add debounce

--- src/renderer/features/fellowship-activity-feed/components/ActivityModal.tsx
+++ src/renderer/features/fellowship-activity-feed/components/ActivityModal.tsx
@@ -4,7 +4,7 @@ import { type PropsWithChildren, useState } from 'react';
 
 import { Slot } from '@/shared/di';
 import { useI18n } from '@/shared/i18n';
-import { useDeferredList } from '@/shared/lib/hooks';
+import { useDebounce, useDeferredList } from '@/shared/lib/hooks';
 import { AsyncItem, nonNullable, performSearch, toAddress, truncate } from '@/shared/lib/utils';
 import { Button, Duration, EmptyList, FootnoteText, HelpText } from '@/shared/ui';
 import { Account } from '@/shared/ui-entities';
@@ -41,6 +41,8 @@ export const ActivityModal = ({ children }: PropsWithChildren) => {
   const [query, setQuery] = useState('');
   const [orderKey, setOrderKey] = useState<OrderKey | null>(null);
 
+  const debouncedQuery = useDebounce(query, 100);
+
   const clearSearch = () => setQuery('');
 
   const records = list.map(record => {
@@ -55,7 +57,7 @@ export const ActivityModal = ({ children }: PropsWithChildren) => {
 
   const filteredList = performSearch({
     records,
-    query: query,
+    query: debouncedQuery,
     queryMinLength: 3,
     weights: {
       type: 0.5,
@@ -118,7 +120,7 @@ export const ActivityModal = ({ children }: PropsWithChildren) => {
             <EmptyList
               title={t('fellowship.activityFeed.activityModal.nothing-found.title')}
               message={t('fellowship.activityFeed.activityModal.nothing-found.description', {
-                query: truncate(query, 6, 6),
+                query: truncate(debouncedQuery, 6, 6),
               })}
             >
               <Button pallet="primary" variant="text" onClick={clearSearch}>
