dbf2a87a48987a826f55234c919331595081d922
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-04-22 13:26:01 -0300

    fix: hide networks for switched off accounts (Polkadot Vault wallet)

--- src/renderer/features/wallets/index.ts
+++ src/renderer/features/wallets/index.ts
@@ -2,3 +2,5 @@ export { KeyConstructor } from './KeyConstructor';
 export { ShardSelectorModal, ShardSelectorButton } from './ShardSelectorModal';
 export { DerivationsAddressModal } from './DerivationsAddressModal/ui/DerivationsAddressModal';
 export { ImportKeysModal } from './ImportKeys/ui/ImportKeysModal';
+export { shardsModel } from './ShardSelectorModal/model/shards-model';
+export { shardsUtils } from './ShardSelectorModal/lib/shards-utils';
