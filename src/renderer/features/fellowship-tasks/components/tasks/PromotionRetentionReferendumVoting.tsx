b9f0d6d77263c0525cc00fbba3fb152e08a4890b
Anton <aaa_eee@icloud.com>
2025-04-22 18:51:52 +0200

    feat: vote badge (#3616)
    * feat: vote badge
    
    * refactor: upd check
    
    * refactor: mv component
    
    * refactor: add badge for completed
    
    * refactor: vote badge only when voted

--- src/renderer/features/fellowship-tasks/components/tasks/PromotionRetentionReferendumVoting.tsx
+++ src/renderer/features/fellowship-tasks/components/tasks/PromotionRetentionReferendumVoting.tsx
@@ -1,13 +1,16 @@
-import { useUnit } from 'effector-react';
+import { useStoreMap, useUnit } from 'effector-react';
 import { memo } from 'react';
 
 import { type Transaction } from '@/shared/core';
 import { Slot } from '@/shared/di';
 import { useI18n } from '@/shared/i18n';
+import { nonNullable } from '@/shared/lib/utils';
 import { FootnoteText, Markdown, SmallTitleText } from '@/shared/ui';
 import { Box, Label, type LabelVariant, Skeleton } from '@/shared/ui-kit';
 import { type OngoingReferendum, type Referendum, referendumService, trackService } from '@/domains/collectives';
 import { evidenceModel } from '../../model/evidence';
+import { votes } from '../../model/voting';
+import { VoteBadge } from '../VoteBadge/VoteBadge';
 
 import { referendumVotingTaskActionSlot } from './OngoingReferendumVoting';
 
@@ -40,7 +43,13 @@ export const PromotionRetentionReferendumVoting = memo(
     const evidenceSummaryPending = useUnit(evidenceModel.requestEvidenceSummary.pending);
     const evidenceSummaryPopulated = useUnit(evidenceModel.$summaryPopulated);
     const evidenceSummaries = useUnit(evidenceModel.$evidencesSummary);
+    const vote = useStoreMap({
+      store: votes.$memberVotes,
+      keys: [referendum.id],
+      fn: (votes, [id]) => votes.find(v => v.referendumId === id) ?? null,
+    });
 
+    const voted = nonNullable(vote);
     const pending = evidenceSummaryPending || !evidenceSummaryPopulated;
     const proposerAccountId = referendumService.getProposer(referendum);
     const evidenceSummary = evidenceSummaries.find(e => e.accountId === proposerAccountId);
@@ -61,6 +70,7 @@ export const PromotionRetentionReferendumVoting = memo(
             <Box direction="row" gap={3}>
               {labelConfig ? <Label variant={labelConfig.color}>{t(labelConfig.text)}</Label> : null}
               <SmallTitleText className="truncate">{title}</SmallTitleText>
+              {voted && <VoteBadge active />}
             </Box>
             {!evidenceSummary?.summary && <Skeleton height="3lh" width="85%" />}
             <FootnoteText as="div">
