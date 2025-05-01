73dfec00502b41183e8a13c9a4070ac4977b4e1e
cuteWarmFrog <livshits.victor@gmail.com>
2025-05-01 10:13:17 +0100

    fix: removed unused list

--- src/renderer/features/fellowship-tasks/model/feature.ts
+++ src/renderer/features/fellowship-tasks/model/feature.ts
@@ -3,6 +3,7 @@ import { combine, sample } from 'effector';
 import { $features } from '@/shared/config/features';
 import { createFeature } from '@/shared/feature';
 import { nullable } from '@/shared/lib/utils';
+import { feed } from '@/domains/collectives';
 import { fellowshipMember } from '@/aggregates/fellowship-member';
 import { fellowshipNetwork } from '@/aggregates/fellowship-network';
 import { ERROR } from '../constants';
@@ -52,4 +53,14 @@ sample({
   clock: fellowshipNetwork.$isConnected,
   filter: fellowshipNetwork.$isConnected,
   target: fellowshipTasksFeature.restore,
+});
+
+sample({
+  clock: fellowshipTasksFeature.running,
+  target: feed.subscribe,
+});
+
+sample({
+  clock: fellowshipTasksFeature.stopped,
+  target: feed.unsubscribe,
 });
