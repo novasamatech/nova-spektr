25b512c5d818d050b4bdc30d16d6a44e2d3fbdbd
anton.e <anton.efanov@novasama.io>
2025-05-07 18:24:14 +0200

    refactor: delay long list rendering

--- src/renderer/pages/Governance/ui/GovernanceReferendumList.tsx
+++ src/renderer/pages/Governance/ui/GovernanceReferendumList.tsx
@@ -4,7 +4,7 @@ import { Outlet, generatePath, useParams } from 'react-router-dom';
 import { type ReferendumId } from '@/shared/core';
 import { nullable } from '@/shared/lib/utils';
 import { Paths } from '@/shared/routes';
-import { Box } from '@/shared/ui-kit';
+import { AsyncItem, Box } from '@/shared/ui-kit';
 import { InactiveNetwork } from '@/entities/network';
 import { CompletedReferendums, Filters, OngoingReferendums, networkSelectorModel } from '@/features/governance';
 import { navigationModel } from '@/features/navigation';
@@ -59,15 +59,17 @@ export const GovernanceReferendumList = () => {
             mixLoadingWithData={isLoadingState}
             onSelect={({ referendumId }) => navigate(referendumId)}
           />
-          <CompletedReferendums
-            api={network?.api}
-            asset={network?.asset}
-            referendums={completed}
-            isLoading={isLoading}
-            isTitlesLoading={isTitlesLoading}
-            mixLoadingWithData={isLoadingState}
-            onSelect={({ referendumId }) => navigate(referendumId)}
-          />
+          <AsyncItem>
+            <CompletedReferendums
+              api={network?.api}
+              asset={network?.asset}
+              referendums={completed}
+              isLoading={isLoading}
+              isTitlesLoading={isTitlesLoading}
+              mixLoadingWithData={isLoadingState}
+              onSelect={({ referendumId }) => navigate(referendumId)}
+            />
+          </AsyncItem>
         </Box>
       )}
 
