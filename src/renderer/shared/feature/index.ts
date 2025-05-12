3d272b00c4050962e4ceaaf89facd17a19febc19
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-05-12 12:21:35 +0200

    feat: migrate feature init to dynamic imports for improved bootstrap performance

--- src/renderer/shared/feature/index.ts
+++ src/renderer/shared/feature/index.ts
@@ -1,3 +1,3 @@
-export { createFeature, type Feature } from './createFeature';
+export { createFeature, isFeature, type Feature } from './createFeature';
 export { attachToFeatureInput } from './attachToFeatureInput';
 export { registerFeatures } from './registerFeatures';
