aaeb507ed01891fb80e0d691993cb0a292d5560e
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-05-21 14:41:25 -0300

    feat: export pv dynamic derivations via QR code

--- src/renderer/features/wallets/index.ts
+++ src/renderer/features/wallets/index.ts
@@ -2,5 +2,6 @@ export { KeyConstructor } from './KeyConstructor';
 export { ShardSelectorModal, ShardSelectorButton } from './ShardSelectorModal';
 export { DerivationsAddressModal } from './DerivationsAddressModal/ui/DerivationsAddressModal';
 export { ImportKeysModal } from './ImportKeys/ui/ImportKeysModal';
+export { ExportKeysModal } from './ExportKeys';
 export { shardsModel } from './ShardSelectorModal/model/shards-model';
 export { shardsUtils } from './ShardSelectorModal/lib/shards-utils';
