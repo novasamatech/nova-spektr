523d99a27e7881f087038d9059fe2f9fd6578d35
Vitya Livshits <64482828+cuteWarmFrog@users.noreply.github.com>
2025-05-22 15:48:00 +0200

    Fix/fellowship layout fixes
    * fix: add request retention task action and improve layout consistency
    
    * refactor: remove unused salaryService and simplify disabled logic in RetentionInfo component
    
    * fix: small layout fixes
    
    * fix: removed vote badge
    
    * refactor: clean up unused imports and enable high priority info slot in ReferendumDetailsModal
    
    * refactor: clean up unused imports and enable high priority info slot in ReferendumDetailsModal
    
    * fix: added revote voting buttons layout

--- src/renderer/features/fellowship-voting/components/VotingButtons.tsx
+++ src/renderer/features/fellowship-voting/components/VotingButtons.tsx
@@ -3,7 +3,7 @@ import { type PropsWithChildren, memo, useState } from 'react';
 
 import { useFlow } from '@/shared/effector';
 import { useI18n } from '@/shared/i18n';
-import { nonNullable, nullable } from '@/shared/lib/utils';
+import { cnTw, nonNullable, nullable } from '@/shared/lib/utils';
 import { ButtonCard, FootnoteText, type IconNames } from '@/shared/ui';
 import { Box, Tooltip } from '@/shared/ui-kit';
 import { type Evidence, type Referendum, referendumService, trackService } from '@/domains/collectives';
@@ -44,8 +44,9 @@ export const VotingButtons = memo(({ referendum }: Props) => {
 
   const buttonDiabled = !canVote || !hasRequiredRank;
 
-  const renderAyeButton = nullable(voting) || voting.decision !== 'Aye';
-  const renderNayButton = nullable(voting) || voting.decision !== 'Nay';
+  const alreadyVotedNay = nonNullable(voting) && voting.decision === 'Nay';
+  const alreadyVotedAye = nonNullable(voting) && voting.decision === 'Aye';
+  const isVoted = alreadyVotedNay || alreadyVotedAye;
 
   const memberVoteWeight = trackService.getVoteWeight({
     pallet: 'fellowship',
@@ -66,31 +67,33 @@ export const VotingButtons = memo(({ referendum }: Props) => {
 
       <Box gap={4}>
         <Box direction="row" gap={4}>
-          {renderNayButton ? (
-            <ButtonWithTooltip
-              pallet="negative"
-              icon="negative"
-              disabled={buttonDiabled}
-              votes={memberVoteWeight}
-              voteImpact={userVotesImpact}
-              onClick={() => setDecision('nay')}
-            >
-              {t('fellowship.voting.notGood')}
-            </ButtonWithTooltip>
-          ) : null}
+          <ButtonWithTooltip
+            pallet="negative"
+            icon="negative"
+            disabled={buttonDiabled}
+            votes={memberVoteWeight}
+            voteImpact={userVotesImpact}
+            isVoted={isVoted}
+            marked={alreadyVotedNay}
+            onClick={() => setDecision('nay')}
+          >
+            {alreadyVotedAye ? t('fellowship.voting.revote') : null}
+            {t('fellowship.voting.notGood')}
+          </ButtonWithTooltip>
 
-          {renderAyeButton ? (
-            <ButtonWithTooltip
-              pallet="positive"
-              icon="positive"
-              disabled={buttonDiabled}
-              votes={memberVoteWeight}
-              voteImpact={userVotesImpact}
-              onClick={() => setDecision('aye')}
-            >
-              {t('fellowship.voting.good')}
-            </ButtonWithTooltip>
-          ) : null}
+          <ButtonWithTooltip
+            pallet="positive"
+            icon="positive"
+            disabled={buttonDiabled}
+            votes={memberVoteWeight}
+            voteImpact={userVotesImpact}
+            isVoted={isVoted}
+            marked={alreadyVotedAye}
+            onClick={() => setDecision('aye')}
+          >
+            {alreadyVotedNay ? t('fellowship.voting.revote') : null}
+            {t('fellowship.voting.good')}
+          </ButtonWithTooltip>
         </Box>
 
         {canVote && !hasRequiredRank ? (
@@ -108,6 +111,8 @@ type ButtonTooltips = {
   voteImpact: number;
   icon: IconNames;
   onClick: () => void;
+  isVoted: boolean;
+  marked: boolean;
 };
 
 export const ButtonWithTooltip = ({
@@ -118,6 +123,8 @@ export const ButtonWithTooltip = ({
   voteImpact,
   icon,
   children,
+  isVoted,
+  marked,
 }: PropsWithChildren<ButtonTooltips>) => {
   const { t } = useI18n();
 
@@ -128,7 +135,18 @@ export const ButtonWithTooltip = ({
     <Tooltip>
       <Tooltip.Trigger>
         <div className="w-full">
-          <ButtonCard pallet={pallet} icon={icon} disabled={disabled} fullWidth onClick={onClick}>
+          <ButtonCard
+            pallet={pallet}
+            icon={icon}
+            disabled={disabled}
+            fullWidth
+            className={cnTw(
+              { 'bg-secondary-positive-button-background-active': marked && pallet === 'positive' },
+              { 'bg-secondary-negative-button-background-active': marked && pallet === 'negative' },
+              { 'px-2': isVoted },
+            )}
+            onClick={onClick}
+          >
             {children}
           </ButtonCard>
         </div>
