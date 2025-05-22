52762f9d3ce0774db6b6ac2c84f8e8430acd8221
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-05-22 17:29:21 -0300

    fix: filter out ethereum addresses from export via QR
    polkadot vault QR code scanner throws an error for QR codes with ethereum addresses

--- src/renderer/entities/transaction/ui/QrCode/QrGenerator/common/utils.ts
+++ src/renderer/entities/transaction/ui/QrCode/QrGenerator/common/utils.ts
@@ -182,12 +182,14 @@ export const createDynamicDerivationExportPayload = (
           MultiSigner: CryptoTypeString.SR25519,
           public: decodeAddress(publicKey, false, 1),
         },
-        derivedKeys: derivations.map((d) => ({
-          address: encodeAddress(d.accountId, chains[d.chainId].addressPrefix),
-          derivationPath: d.derivationPath,
-          genesisHash: hexToU8a(d.chainId),
-          encryption: cryptoTypeToMultisignerIndex(d.cryptoType),
-        })),
+        derivedKeys: derivations
+          .filter((d) => d.cryptoType !== CryptoType.ETHEREUM)
+          .map((d) => ({
+            address: encodeAddress(d.accountId, chains[d.chainId].addressPrefix),
+            derivationPath: d.derivationPath,
+            genesisHash: hexToU8a(d.chainId),
+            encryption: cryptoTypeToMultisignerIndex(d.cryptoType),
+          })),
       },
     ],
   });
