fc46bd683c8351883bfd535d62986d615bd0a546
Fedor Kiselev <fedor@novasama.io>
2025-06-09 22:21:21 +0300

    Fix

--- tests/system/tests/transfers/xcm.transfers.system.test.ts
+++ tests/system/tests/transfers/xcm.transfers.system.test.ts
@@ -9,22 +9,22 @@ const feature = 'Wallets. XCM transfers.';
 const story = 'Transfers';
 
 test.describe('XCM transfers', { tag: ['@xcm-transfers', '@regress1'] }, () => {
-    {
+  {
     test(`Polkadot Vault, single wallet, can make regular xcm transfer on asset-hub`, async ({ loginPage }) => {
-        await allure.feature(feature);
-        await allure.story(story);
-        test.slow();
-        const vaultWallet = await loginPage.importDatabase('transfers/pv-root-polkadot.json');
-        const assetsPage = await vaultWallet.gotoMain();
-        const chain = getChainByName(substrateChains, "Polkadot");
-        const transferModal = await assetsPage.openTransfer(chain, 0);
-        await transferModal.chooseXcmChain('Polkadot Asset Hub Polkadot');
-        await transferModal.fillAmount('1');
-        await transferModal.fillRecipient('14tqRfiFfYeB12o7d9YK4X9LiCMKLNCt1LoVdWZypWbwNp2Y');
-        const confirmationModal = await transferModal.openConfirmationModal();
-        const signingModal = await confirmationModal.confirm();
+      await allure.feature(feature);
+      await allure.story(story);
+      test.slow();
+      const vaultWallet = await loginPage.importDatabase('transfers/pv-root-polkadot.json');
+      const assetsPage = await vaultWallet.gotoMain();
+      const chain = getChainByName(substrateChains, 'Polkadot');
+      const transferModal = await assetsPage.openTransfer(chain, 0);
+      await transferModal.chooseXcmChain('Polkadot Asset Hub Polkadot');
+      await transferModal.fillAmount('1');
+      await transferModal.fillRecipient('14tqRfiFfYeB12o7d9YK4X9LiCMKLNCt1LoVdWZypWbwNp2Y');
+      const confirmationModal = await transferModal.openConfirmationModal();
+      const signingModal = await confirmationModal.confirm();
 
-        await signingModal.checkQRCode();
+      await signingModal.checkQRCode();
     });
-    }
+  }
 });
