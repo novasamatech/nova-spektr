d1f4f5b236d1935336c00d2cface24f5aa0a65c4
sokolova-an <dev.anastasis@gmail.com>
2025-06-10 11:19:40 +0200

    fix: update import

--- src/renderer/widgets/Staking/Unstake/index.ts
+++ src/renderer/widgets/Staking/Unstake/index.ts
@@ -1,4 +1,4 @@
 export { Unstake } from './default/components/Unstake';
 export { unstakeModel } from './default/model/unstake-model';
 export { unstakeModel as unstakeModelShards } from './shards/model/unstake-model';
-export { Unstake as UnstakeShards } from './shards/ui/Unstake';
+export { Unstake as UnstakeShards } from './shards/components/Unstake';
