49e70b5a58ad86dd747a7d62566de6b23eceb871
cuteWarmFrog <livshits.victor@gmail.com>
2025-06-10 13:43:07 +0200

    feat: added shards branching

--- src/renderer/pages/Staking/ui/Staking.tsx
+++ src/renderer/pages/Staking/ui/Staking.tsx
@@ -44,6 +44,16 @@ const LazyBondExtraShards = lazy(() =>
   import('@/widgets/Staking').then(({ BondExtraShards }) => ({ default: BondExtraShards })),
 );
 
+const LazyWithdrawShards = lazy(() =>
+  import('@/widgets/Staking').then(({ WithdrawShards }) => ({ default: WithdrawShards })),
+);
+
+const LazyWithdraw = lazy(() => import('@/widgets/Staking').then(({ Withdraw }) => ({ default: Withdraw })));
+
+const LazyPayee = lazy(() => import('@/widgets/Staking').then(({ Payee }) => ({ default: Payee })));
+
+const LazyPayeeShards = lazy(() => import('@/widgets/Staking').then(({ PayeeShards }) => ({ default: PayeeShards })));
+
 export const Staking = () => {
   const { t } = useI18n();
 
@@ -296,7 +306,10 @@ export const Staking = () => {
         totalStakes.length > 1
           ? Operations.withdrawShardsModel.events.flowStarted
           : Operations.withdrawModel.events.flowStarted,
-      [StakeOperations.SET_PAYEE]: Operations.payeeModel.events.flowStarted,
+      [StakeOperations.SET_PAYEE]:
+        selectedNominators.length > 1
+          ? Operations.payeeModelShards.events.flowStarted
+          : Operations.payeeModel.events.flowStarted,
     };
 
     model[operation]({
@@ -399,8 +412,8 @@ export const Staking = () => {
       <Operations.Unstake />
       <Operations.Nominate />
       <Operations.Restake />
-      {totalStakes.length > 1 ? <Operations.WithdrawShards /> : <Operations.Withdraw />}
-      <Operations.Payee />
+      <Suspense fallback={null}>{totalStakes.length > 1 ? <LazyWithdrawShards /> : <LazyWithdraw />}</Suspense>
+      <Suspense fallback={null}>{selectedNominators.length > 1 ? <LazyPayeeShards /> : <LazyPayee />}</Suspense>
     </>
   );
 };
--- src/renderer/widgets/Staking/index.ts
+++ src/renderer/widgets/Staking/index.ts
@@ -1,7 +1,7 @@
 export { BondNominate, bondNominateModel } from './BondNominate';
 export { BondExtra, bondExtraModel, BondExtraShards, bondExtraShardsModel } from './BondExtra';
 export { Nominate, nominateModel } from './Nominate';
-export { Payee, payeeModel } from './Payee';
+export { Payee, payeeModel, PayeeShards, payeeModelShards } from './Payee';
 export { Restake, restakeModel } from './Restake';
 export { Withdraw, withdrawModel, WithdrawShards, withdrawShardsModel } from './Withdraw';
 export { Unstake, unstakeModel } from './Unstake';
