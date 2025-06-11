ded9a2a1973353ea6001fcabd2a4e9e15ebcff94
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-06-10 22:59:30 -0300

    refactor: isMultisig is redundant

--- src/renderer/widgets/Staking/Restake/default/ui/RestakeForm.tsx
+++ src/renderer/widgets/Staking/Restake/default/ui/RestakeForm.tsx
@@ -78,7 +78,6 @@ const Signatories = () => {
 
   const signatories = useUnit(formModel.$signatories);
   const network = useUnit(formModel.$networkStore);
-  const isMultisig = useUnit(formModel.$isMultisig);
   const balances = useUnit(balanceModel.$balances);
 
   const signatoriesWithBalance = useMemo(() => {
@@ -93,7 +92,7 @@ const Signatories = () => {
     });
   }, [signatories, balances, network]);
 
-  if (!isMultisig || !network) {
+  if (!network) {
     return null;
   }
 
