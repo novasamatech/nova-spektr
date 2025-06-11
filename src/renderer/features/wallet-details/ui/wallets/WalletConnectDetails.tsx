7b753f37756a9263d45f4243a4ccd3946dcde47b
anton.e <anton.efanov@novasama.io>
2025-06-11 14:13:22 +0300

    refactor(cas): add test buttons for wallet connect

--- src/renderer/features/wallet-details/ui/wallets/WalletConnectDetails.tsx
+++ src/renderer/features/wallet-details/ui/wallets/WalletConnectDetails.tsx
@@ -131,7 +131,9 @@ export const WalletConnectDetails = ({ wallet, onClose }: Props) => {
   };
 
   // ToDo: implement acc selector in modal, here we have 1 acc per chain
-  const firstAccount = wallet.accounts.at(0);
+  const pkdtAccount = wallet.accounts.find(
+    a => a.chainId === '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
+  );
   const kusamaAccount = wallet.accounts.find(
     a => a.chainId === '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe',
   );
@@ -154,8 +156,15 @@ export const WalletConnectDetails = ({ wallet, onClose }: Props) => {
               />
             </WalletCardLg>
 
-            <div>{t('This is for testing purpose, only first kusama account')}</div>
+            <hr />
+
+            <div>{t('This is for testing purpose, for Kusama account')}</div>
             {features.accountsStructure && kusamaAccount && <AccountsStructureModal account={kusamaAccount} />}
+
+            <hr />
+
+            <div>{t('This is for testing purpose, for Polkadot account')}</div>
+            {features.accountsStructure && pkdtAccount && <AccountsStructureModal account={pkdtAccount} />}
           </div>
         </Modal.HeaderContent>
         <Modal.Content disableScroll>
