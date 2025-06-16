c15d9ba08113caba8e3a4c04c361432d5acc5bf4
anton.e <anton.efanov@novasama.io>
2025-06-16 10:09:10 +0300

    fix(cas): lint

--- src/renderer/widgets/AccountsStructureModal/ui/AccountStructureNode.tsx
+++ src/renderer/widgets/AccountsStructureModal/ui/AccountStructureNode.tsx
@@ -2,19 +2,19 @@ import { Handle, Position, useNodeConnections } from '@xyflow/react';
 import { useMemo } from 'react';
 
 import { AccountType, SigningType } from '@/shared/core';
-import { toAddress } from '@/shared/lib/utils';
+import { cnTw, toAddress } from '@/shared/lib/utils';
 import { SmallTitleText } from '@/shared/ui/Typography';
 import { Address } from '@/shared/ui-entities/Address/Address';
 import { type AnyAccount } from '@/domains/network';
 import { accountUtils } from '@/entities/wallet';
 
-interface AccountStructureNodeProps {
+type AccountStructureNodeProps = {
   data: {
     account: AnyAccount;
     isSelected: boolean;
   };
   id: string;
-}
+};
 
 type AccountStyle = {
   stripeBackground: string;
@@ -22,56 +22,47 @@ type AccountStyle = {
 };
 
 function getAccountStyle(account: AnyAccount): AccountStyle {
-  // ToDo: improve check
   if ('accountType' in account) {
     switch (account.accountType) {
       case AccountType.PROXIED:
         return {
-          // stripeBackground: 'linear-gradient(217deg, #23B4F2 11.88%, #2A0FD2 57.52%, #8A00CC 85.97%)',
-          stripeBackground: '#23B4F2',
-          titleBackground: '#23B4F2',
+          stripeBackground: 'var(--label-lightblue-default)',
+          titleBackground: 'var(--label-lightblue-default)',
         };
       case AccountType.FLEXIBLE_MULTISIG:
         return {
-          // stripeBackground: 'linear-gradient(221deg, #8707D5 13.45%, #FF6928 86.32%)',
-          stripeBackground: '#8707D5',
-          titleBackground: '#8707D5',
+          stripeBackground: 'var(--label-purple-default)',
+          titleBackground: 'var(--label-purple-default)',
         };
       case AccountType.MULTISIG:
         return {
-          // stripeBackground: 'linear-gradient(223deg, #D4FF59 -17.82%, #D4FF59 55.03%, #1AB775 100.43%)',
-          stripeBackground: '#00AF9A',
-          titleBackground: '#00AF9A',
+          stripeBackground: 'var(--label-background-green)',
+          titleBackground: 'var(--label-background-green)',
         };
     }
   }
 
-  // Then check signing type
   switch (account.signingType) {
     case SigningType.POLKADOT_VAULT:
-      return {
-        stripeBackground: '#EC007D',
-        titleBackground: '#EC007D',
-      };
     case SigningType.PARITY_SIGNER:
       return {
-        stripeBackground: '#EC007D',
-        titleBackground: '#EC007D',
+        stripeBackground: 'var(--badge-red-background)',
+        titleBackground: 'var(--badge-red-background)',
       };
     case SigningType.EXTENSION:
       return {
-        stripeBackground: '#FF8C00',
-        titleBackground: '#FF8C00',
+        stripeBackground: 'var(--badge-orange-background-default)',
+        titleBackground: 'var(--badge-orange-background-default)',
       };
     case SigningType.WALLET_CONNECT:
       return {
-        stripeBackground: '#3B99FC',
-        titleBackground: '#3B99FC',
+        stripeBackground: 'var(--badge-background)',
+        titleBackground: 'var(--badge-background)',
       };
     default:
       return {
-        stripeBackground: '#C3C3CB',
-        titleBackground: '#C3C3CB',
+        stripeBackground: 'var(--icon-default)',
+        titleBackground: 'var(--icon-default)',
       };
   }
 }
@@ -98,7 +89,10 @@ export const AccountStructureNode = ({ data, id }: AccountStructureNodeProps) =>
           <div className="flex flex-col">
             <div style={{ background: data.isSelected ? titleBackground : 'transparent' }}>
               <SmallTitleText
-                className={`border-stroke border-b px-4 py-2 ${data.isSelected ? 'text-white' : 'text-text-secondary'}`}
+                className={cnTw(
+                  'border-stroke border-b px-4 py-2',
+                  data.isSelected ? 'text-white' : 'text-text-secondary',
+                )}
               >
                 {getAccountType(data.account)}
               </SmallTitleText>
@@ -114,21 +108,7 @@ export const AccountStructureNode = ({ data, id }: AccountStructureNodeProps) =>
             </div>
           </div>
 
-          {hasOutgoing && (
-            <Handle
-              type="source"
-              position={Position.Right}
-              className="h-[6px] w-[6px] rounded-full border-2 border-[#363643] bg-transparent"
-              style={{
-                background: '#F9F9F9',
-                border: '2px solid #363643',
-                borderRadius: '4px',
-                width: '6px',
-                height: '6px',
-                // transform: 'translate(1px, -5px)', // this moves the edge origin too
-              }}
-            />
-          )}
+          {hasOutgoing && <Handle type="source" position={Position.Right} className="opacity-0" />}
         </div>
       </div>
     </>
@@ -146,7 +126,6 @@ function getAccountType(account: AnyAccount) {
     return 'Multisig';
   }
 
-  // Show wallet type based on signingType
   switch (account.signingType) {
     case SigningType.POLKADOT_VAULT:
       return 'Vault';
