9aa3cb32efa6852313e443f8cd37b0b5f72d641c
Vitya Livshits <64482828+cuteWarmFrog@users.noreply.github.com>
2025-06-19 12:05:48 +0200

    fix: response code fix (#3953)

--- src/renderer/domains/collectives/evidence/resource.ts
+++ src/renderer/domains/collectives/evidence/resource.ts
@@ -66,7 +66,7 @@ export const evidenceContentResource = createRemoteResource<EvidenceContentReque
     if (evidence?.evidence) {
       const response = await fetch(evidenceService.getEvidenceIpfsUrl(evidence.evidence.value));
 
-      if (response.status <= 200 || response.status >= 300) {
+      if (response.status < 200 || response.status >= 300) {
         return null;
       }
       const content = await response.text();
