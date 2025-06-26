2b7486d505a175e72a13cc680cbd5e9c4ae37489
cuteWarmFrog <livshits.victor@gmail.com>
2025-06-26 16:38:10 +0200

    fix: choosing default signatories

--- src/renderer/widgets/Staking/Withdraw/default/model/form-model.ts
+++ src/renderer/widgets/Staking/Withdraw/default/model/form-model.ts
@@ -318,6 +318,14 @@ sample({
 
 sample({
   clock: formInitiated,
+  source: $signatories,
+  filter: (signatories) => signatories.length === 1,
+  fn: (signatories) => signatories.at(0) ?? null,
+  target: form.fields.signatory.change,
+});
+
+sample({
+  clock: formInitiated,
   source: {
     networkStore: $networkStore,
     api: $api,
