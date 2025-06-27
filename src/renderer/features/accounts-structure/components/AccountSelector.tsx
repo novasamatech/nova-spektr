7fe2dc361d7c0630c23e66896fd55165c95e44bc
Anton <aaa_eee@icloud.com>
2025-06-27 13:45:15 +0300

    refactor(cas): filter chains by graph support
    * feat(cas): filter chains by graph support
    
    * feat(cas): filter chains by graph support
    
    * refactor(cas): simplify
    
    * Revert "refactor(cas): simplify"
    
    This reverts commit d79861381dff4ecedaac597d34b7ddb4da566360.
    
    * refactor(cas): simplify

--- src/renderer/features/accounts-structure/components/AccountSelector.tsx
+++ src/renderer/features/accounts-structure/components/AccountSelector.tsx
@@ -3,31 +3,27 @@ import { useUnit } from 'effector-react';
 import { toAddress } from '@/shared/lib/utils';
 import { Address } from '@/shared/ui-entities';
 import { Select } from '@/shared/ui-kit';
-import { type AnyAccount, accountService } from '@/domains/network';
-import { networkModel } from '@/entities/network';
+import { accountService } from '@/domains/network';
 import { accountsStructureModel } from '../model/accountsStructureModel';
 
-type AccountSelector = {
-  walletAccounts: AnyAccount[];
-};
-
-export const AccountSelector = ({ walletAccounts }: AccountSelector) => {
+export const AccountSelector = () => {
   const selectedAccount = useUnit(accountsStructureModel.$selectedAccount);
-  const chains = useUnit(networkModel.$chains);
+  const chains = useUnit(accountsStructureModel.$allChainsMap);
+  const availableAccounts = useUnit(accountsStructureModel.$availableAccounts);
 
   return (
     <Select
       value={selectedAccount?.id ?? null}
       placeholder="Select account"
       onChange={(id) => {
-        const account = walletAccounts.find((a) => a.id === id);
+        const account = availableAccounts?.find((a) => a.id === id);
         if (account) {
           accountsStructureModel.selectAccount(account);
         }
       }}
     >
-      {walletAccounts.map((account) => {
-        const chain = accountService.isChainAccount(account) ? chains[account.chainId] : null;
+      {availableAccounts?.map((account) => {
+        const chain = accountService.isChainAccount(account) ? chains.get(account.chainId) : undefined;
         return (
           <Select.Item key={account.id} value={account.id}>
             <Address
