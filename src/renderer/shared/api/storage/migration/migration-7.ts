a417704ff99de1cc73851ece438be6e67b0a4832
anton.e <anton.efanov@novasama.io>
2025-06-27 14:16:59 +0300

    refactor(cas): simplify wc check

--- src/renderer/shared/api/storage/migration/migration-7.ts
+++ src/renderer/shared/api/storage/migration/migration-7.ts
@@ -1,21 +1,18 @@
 import { type Transaction } from 'dexie';
 
-import { CryptoType, type Wallet } from '@/shared/core';
+import { CryptoType } from '@/shared/core';
 import { isEthereumAccountId } from '@/shared/lib/utils/address';
 import { type AnyAccount } from '@/domains/network';
-import { walletUtils } from '@/entities/wallet';
+import { accountUtils } from '@/entities/wallet';
 
 /**
  * Migration to fix cryptoType for connected EVM accounts
  */
 export async function migrateEVMAccountsCryptoType(t: Transaction): Promise<void> {
-  const wallets = await t.db.table<Wallet>('wallets').toArray();
   const accounts = await t.table<AnyAccount>('accounts2').toArray();
 
   const accountsToUpdate = accounts.map((account) => {
-    const wallet = wallets.find((wallet) => wallet.id === account.walletId);
-
-    if (walletUtils.isWalletConnect(wallet)) {
+    if (accountUtils.isWcAccount(account)) {
       return {
         ...account,
         cryptoType: isEthereumAccountId(account.accountId) ? CryptoType.ETHEREUM : account.cryptoType,
