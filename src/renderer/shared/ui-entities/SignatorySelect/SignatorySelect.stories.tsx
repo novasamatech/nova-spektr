05efcf01df29058d373887a05e86120fd42ad163
cuteWarmFrog <livshits.victor@gmail.com>
2025-07-08 18:50:11 +0200

    fix: storybook

--- src/renderer/shared/ui-entities/SignatorySelect/SignatorySelect.stories.tsx
+++ src/renderer/shared/ui-entities/SignatorySelect/SignatorySelect.stories.tsx
@@ -1,40 +1,50 @@
-// import { BN } from '@polkadot/util';
-// import { type Meta, type StoryObj } from '@storybook/react';
-// import { fn } from '@storybook/test';
+import { type Meta, type StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
 
-// import { createVaultBaseAccount, dotAsset } from '@/shared/mocks';
+import { createPolkadotWallet, createVaultBaseAccount, dotAsset, polkadotChain } from '@/shared/mocks';
 
-// import { SignatorySelect } from './SignatorySelect';
+import { SignatorySelect } from './SignatorySelect';
 
-// const signatories = [
-//   { signer: createVaultBaseAccount('1', { walletId: 1 }), balance: new BN('100000000000') },
-//   { signer: createVaultBaseAccount('2', { walletId: 1 }), balance: new BN('50000000000') },
-//   { signer: createVaultBaseAccount('3', { walletId: 2 }), balance: new BN('10000000000') },
-// ];
+const signatories = [
+  { signer: createVaultBaseAccount('1', { walletId: 1 }), balance: '100000000000' },
+  { signer: createVaultBaseAccount('2', { walletId: 1 }), balance: '50000000000' },
+  { signer: createVaultBaseAccount('3', { walletId: 2 }), balance: '10000000000' },
+];
 
-// const meta: Meta<typeof SignatorySelect> = {
-//   title: 'Design System/entities/SignatorySelector',
-//   component: SignatorySelect,
-//   args: {
-//     signatories,
-//     signatory: signatories[0]?.signer ?? null,
-//     asset: dotAsset,
-//     addressPrefix: 0,
-//     hasError: false,
-//     errorText: '',
-//     onChange: fn(),
-//   },
-// };
+const allAccounts = signatories.map(({ signer }) => signer);
 
-// export default meta;
+const allWallets = [
+  createPolkadotWallet(1, { rootAccountId: signatories[0]!.signer.accountId }),
+  createPolkadotWallet(2, { rootAccountId: signatories[2]!.signer.accountId }),
+];
 
-// type Story = StoryObj<typeof SignatorySelect>;
+const network = { chain: polkadotChain, asset: dotAsset } as const;
 
-// export const Default: Story = {};
+const meta: Meta<typeof SignatorySelect> = {
+  title: 'Design System/entities/SignatorySelector',
+  component: SignatorySelect,
+  args: {
+    signatories,
+    signatory: signatories[0]?.signer ?? null,
+    hasError: false,
+    errorText: '',
+    onChange: fn(),
+    network,
+    allAccounts,
+    initiator: signatories[0]?.signer ?? null,
+    allWallets,
+  },
+};
 
-// export const WithError: Story = {
-//   args: {
-//     hasError: true,
-//     errorText: 'Select signatory',
-//   },
-// };
+export default meta;
+
+type Story = StoryObj<typeof SignatorySelect>;
+
+export const Default: Story = {};
+
+export const WithError: Story = {
+  args: {
+    hasError: true,
+    errorText: 'Select signatory',
+  },
+};
