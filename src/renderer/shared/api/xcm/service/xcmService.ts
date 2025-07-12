fa68ec794b864fd5e2378d8a0812921a0ea1fb3c
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-07-11 22:02:38 -0300

    refactor: extract localstorage keys to constants for safety

--- src/renderer/shared/api/xcm/service/xcmService.ts
+++ src/renderer/shared/api/xcm/service/xcmService.ts
@@ -4,7 +4,7 @@ import { BN, BN_TEN } from '@polkadot/util';
 import { camelCase, get } from 'lodash';
 
 import { type Chain, type ChainId, type HexString } from '@/shared/core';
-import { getAssetId, getTypeName, getTypeVersion, toLocalChainId } from '@/shared/lib/utils';
+import { CHAIN_MAP_STORAGE_KEY, getAssetId, getTypeName, getTypeVersion, toLocalChainId } from '@/shared/lib/utils';
 import { type AccountId } from '@/shared/polkadotjs-schemas';
 import { type XTokenPalletTransferArgs, type XcmPalletTransferArgs } from '@/entities/transaction';
 import { localStorageService } from '../../local-storage';
@@ -55,7 +55,7 @@ function saveXcmConfig(config: XcmConfig) {
 }
 
 function getChainsConfig(): Record<ChainId, Chain> | null {
-  return localStorageService.getFromStorage('network_chains', null);
+  return localStorageService.getFromStorage(CHAIN_MAP_STORAGE_KEY, null);
 }
 
 function getAvailableTransfers(chains: ChainXCM[], assetId: number, chainId: ChainId): XcmTransfer[] {
