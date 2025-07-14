77e8a6b0ef8377c20c29bae1f9b16fe71a780bb2
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-07-14 14:57:00 +0300

    fix: change proxy tests

--- tests/system/utils/readConfig.ts
+++ tests/system/utils/readConfig.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import url from 'node:url';
 
 export async function readConfig(): Promise<any> {
-  const chainsJsonURL = new URL('../../../src/renderer/shared/config/chains/chains.json', import.meta.url);
+  const chainsJsonURL = new URL('../../../src/renderer/shared/config/chains/chains_dev.json', import.meta.url);
   const chainsFilePath = url.fileURLToPath(chainsJsonURL);
   const chainsData = JSON.parse(fs.readFileSync(chainsFilePath, 'utf-8'));
 
