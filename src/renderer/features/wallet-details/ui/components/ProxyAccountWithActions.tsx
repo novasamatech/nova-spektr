c1d5cfea2f7447de4cd0d4a48f752f9dd7a14965
cuteWarmFrog <livshits.victor@gmail.com>
2025-07-21 18:20:46 +0200

    fix: old shit explorer is removed

--- src/renderer/features/wallet-details/ui/components/ProxyAccountWithActions.tsx
+++ src/renderer/features/wallet-details/ui/components/ProxyAccountWithActions.tsx
@@ -1,12 +1,11 @@
-import noop from 'lodash/noop';
-
 import { type Chain, type ProxyAccount } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
 import { copyToClipboard, toAddress } from '@/shared/lib/utils';
-import { DropdownIconButton, HelpText, IconButton } from '@/shared/ui';
+import { DropdownIconButton, FootnoteText, HelpText, IconButton, Separator } from '@/shared/ui';
 import { type DropdownIconButtonOption } from '@/shared/ui/types';
+import { Hash } from '@/shared/ui-entities';
+import { Box, Popover } from '@/shared/ui-kit';
 import { ProxyAccount as ProxyAccountComponent } from '@/entities/proxy';
-import { ExplorersPopover } from '@/entities/wallet';
 
 type Props = {
   account: ProxyAccount;
@@ -18,17 +17,13 @@ type Props = {
 export const ProxyAccountWithActions = ({ account, chain, canCreateProxy, onRemoveProxy }: Props) => {
   const { t } = useI18n();
 
+  const proxyAddress = toAddress(account.accountId, { prefix: chain.addressPrefix });
   const proxiedAddress = toAddress(account.proxiedAccountId, { prefix: chain.addressPrefix });
 
   const forgetProxyAction: DropdownIconButtonOption = {
     icon: 'forget',
     title: t('walletDetails.common.removeProxyAction'),
     onClick: () => onRemoveProxy(account),
-  };
-  const openInfoAction: DropdownIconButtonOption = {
-    icon: 'info',
-    title: t('walletDetails.common.openInfoAction'),
-    onClick: () => noop(),
   };
 
   return (
@@ -40,25 +35,50 @@ export const ProxyAccountWithActions = ({ account, chain, canCreateProxy, onRemo
         <DropdownIconButton name="more" className="ml-2">
           <DropdownIconButton.Items>
             <DropdownIconButton.Item>
-              <ExplorersPopover
-                address={account.accountId}
-                explorers={chain.explorers}
-                addressPrefix={chain.addressPrefix}
-                className="-mr-1 -mt-10"
-                button={<DropdownIconButton.Option option={openInfoAction} />}
-              >
-                <ExplorersPopover.Group title={t('walletDetails.common.proxiedAddressTitle')}>
-                  <div className="flex items-center gap-x-2">
-                    <HelpText className="break-all text-text-secondary">{proxiedAddress}</HelpText>
-                    <IconButton
-                      className="shrink-0"
-                      name="copy"
-                      size={20}
-                      onClick={() => copyToClipboard(proxiedAddress)}
-                    />
-                  </div>
-                </ExplorersPopover.Group>
-              </ExplorersPopover>
+              {/* hack to override dropdown hide on click */}
+              <div onClick={e => e.stopPropagation()}>
+                <Popover align="end" side="bottom">
+                  <Popover.Trigger>
+                    <button className="flex items-center gap-x-1 py-2 pl-2">
+                      <IconButton name="info" size={20} className="text-icon-accent" />
+                      <FootnoteText className="text-text-secondary">
+                        {t('walletDetails.common.openInfoAction')}
+                      </FootnoteText>
+                    </button>
+                  </Popover.Trigger>
+                  <Popover.Content>
+                    <Box width="230px" gap={2} padding={4}>
+                      <FootnoteText className="text-text-tertiary">{t('general.explorers.addressTitle')}</FootnoteText>
+                      <Box direction="row" verticalAlign="center" gap={3}>
+                        <HelpText className="text-text-secondary">
+                          <Hash value={proxyAddress} variant="full" />
+                        </HelpText>
+                        <IconButton
+                          className="shrink-0"
+                          name="copy"
+                          size={20}
+                          onClick={() => copyToClipboard(proxyAddress)}
+                        />
+                      </Box>
+
+                      <Separator />
+
+                      <FootnoteText className="text-text-tertiary">
+                        {t('walletDetails.common.proxiedAddressTitle')}
+                      </FootnoteText>
+                      <div className="flex items-center gap-x-2">
+                        <HelpText className="break-all text-text-secondary">{proxiedAddress}</HelpText>
+                        <IconButton
+                          className="shrink-0"
+                          name="copy"
+                          size={20}
+                          onClick={() => copyToClipboard(proxiedAddress)}
+                        />
+                      </div>
+                    </Box>
+                  </Popover.Content>
+                </Popover>
+              </div>
             </DropdownIconButton.Item>
             {canCreateProxy && (
               <DropdownIconButton.Item>
