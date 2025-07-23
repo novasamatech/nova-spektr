2030fa2babf9dfbe05d5cd34803e3ad1ff094b14
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-07-23 13:00:48 -0300

    refactor: can pass both Address and AccountId

--- src/renderer/entities/transaction/lib/transactionBuilder.ts
+++ src/renderer/entities/transaction/lib/transactionBuilder.ts
@@ -1,4 +1,3 @@
-import { encodeAddress } from '@polkadot/util-crypto';
 import { camelCase } from 'lodash';
 
 import { type ClaimAction } from '@/shared/api/governance';
@@ -582,7 +581,7 @@ function buildAddProxy({ chain, accountId, delegateAccountId, type }: AddProxyPa
     accountId: accountId,
     type: TransactionType.ADD_PROXY,
     args: {
-      delegate: encodeAddress(delegateAccountId, chain.addressPrefix),
+      delegate: delegateAccountId,
       proxyType: type,
       delay: 0,
     },
