5d903afd6725e043ae4aab4b6b10466bd640fb87
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-07-31 11:22:03 -0300

    fix: comment out raptorq encoder
    it doesnt generate correct qr payload for some reason

--- src/renderer/entities/transaction/ui/QrCode/QrGenerator/QrTxGenerator.tsx
+++ src/renderer/entities/transaction/ui/QrCode/QrGenerator/QrTxGenerator.tsx
@@ -1,6 +1,3 @@
-import init, { Encoder } from 'raptorq/raptorq';
-import { useCallback, useEffect, useState } from 'react';
-
 import { Skeleton } from '@/shared/ui-kit';
 
 import { DEFAULT_FRAME_DELAY } from './common/constants';
@@ -21,24 +18,28 @@ export const QrTxGenerator = ({
   bgColor = 'none',
   delay = DEFAULT_FRAME_DELAY,
 }: Props) => {
-  const [encoder, setEncoder] = useState<Encoder>();
+  // const [encoder, setEncoder] = useState<Encoder>();
 
-  const createEncoder = useCallback(async () => {
-    try {
-      await init();
-      setEncoder(Encoder.with_defaults(payload, 128));
-    } catch (error) {
-      console.error('Failed to create encoder:', error);
-    }
-  }, [payload]);
+  // const createEncoder = useCallback(async () => {
+  //   try {
+  //     const raptorq = await import('raptorq/raptorq');
+  //     const init = raptorq.default;
+  //     const { Encoder } = raptorq;
 
-  useEffect(() => {
-    if (!skipEncoding) {
-      createEncoder();
-    }
-  }, [createEncoder, skipEncoding]);
+  //     await init();
+  //     setEncoder(Encoder.with_defaults(payload, 128));
+  //   } catch (error) {
+  //     console.error('Failed to create encoder:', error);
+  //   }
+  // }, [payload]);
 
-  const image = useGenerator(payload, skipEncoding, delay, bgColor, encoder);
+  // useEffect(() => {
+  //   if (!skipEncoding && typeof window !== 'undefined') {
+  //     createEncoder();
+  //   }
+  // }, [createEncoder, skipEncoding]);
+
+  const image = useGenerator(payload, skipEncoding, delay, bgColor);
 
   if (!image) {
     return <Skeleton width={size} height={size} />;
