76969ddaa68cd8e0217d93688f1838b44cf3e947
sokolova-an <dev.anastasis@gmail.com>
2025-08-01 12:25:45 +0200

    fix: alerts and ui

--- src/renderer/features/multisig-wallet-create/flexible-multisig/components/SelectSignatoriesThreshold.tsx
+++ src/renderer/features/multisig-wallet-create/flexible-multisig/components/SelectSignatoriesThreshold.tsx
@@ -172,7 +172,7 @@ export const SelectSignatoriesThreshold = () => {
               </Alert>
             )}
 
-            {existingMultisig && (
+            {existingMultisig && !existingProxy && (
               <Alert
                 variant="info"
                 active={multisigAlreadyExists}
--- src/renderer/features/multisig-wallet-create/flexible-multisig/model/assign-model.ts
+++ src/renderer/features/multisig-wallet-create/flexible-multisig/model/assign-model.ts
@@ -90,13 +90,14 @@ const $coreTx = combine(
   {
     signatory: flexibleMultisigModel.$signer,
     totalDeposit: flexibleMultisigModel.$totalDeposit,
+    isMultisigExists: formModel.$multisigAlreadyExists,
     threshold: formModel.form.fields.threshold.$value,
     chain: formModel.$chain,
     multisigAccountId: formModel.$multisigAccountId,
     signatories: signatoryModel.$signatories,
     proxyAddress: $proxyAddress,
   },
-  ({ signatories, chain, threshold, signatory, multisigAccountId, proxyAddress, totalDeposit }) => {
+  ({ signatories, chain, threshold, signatory, multisigAccountId, proxyAddress, totalDeposit, isMultisigExists }) => {
     if (
       nullable(multisigAccountId) ||
       nullable(signatory) ||
@@ -118,6 +119,7 @@ const $coreTx = combine(
       threshold,
       proxyAccountId: toAccountId(proxyAddress),
       proxyDeposit: totalDeposit.toString(),
+      isMultisigExists,
     });
   },
 );
@@ -234,7 +236,7 @@ sample({
 
     const multisigAccount: Omit<NoID<FlexibleMultisigAccount>, 'walletId'> = {
       signatories: sortedSignatories,
-      name: name.trim(),
+      name: `${name.trim()} Multisig`,
       accountId: multisigAccountId!,
       threshold: threshold,
       cryptoType: isEthereumChain ? CryptoType.ETHEREUM : CryptoType.SR25519,
