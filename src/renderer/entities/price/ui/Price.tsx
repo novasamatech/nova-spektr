48cf158efa65db8a1bfcb259a5615ca67c7c7c97
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-08-12 12:30:11 -0300

    feat: add price tooltip

--- src/renderer/entities/price/ui/Price.tsx
+++ src/renderer/entities/price/ui/Price.tsx
@@ -1,4 +1,5 @@
 import { useI18n } from '@/shared/i18n';
+import { Tooltip } from '@/shared/ui-kit';
 
 type Props = {
   amount: string;
@@ -6,9 +7,32 @@ type Props = {
   symbol?: string;
 };
 
+const MAX_LENGTH = 16;
+const ELLIPSIS = '…';
+
+function truncatePrice(price: string) {
+  if (price.length <= MAX_LENGTH) {
+    return price;
+  }
+  const truncateEndIndex = MAX_LENGTH - ELLIPSIS.length;
+  return price.slice(0, truncateEndIndex).concat(ELLIPSIS);
+}
+
 export const Price = ({ amount, code, symbol }: Props) => {
   const { t } = useI18n();
 
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{symbol ? t('price.withSymbol', { amount, symbol }) : t('price.withCode', { amount, code })}</>;
+  const priceText = symbol ? t('price.withSymbol', { amount, symbol }) : t('price.withCode', { amount, code });
+
+  return priceText.length > MAX_LENGTH ? (
+    <Tooltip>
+      <Tooltip.Trigger>
+        <span>{truncatePrice(priceText)}</span>
+      </Tooltip.Trigger>
+      <Tooltip.Content>
+        <p>{priceText}</p>
+      </Tooltip.Content>
+    </Tooltip>
+  ) : (
+    <span>{priceText}</span>
+  );
 };
