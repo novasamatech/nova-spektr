cf7dca0380949f35b16d7dbe1ed4bdee516a1cb5
Sergey Zhuravlev <Zhuravlev1337@gmail.com>
2025-08-15 22:11:49 +0200

    fix: update balance formatting to support rounding configs (#4396)

--- src/renderer/shared/ui-entities/TransactionValidationError/TransactionValidationError.tsx
+++ src/renderer/shared/ui-entities/TransactionValidationError/TransactionValidationError.tsx
@@ -160,7 +160,7 @@ const TransactionBalanceError = ({
       </span>
       <span>
         {t('general.transactionErrors.balance.required', {
-          balances: imbalances.map(({ asset, imbalance }) => formatAsset(imbalance, asset)).join(', '),
+          balances: imbalances.map(({ asset, imbalance }) => formatAsset(imbalance, asset, { round: 'up' })).join(', '),
         })}
       </span>
     </Box>
