3d78199ad108b9b12947463f08a8925eabb41ea6
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-08-18 23:40:55 -0300

    feat: copy receiving address notifications

--- src/renderer/features/assets-transaction/components/CompareAddressModal.tsx
+++ src/renderer/features/assets-transaction/components/CompareAddressModal.tsx
@@ -1,14 +1,14 @@
 import { useUnit } from 'effector-react';
 import { type PropsWithChildren, useState } from 'react';
+import { toast } from 'sonner';
 
 import { type Asset, type Chain } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
-import { copyToClipboard, toAddress } from '@/shared/lib/utils';
-import { Alert, Button, FootnoteText, Icon } from '@/shared/ui';
+import { toAddress } from '@/shared/lib/utils';
+import { Alert, Button, FootnoteText } from '@/shared/ui';
 import { Address } from '@/shared/ui-entities';
-import { Box, Checkbox, Label, Modal } from '@/shared/ui-kit';
+import { Box, Checkbox, Copy, Label, Modal } from '@/shared/ui-kit';
 import { type AnyAccount } from '@/domains/network';
-import { OperationResult } from '@/entities/transaction';
 import { type StatusType } from '../lib/types';
 import { assetTransactionUtils } from '../lib/utils';
 import { receiveModel } from '../model/receive-model';
@@ -27,7 +27,6 @@ export const CompareAddressModal = ({ account, chain, asset, children }: Props) 
   const [isOpen, closeModal] = useState(showPopup);
   const [isPopupDetails, setIsPopupDetails] = useState(true);
   const [isChecked, checked] = useState(false);
-  const [statusType, setStatusType] = useState<StatusType | null>(null);
 
   const address = toAddress(account.accountId, { prefix: chain.addressPrefix });
   const legacyAddress = toAddress(account.accountId, { prefix: chain.legacyAddressPrefix ?? chain.addressPrefix });
@@ -38,83 +37,78 @@ export const CompareAddressModal = ({ account, chain, asset, children }: Props) 
     receiveModel.saveLegacyFormatViewed(!isChecked);
   };
 
-  const handleCopy = (address: string, type: StatusType) => {
-    copyToClipboard(address);
-    setStatusType(type);
+  const onAddressCopied = (type: StatusType) => {
+    toast.success(t(assetTransactionUtils.getStatusTitle(type)));
   };
 
   return (
-    <>
-      <Modal size="md" isOpen={isOpen} onToggle={closeModal}>
-        <Modal.Title close>{t(title)}</Modal.Title>
-        <Modal.Trigger>
-          <div onClick={() => setIsPopupDetails(false)}>{children}</div>
-        </Modal.Trigger>
-        <Modal.Content>
-          <Box padding={[4, 5]} horizontalAlign="center" gap={3}>
-            <FootnoteText className="mb-3 text-text-secondary">{t('receive.unfiedAddress.description')}</FootnoteText>
-            <div className="flex w-full items-center justify-between gap-2 bg-main-app-background p-3 pr-2">
-              <Box gap={1}>
-                <FootnoteText className="flex gap-2 text-text-secondary">
-                  {t('receive.unfiedAddress.unifiedFormat')}
-                  <Label variant="green">{t('receive.unfiedAddress.new')}</Label>
-                </FootnoteText>
-                <FootnoteText>
-                  <Address showIcon={false} variant="truncate" address={address} />
-                </FootnoteText>
-              </Box>
-              <Button variant="text" size="sm" onClick={() => handleCopy(address, 'unified')}>
-                {t('receive.copy')}
-              </Button>
+    <Modal size="md" isOpen={isOpen} onToggle={closeModal}>
+      <Modal.Title close>{t(title)}</Modal.Title>
+      <Modal.Trigger>
+        <div onClick={() => setIsPopupDetails(false)}>{children}</div>
+      </Modal.Trigger>
+      <Modal.Content>
+        <Box padding={[4, 5]} horizontalAlign="center" gap={3}>
+          <FootnoteText className="mb-3 text-text-secondary">{t('receive.unfiedAddress.description')}</FootnoteText>
+          <div className="flex w-full items-center justify-between gap-2 bg-main-app-background p-3 pr-2">
+            <Box gap={1}>
+              <FootnoteText className="flex gap-2 text-text-secondary">
+                {t('receive.unfiedAddress.unifiedFormat')}
+                <Label variant="green">{t('receive.unfiedAddress.new')}</Label>
+              </FootnoteText>
+              <FootnoteText>
+                <Address showIcon={false} variant="truncate" address={address} />
+              </FootnoteText>
+            </Box>
+            <Copy
+              value={address}
+              className="px-2 py-1 text-icon-accent hover:text-primary-button-background-hover active:text-primary-button-background-active"
+              onCopied={() => onAddressCopied('unified')}
+            >
+              {t('receive.copy')}
+            </Copy>
+          </div>
+
+          <div className="flex w-full items-center justify-between gap-2 bg-main-app-background p-3 pr-2">
+            <Box gap={1}>
+              <FootnoteText className="text-text-secondary">{t('receive.unfiedAddress.legacyFormat')}</FootnoteText>
+              <FootnoteText>
+                <Address showIcon={false} variant="truncate" address={legacyAddress} />
+              </FootnoteText>
+            </Box>
+            <Copy
+              value={address}
+              className="px-2 py-1 text-icon-accent hover:text-primary-button-background-hover active:text-primary-button-background-active"
+              onCopied={() => onAddressCopied('legacy')}
+            >
+              {t('receive.copy')}
+            </Copy>
+          </div>
+
+          <Alert active={true} variant="warn" title={t('receive.unfiedAddress.alertTitle')}>
+            <Alert.Item withDot={false}>
+              {t('receive.unfiedAddress.alertDescription', {
+                asset: asset.symbol,
+                assetName: asset.name,
+                network: chain.name,
+              })}
+            </Alert.Item>
+          </Alert>
+
+          {isPopupDetails && (
+            <div className="mt-3">
+              <Checkbox checked={isChecked} onChange={handleChecked}>
+                <FootnoteText className="text-text-secondary">{t('receive.unfiedAddress.checkboxLabel')}</FootnoteText>
+              </Checkbox>
             </div>
-
-            <div className="flex w-full items-center justify-between gap-2 bg-main-app-background p-3 pr-2">
-              <Box gap={1}>
-                <FootnoteText className="text-text-secondary">{t('receive.unfiedAddress.legacyFormat')}</FootnoteText>
-                <FootnoteText>
-                  <Address showIcon={false} variant="truncate" address={legacyAddress} />
-                </FootnoteText>
-              </Box>
-              <Button variant="text" size="sm" onClick={() => handleCopy(legacyAddress, 'legacy')}>
-                {t('receive.copy')}
-              </Button>
-            </div>
-
-            <Alert active={true} variant="warn" title={t('receive.unfiedAddress.alertTitle')}>
-              <Alert.Item withDot={false}>
-                {t('receive.unfiedAddress.alertDescription', {
-                  asset: asset.symbol,
-                  assetName: asset.name,
-                  network: chain.name,
-                })}
-              </Alert.Item>
-            </Alert>
-
-            {isPopupDetails && (
-              <div className="mt-3">
-                <Checkbox checked={isChecked} onChange={handleChecked}>
-                  <FootnoteText className="text-text-secondary">
-                    {t('receive.unfiedAddress.checkboxLabel')}
-                  </FootnoteText>
-                </Checkbox>
-              </div>
-            )}
-          </Box>
-        </Modal.Content>
-        <Modal.Footer>
-          <Button variant="fill" onClick={() => closeModal(false)}>
-            {t('general.button.closeButton')}
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
-      <OperationResult
-        autoCloseTimeout={2000}
-        isOpen={Boolean(statusType)}
-        content={<Icon size={40} name="checkmarkOutline" className="m-4 text-icon-positive" />}
-        title={t(assetTransactionUtils.getStatusTitle(statusType))}
-        onClose={() => setStatusType(null)}
-      />
-    </>
+          )}
+        </Box>
+      </Modal.Content>
+      <Modal.Footer>
+        <Button variant="fill" onClick={() => closeModal(false)}>
+          {t('general.button.closeButton')}
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
--- src/renderer/features/assets-transaction/components/ReceiveAssetContent.tsx
+++ src/renderer/features/assets-transaction/components/ReceiveAssetContent.tsx
@@ -1,15 +1,14 @@
 import { useUnit } from 'effector-react';
-import { useState } from 'react';
+import { toast } from 'sonner';
 
 import { type Asset, type Chain } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
-import { copyToClipboard, toAddress } from '@/shared/lib/utils';
+import { toAddress } from '@/shared/lib/utils';
 import { Button, FootnoteText, Icon } from '@/shared/ui';
 import { DefaultExplorer, ExplorerIcons } from '@/shared/ui/ExplorerLink/constants';
 import { Account } from '@/shared/ui-entities';
-import { Box } from '@/shared/ui-kit';
-import { OperationResult, QrTextGenerator } from '@/entities/transaction';
-import { type StatusType } from '../lib/types';
+import { Box, Copy } from '@/shared/ui-kit';
+import { QrTextGenerator } from '@/entities/transaction';
 import { assetTransactionUtils } from '../lib/utils';
 import { receiveModel } from '../model/receive-model';
 
@@ -24,7 +23,6 @@ export const ReceiveAssetContent = ({ chain, asset }: Props) => {
   const { t } = useI18n();
 
   const selectedAccount = useUnit(receiveModel.$selectedAccount);
-  const [statusType, setStatusType] = useState<StatusType | null>(null);
 
   if (!selectedAccount) return null;
 
@@ -34,13 +32,12 @@ export const ReceiveAssetContent = ({ chain, asset }: Props) => {
   });
   const isUnifiedAddress = address !== legacyAddress;
 
-  const handleCopy = (address: string, type: StatusType) => {
-    copyToClipboard(address);
-    setStatusType(type);
-  };
-
   //eslint-disable-next-line i18next/no-literal-string
   const qrCodePayload = `substrate:${address}:${selectedAccount.accountId}`;
+
+  const onAddressCopied = () => {
+    toast.success(t(assetTransactionUtils.getStatusTitle(isUnifiedAddress ? 'unified' : 'regular')));
+  };
 
   return (
     <Box padding={[4, 5, 6, 5]} horizontalAlign="center" gap={2}>
@@ -55,9 +52,13 @@ export const ReceiveAssetContent = ({ chain, asset }: Props) => {
           title={selectedAccount.name}
           hideExplorers
         />
-        <Button variant="text" size="sm" onClick={() => handleCopy(address, isUnifiedAddress ? 'unified' : 'regular')}>
+        <Copy
+          value={address}
+          className="text-icon-accent hover:text-primary-button-background-hover active:text-primary-button-background-active"
+          onCopied={onAddressCopied}
+        >
           {t('receive.copy')}
-        </Button>
+        </Copy>
       </div>
       {(chain.explorers || []).length > 0 && (
         <div className="flex w-full items-center justify-between bg-main-app-background px-5 py-3">
@@ -83,9 +84,13 @@ export const ReceiveAssetContent = ({ chain, asset }: Props) => {
         <>
           <div className="mb-3 flex w-full items-center justify-between bg-main-app-background px-5 py-3">
             <FootnoteText className="text-text-secondary">{t('receive.exchangeAccount')}</FootnoteText>
-            <Button variant="text" size="sm" onClick={() => handleCopy(legacyAddress, 'legacy')}>
+            <Copy
+              value={address}
+              className="text-icon-accent hover:text-primary-button-background-hover active:text-primary-button-background-active"
+              onCopied={onAddressCopied}
+            >
               {t('receive.copyLegacyFormat')}
-            </Button>
+            </Copy>
           </div>
 
           <CompareAddressModal account={selectedAccount} chain={chain} asset={asset}>
@@ -95,14 +100,6 @@ export const ReceiveAssetContent = ({ chain, asset }: Props) => {
           </CompareAddressModal>
         </>
       )}
-
-      <OperationResult
-        autoCloseTimeout={2000}
-        isOpen={Boolean(statusType)}
-        content={<Icon size={40} name="checkmarkOutline" className="m-4 text-icon-positive" />}
-        title={t(assetTransactionUtils.getStatusTitle(statusType))}
-        onClose={() => setStatusType(null)}
-      />
     </Box>
   );
 };
