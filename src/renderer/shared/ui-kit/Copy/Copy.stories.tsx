5a963f9918bb6f275b6fd4abeb08f078ba86ec75
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-08-19 18:28:15 -0300

    refactor: copy storybook

--- src/renderer/shared/ui-kit/Copy/Copy.stories.tsx
+++ src/renderer/shared/ui-kit/Copy/Copy.stories.tsx
@@ -7,16 +7,6 @@ import { Copy } from './Copy';
 const meta: Meta<typeof Copy> = {
   title: 'Design System/kit/Copy',
   component: Copy,
-};
-
-export default meta;
-
-type Story = StoryObj<typeof Copy>;
-
-export const Default: Story = {
-  args: {
-    value: 'Hello, world!',
-  },
   render: args => (
     <div className="flex items-center gap-2">
       <span>Hello, world!</span>
@@ -27,18 +17,12 @@ export const Default: Story = {
   ),
 };
 
-export const WithOnCopiedCallback: Story = {
+export default meta;
+
+type Story = StoryObj<typeof Copy>;
+
+export const Default: Story = {
   args: {
-    value: '5FYxhVJjGsUxutYmaaWtm76Jhb4FNsu4yvC4prm5yDmLPozN',
-  },
-  render: args => {
-    return (
-      <div className="flex items-center gap-2">
-        <span>5FYxhVJjGsUxutYmaaWtm76Jhb4FNsu4yvC4prm5yDmLPozN</span>
-        <Copy {...args}>
-          <span className="text-blue-600 underline">Copy Address</span>
-        </Copy>
-      </div>
-    );
+    value: 'Hello, world!',
   },
 };
