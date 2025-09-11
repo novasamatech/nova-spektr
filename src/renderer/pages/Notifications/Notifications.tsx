9f35792f48c559259d0e6f3bd7d631df63d59335
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2025-09-11 12:01:22 +0300

    feat: notification dot indicator
    * feat: notification dot indicator
    
    * fix: add counter and position
    
    * chore: code style fixes
    
    ---------
    
    Co-authored-by: Sergey Zhuravlev <zhuravlev1337@gmail.com>

--- src/renderer/pages/Notifications/Notifications.tsx
+++ src/renderer/pages/Notifications/Notifications.tsx
@@ -1,9 +1,16 @@
+import { useEffect } from 'react';
+
 import { useI18n } from '@/shared/i18n';
 import { Header } from '@/shared/ui';
+import { notificationModel } from '@/entities/notification';
 import { EmptyNotifications, NotificationsList } from '@/features/notifications';
 
 export const Notifications = () => {
   const { t } = useI18n();
+
+  useEffect(() => {
+    notificationModel.events.notificationsViewed();
+  }, []);
 
   return (
     <div className="flex h-full flex-col">
