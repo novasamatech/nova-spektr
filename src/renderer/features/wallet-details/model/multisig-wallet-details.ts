ddb792f0464e8c6b046260b6a15863918c89290b
Ilia Kalinin <kalinination@gmail.com>
2025-09-22 11:38:00 -0600

    refactor: use nonNullable

--- src/renderer/features/wallet-details/model/multisig-wallet-details.ts
+++ src/renderer/features/wallet-details/model/multisig-wallet-details.ts
@@ -2,7 +2,7 @@ import { combine, sample } from 'effector';
 import { createGate } from 'effector-react';
 
 import { type Contact, type Wallet } from '@/shared/core';
-import { dictionary, keys, nullable } from '@/shared/lib/utils';
+import { dictionary, keys, nonNullable, nullable } from '@/shared/lib/utils';
 import { type AccountId } from '@/shared/polkadotjs-schemas';
 import { contactModel } from '@/entities/contact';
 import { networkModel } from '@/entities/network';
@@ -65,7 +65,7 @@ const $signatories = combine(
 
 sample({
   clock: $wallet,
-  filter: wallet => !nullable(wallet),
+  filter: nonNullable,
   target: walletProxiesModel.resetWalletProxies,
 });
 
@@ -75,7 +75,7 @@ sample({
     chains: networkModel.$chains,
     apis: networkModel.$apis,
   },
-  filter: ({ wallet }) => !nullable(wallet),
+  filter: ({ wallet }) => nonNullable(wallet),
   fn: ({ wallet, chains, apis }) => ({ wallet: wallet!, chains, apis }),
   target: walletProxiesModel.fetchWalletProxiesFx,
 });
