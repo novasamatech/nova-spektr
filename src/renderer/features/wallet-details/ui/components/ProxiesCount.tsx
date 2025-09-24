146ebf209a2a44aa375b8e49e2e464a8c3a2b95f
Ilia Kalinin <kalinination@gmail.com>
2025-09-24 11:37:28 -0600

    feat: use correct loading states for proxy count

--- src/renderer/features/wallet-details/ui/components/ProxiesCount.tsx
+++ src/renderer/features/wallet-details/ui/components/ProxiesCount.tsx
@@ -1,4 +1,9 @@
+import { useUnit } from 'effector-react';
+
 import { cnTw } from '@/shared/lib/utils';
+import { Skeleton } from '@/shared/ui-kit';
+import { accountSync } from '@/domains/network';
+import { walletProxiesModel } from '../../model/wallet-proxies-model';
 
 type Props = {
   count: number;
@@ -6,6 +11,14 @@ type Props = {
 };
 
 export const ProxiesCount = ({ count, className }: Props) => {
+  const isProxiesLoading = useUnit(walletProxiesModel.fetchWalletProxiesFx.pending);
+  const isAccountSyncPending = useUnit(accountSync.syncAccounts.pending);
+  const isLoading = isProxiesLoading || isAccountSyncPending;
+
+  if (isLoading) {
+    return <Skeleton width={2} height={2.5} />;
+  }
+
   if (count === 0) {
     return null;
   }
