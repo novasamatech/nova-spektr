3ae5ecacc2c945019040f0a82fd8402c8694d17f
Sergey Zhuravlev <zhuravlev1337@gmail.com>
2025-09-26 10:18:05 +0200

    fix: correct transferred deposit of flex create

--- src/renderer/entities/transaction/lib/transactionBuilder.ts
+++ src/renderer/entities/transaction/lib/transactionBuilder.ts
@@ -1,4 +1,5 @@
 import { type Weight } from '@polkadot/types/interfaces';
+import { type BN } from '@polkadot/util';
 import { camelCase } from 'lodash';
 
 import { type ClaimAction } from '@/shared/api/governance';
@@ -600,7 +601,7 @@ type CreateFlexibleMultisigParams = {
   multisigAccountId: AccountId;
   proxyAccountId: AccountId;
   threshold: number;
-  proxyDeposit: string;
+  pureTopUpAmount: BN;
   signatories: Signatory[];
   isMultisigExists?: boolean;
 };
@@ -612,7 +613,7 @@ function buildCreateFlexibleMultisig({
   threshold,
   signatories,
   signatoryAccountId,
-  proxyDeposit,
+  pureTopUpAmount,
   isMultisigExists,
 }: CreateFlexibleMultisigParams): Transaction {
   // transfer deposit to proxy account
@@ -622,7 +623,7 @@ function buildCreateFlexibleMultisig({
     type: TransactionType.TRANSFER,
     args: {
       dest: proxyAccountId,
-      value: proxyDeposit,
+      value: pureTopUpAmount.toString(),
     },
   };
 
