aaea52055cd13cb7f066ba815cf34cccdf2f9739
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-09-29 16:09:45 +0700

    feat: group accounts by consensus chain in vault wallet details

--- src/renderer/entities/wallet/ui/VaultAccountsList/VaultAccountsList.tsx
+++ src/renderer/entities/wallet/ui/VaultAccountsList/VaultAccountsList.tsx
@@ -1,10 +1,15 @@
+import { useUnit } from 'effector-react';
+import { entries, groupBy } from 'lodash';
+import { memo, useMemo } from 'react';
+
 import { type Chain, type ChainId, type VaultChainAccount, type VaultShardAccount } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
-import { cnTw } from '@/shared/lib/utils';
+import { cnTw, nonNullable } from '@/shared/lib/utils';
 import { FootnoteText, HelpText, Separator } from '@/shared/ui';
 import { AccountExplorers } from '@/shared/ui-entities';
 import { Accordion, Box } from '@/shared/ui-kit';
 import { ChainTitle } from '@/entities/chain';
+import { networkModel } from '@/entities/network';
 import { accountUtils } from '../../lib/account-utils';
 import { DerivedAccount } from '../Cards/DerivedAccount';
 
@@ -15,32 +20,43 @@ type Props = {
   onShardClick?: (shards: VaultShardAccount[]) => void;
 };
 
-export const VaultAccountsList = ({ chains, accountsMap, className, onShardClick }: Props) => {
+export const VaultAccountsList = memo(({ chains, accountsMap, className, onShardClick }: Props) => {
   const { t } = useI18n();
+  const allChains = useUnit(networkModel.$chains);
+  const groups = useMemo(
+    () => entries(groupBy(chains, (chain) => chain.parentId ?? chain.chainId)) as [ChainId, Chain[]][],
+    [chains],
+  );
 
   return (
     <div className={cnTw('flex flex-col overflow-y-auto', className)}>
-      {chains.map((chain, index) => {
-        if (!accountsMap[chain.chainId]) return;
+      {groups.map(([consensusChainId, chains], groupIndex) => {
+        const consensusAccounts = chains
+          .map((chain) => accountsMap[chain.chainId])
+          .flat()
+          .filter(nonNullable);
+        if (!consensusAccounts.length) return;
+        const consensusChain = allChains[consensusChainId];
+
         return (
           <>
-            <Accordion initialOpen key={chain.chainId}>
+            <Accordion initialOpen key={consensusChainId}>
               <Accordion.Trigger>
                 <span className="normal-case">
-                  <ChainTitle fontClass="text-text-primary" chain={chain} />
+                  <ChainTitle fontClass="text-text-secondary uppercase" chain={consensusChain} />
                 </span>
-                <FootnoteText className="text-text-tertiary">{accountsMap[chain.chainId].length}</FootnoteText>
+                <FootnoteText className="text-text-tertiary">{consensusAccounts.length}</FootnoteText>
               </Accordion.Trigger>
               <Accordion.Content>
                 <ul>
-                  {accountsMap[chain.chainId].map((account) => {
+                  {consensusAccounts.map((account) => {
                     const isSharded = accountUtils.isAccountWithShards(account);
-                    const accountId = isSharded ? account.at(0)?.accountId : account.accountId;
-
-                    if (!accountId) return null;
+                    const accountId = isSharded ? account[0].accountId : account.accountId;
+                    const chain = allChains[isSharded ? account[0].chainId : account.chainId];
+                    const derivationPath = accountUtils.getDerivationPath(account);
 
                     return (
-                      <li className="mb-2 last:mb-0" key={accountUtils.getDerivationPath(account)}>
+                      <li className="mb-2 last:mb-0" key={derivationPath}>
                         <DerivedAccount
                           account={account}
                           addressPrefix={chain.addressPrefix}
@@ -52,9 +68,7 @@ export const VaultAccountsList = ({ chains, accountsMap, className, onShardClick
                                 <FootnoteText className="text-text-tertiary">
                                   {t('general.explorers.derivationTitle')}
                                 </FootnoteText>
-                                <HelpText className="break-all text-text-secondary">
-                                  {accountUtils.getDerivationPath(account)}
-                                </HelpText>
+                                <HelpText className="break-all text-text-secondary">{derivationPath}</HelpText>
                               </Box>
                             </AccountExplorers>
                           )}
@@ -65,10 +79,10 @@ export const VaultAccountsList = ({ chains, accountsMap, className, onShardClick
                 </ul>
               </Accordion.Content>
             </Accordion>
-            {index !== chains.length - 1 && <Separator className="my-1 w-full" />}
+            {groupIndex !== groups.length - 1 && <Separator className="my-1 w-full" />}
           </>
         );
       })}
     </div>
   );
-};
+});
