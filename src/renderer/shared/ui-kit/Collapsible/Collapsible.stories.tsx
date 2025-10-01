90812d998540284785d36f318d7a5c42e2554a39
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-10-01 16:16:11 +0700

    fix: collapsible component truncate trigger contents

--- src/renderer/shared/ui-kit/Collapsible/Collapsible.stories.tsx
+++ src/renderer/shared/ui-kit/Collapsible/Collapsible.stories.tsx
@@ -39,19 +39,17 @@ export const StickyTrigger: Story = {
   },
 };
 
-/**
- * `text-overflow: ellipsis` and `display: flex` don't play well together, this
- * story highlights overflow behavior.
- */
 export const TriggerContentOverflow: Story = {
   render(args) {
     return (
       <Collapsible {...args}>
         <Collapsible.Trigger>
           <span>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel erat lobortis, tristique erat a,
-            fringilla dui. Ut eu nibh nulla. Vivamus ac erat at sem aliquam dapibus. Etiam imperdiet arcu congue justo
-            sollicitudin feugiat.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
           </span>
         </Collapsible.Trigger>
         <Collapsible.Content>
