66e31757a5f77c51eb017f0216d700c63b43cf1e
Sergey Malivanov <75333966+MaliDo@users.noreply.github.com>
2025-10-02 09:09:29 +0300

    test: fix regress autotests
    Co-authored-by: Sergey Malivanov <sergey.m@knowledgecity.pro>

--- src/renderer/shared/ui-kit/Field/Field.tsx
+++ src/renderer/shared/ui-kit/Field/Field.tsx
@@ -2,11 +2,12 @@ import { type PropsWithChildren } from 'react';
 
 type Props = {
   text: string;
+  testId?: string;
 };
 
-export const Field = ({ text, children }: PropsWithChildren<Props>) => {
+export const Field = ({ text, testId, children }: PropsWithChildren<Props>) => {
   return (
-    <label className="flex w-full flex-col gap-y-2">
+    <label className="flex w-full flex-col gap-y-2" data-testid={testId}>
       <span className="text-footnote font-medium text-text-tertiary">{text}</span>
       {children}
     </label>
