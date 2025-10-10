41f2cbf3f501600c7db3db23af7dddca7e7bb459
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-10-10 13:57:51 +0700

    feat: add hard/soft derivation hint tooltips

--- src/renderer/features/wallets/KeyConstructor/ui/HintChips.tsx
+++ src/renderer/features/wallets/KeyConstructor/ui/HintChips.tsx
@@ -1,6 +1,8 @@
 import { memo, useCallback, useMemo } from 'react';
 
+import { useI18n } from '@/shared/i18n';
 import { Button } from '@/shared/ui';
+import { Tooltip } from '@/shared/ui-kit';
 import { constructorModel } from '../model/constructor-model';
 
 type Props = {
@@ -10,13 +12,14 @@ type Props = {
 };
 
 const SHARD_COUNT = 10;
-const DEFAULT_HINTS = ['//', '/'];
+const HARD_SOFT_HINTS = ['//', '/'];
 const TYPE_HINTS = ['main', 'hot', 'public', 'sharded'];
 
 export const HintChips = memo(({ keyId, derivationPath, chainName }: Props) => {
+  const { t } = useI18n();
   const hints = useMemo(() => {
     if (derivationPath === '' || /[a-z0-9]$/i.test(derivationPath)) {
-      return DEFAULT_HINTS;
+      return HARD_SOFT_HINTS;
     }
     if (derivationPath === '//' || derivationPath === '/') {
       return [chainName];
@@ -27,7 +30,7 @@ export const HintChips = memo(({ keyId, derivationPath, chainName }: Props) => {
     if (derivationPath.endsWith('//') || derivationPath.endsWith('/')) {
       return TYPE_HINTS;
     }
-    return DEFAULT_HINTS;
+    return HARD_SOFT_HINTS;
   }, [derivationPath]);
 
   const insertHint = useCallback(
@@ -41,18 +44,27 @@ export const HintChips = memo(({ keyId, derivationPath, chainName }: Props) => {
   return (
     <div className="flex flex-row gap-x-2">
       {hints.map((hint) => (
-        <Button
-          size="sm"
-          variant="chip"
-          pallet="secondary"
-          key={hint}
-          onMouseDown={(e) => {
-            e.preventDefault();
-            insertHint(keyId, hint);
-          }}
-        >
-          {hint}
-        </Button>
+        <Tooltip key={hint} delay={400} side="bottom">
+          <Tooltip.Trigger>
+            <Button
+              size="sm"
+              variant="chip"
+              pallet="secondary"
+              onMouseDown={(e) => {
+                e.preventDefault();
+                insertHint(keyId, hint);
+              }}
+            >
+              {hint}
+            </Button>
+          </Tooltip.Trigger>
+          {HARD_SOFT_HINTS.includes(hint) && (
+            <Tooltip.Content>
+              {hint === '/' && t('dynamicDerivations.keysConstructor.softDerivationTooltip')}
+              {hint === '//' && t('dynamicDerivations.keysConstructor.hardDerivationTooltip')}
+            </Tooltip.Content>
+          )}
+        </Tooltip>
       ))}
     </div>
   );
