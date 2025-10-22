908d740ca4fa03513176e7aa3a8848bcd84717b5
Sergey Malivanov <sergey.m@knowledgecity.pro>
2025-10-22 18:18:01 +0300

    test: fee loader id

--- src/renderer/entities/transaction/ui/FeeLoader/FeeLoader.tsx
+++ src/renderer/entities/transaction/ui/FeeLoader/FeeLoader.tsx
@@ -3,11 +3,12 @@ import { Shimmering } from '@/shared/ui';
 
 type Props = {
   fiatFlag?: boolean;
+  testId?: string;
 };
 
-export const FeeLoader = ({ fiatFlag }: Props) => (
+export const FeeLoader = ({ fiatFlag, testId = TEST_IDS.OPERATIONS.FEE_LOADER }: Props) => (
   <div className="flex flex-col items-end gap-y-0.5">
-    <Shimmering width={90} height={20} data-testid={TEST_IDS.OPERATIONS.FEE_LOADER} />
-    {fiatFlag && <Shimmering width={70} height={18} data-testid={TEST_IDS.OPERATIONS.FEE_LOADER} />}
+    <Shimmering width={90} height={20} testId={testId} />
+    {fiatFlag && <Shimmering width={70} height={18} testId={testId} />}
   </div>
 );
