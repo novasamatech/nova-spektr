f93098cfcd4e2ae7e8feebeeae47d1c0546eed61
anton.e <anton.efanov@novasama.io>
2025-10-24 12:20:19 +0200

    refactor(transfer): nullable fee

--- src/renderer/shared/transactions/createFeeCalculator.ts
+++ src/renderer/shared/transactions/createFeeCalculator.ts
@@ -1,9 +1,9 @@
 import { type SubmittableExtrinsic } from '@polkadot/api/types/submittable';
-import { BN, BN_ZERO } from '@polkadot/util';
+import { BN } from '@polkadot/util';
 import { type Store, type UnitValue, createEffect, createStore, sample } from 'effector';
 
 import { takeLast } from '@/shared/effector';
-import { nonNullable, nullable } from '@/shared/lib/utils';
+import { nullable } from '@/shared/lib/utils';
 import { transactionService } from '@/domains/network';
 
 type Params = {
@@ -16,28 +16,35 @@ type FeeCalculationRequest = {
 };
 
 export const createFeeCalculator = ({ active = createStore(true), extrinsic }: Params) => {
-  const $fee = createStore(BN_ZERO);
+  const $fee = createStore<BN | null>(null);
 
   const fetchFeeFx = takeLast({
     fn: async ({ extrinsic }: FeeCalculationRequest): Promise<BN> => {
+      console.log({ extrinsic });
       const fee = await transactionService.getExtrinsicFee(extrinsic).then((x) => new BN(x));
       return fee;
     },
     key: () => 'feeCalculation',
   });
 
+  const isAbortError = (err: UnitValue<typeof fetchFeeFx.failData>) =>
+    err && 'name' in err && err.name === 'AbortError';
+
   const logErrorFx = createEffect((err: UnitValue<typeof fetchFeeFx.failData>) => {
-    if (err && 'name' in err && err.name === 'AbortError') {
+    if (isAbortError(err)) {
       return;
     }
 
-    console.error('fee calculation faied', err);
+    console.error('fee calculation failed', err);
   });
 
   sample({
     clock: extrinsic,
     filter: nullable,
-    fn: () => BN_ZERO,
+    fn: () => {
+      console.log('set fee', { value: null });
+      return null;
+    },
     target: $fee,
   });
 
@@ -58,14 +65,20 @@ export const createFeeCalculator = ({ active = createStore(true), extrinsic }: P
 
   sample({
     clock: fetchFeeFx.doneData,
+    fn: (value) => {
+      console.log('set fee', { value: value.toString() });
+      return value;
+    },
     target: $fee,
   });
 
   sample({
-    clock: fetchFeeFx.fail,
-    source: extrinsic,
-    filter: nonNullable,
-    fn: (_) => BN_ZERO,
+    clock: fetchFeeFx.failData,
+    filter: (error) => !isAbortError(error),
+    fn: (_) => {
+      console.log('set fee', { value: null });
+      return null;
+    },
     target: $fee,
   });
 
