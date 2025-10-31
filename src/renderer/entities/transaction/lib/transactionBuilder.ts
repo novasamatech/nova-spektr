1e1be677b1215ea8800ebf0c958ab5739184c2b4
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-10-31 20:49:04 +0300

    fix: transfer_all

--- src/renderer/entities/transaction/lib/transactionBuilder.ts
+++ src/renderer/entities/transaction/lib/transactionBuilder.ts
@@ -6,6 +6,7 @@ import { type ClaimAction } from '@/shared/api/governance';
 import {
   type Address,
   type Asset,
+  AssetType,
   type Chain,
   type ChainId,
   type Conviction,
@@ -87,7 +88,7 @@ function buildTransfer({
   if (xcmData) {
     transactionType = xcmData.transactionType;
   }
-  if (transferAll) {
+  if (transferAll && asset.type === AssetType.NATIVE) {
     transactionType = TransactionType.TRANSFER_ALL;
   }
   if (allowDeath) {
