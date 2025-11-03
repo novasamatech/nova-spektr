c1ecb640351787b6d1df68e1172c2567aba49896
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-11-02 21:05:52 -0300

    fix: file import shard count

--- src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-utils.test.ts
+++ src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-utils.test.ts
@@ -1,13 +1,7 @@
-import { webcrypto } from 'node:crypto';
-
 import { KeyType } from '@/shared/core';
 import { polkadotChain, polkadotChainId } from '@/shared/mocks';
 import { importKeysUtils } from '../import-keys-utils';
 import { importKeysMocks } from '../mocks/import-keys-utils.mock';
-
-Object.defineProperty(global.self, 'crypto', {
-  value: webcrypto,
-});
 
 describe('entities/dynamicDerivations/import-keys-utils', () => {
   describe('entities/dynamicDerivations/import-keys-utils/validateDerivation', () => {
