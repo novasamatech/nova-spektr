b1ffafa41928cd16c03ad246977960a514a8265f
anton.e <anton.efanov@novasama.io>
2025-11-05 11:18:02 +0100

    chore: revert assert

--- src/renderer/shared/transactions/createTxValidator.ts
+++ src/renderer/shared/transactions/createTxValidator.ts
@@ -172,7 +172,6 @@ const basicRules: ValidatorRule<unknown>[] = [
     assert(signatory, 'Signatory not found');
 
     const fee = await transactionService.getTransactionFee(transaction, signatory.accountId, api);
-    assert(fee, 'Fee not found');
 
     const balanceForFee = getBalance(signatory.accountId, chainId, asset.assetId);
     assert(balanceForFee, 'Balance for fee not found');
