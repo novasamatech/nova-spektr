9137ebe470b057682800fa92b88953e361951cf0
Sergey Malivanov <sergey.m@knowledgecity.pro>
2025-11-06 17:16:37 +0300

    fix: lint fix

--- src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-model.test.ts
+++ src/renderer/features/wallets/ImportKeys/lib/__tests__/import-keys-model.test.ts
@@ -67,7 +67,8 @@ describe('features/ImportKeys/lib/import-keys-model', () => {
       error: ValidationError.DERIVATIONS_ERROR,
       details: {
         [DerivationValidationError.PASSWORD_PATH]: ['//polkadot///password'],
-        [DerivationValidationError.INVALID_PATH]: ['//polkadot///password', 'invalid_path1', 'invalid_path2'],
+        [DerivationValidationError.INVALID_PATH]: ['invalid_path1', 'invalid_path2'],
+        [DerivationValidationError.ETHEREUM_SINGLE_SLASH]: [],
         [DerivationValidationError.WRONG_SHARDS_NUMBER]: [],
       },
     };
