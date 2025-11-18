db10bde4d6016e7e277f9f63773173213a40fbfb
Vitya Livshits <64482828+cuteWarmFrog@users.noreply.github.com>
2025-11-18 18:32:42 +0100

    fix: votes for ref (#5116)

--- src/renderer/features/fellowship-retention/hooks/useVotes.ts
+++ src/renderer/features/fellowship-retention/hooks/useVotes.ts
@@ -1,19 +1,21 @@
 import { nonNullable } from '@/shared/lib/utils';
-import { useVotes as useVotesDomain } from '@/domains/collectives';
-import { useFellowshipAccount, useMemberRetentionReferendum } from '@/aggregates/fellowship-member';
+import { useMembers, useVotes as useVotesDomain } from '@/domains/collectives';
+import { useMemberRetentionReferendum } from '@/aggregates/fellowship-member';
 import { useFellowshipApi, useFellowshipChain } from '@/aggregates/fellowship-network';
 
 export const useVotes = () => {
   const api = useFellowshipApi();
   const chain = useFellowshipChain();
-  const { data: account } = useFellowshipAccount();
   const { data: referendum } = useMemberRetentionReferendum();
+  const { data: members } = useMembers({ palletType: 'fellowship', api });
+
+  const accounts = members.map(member => member.accountId);
 
   return useVotesDomain({
     palletType: 'fellowship',
     chain,
     api,
     referendums: nonNullable(referendum) ? [referendum.id] : [],
-    accounts: account ? [account.accountId] : null,
+    accounts,
   });
 };
