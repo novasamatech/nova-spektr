bf3b0f51491c4761aa21f54ce7d15facaca19766
Ilya Kalinin <kalinination@gmail.com>
2025-11-28 04:25:05 -0700

    feat: add retention and promotion submission warning modals
    * feat: add retention and promotion submission warning modals
    
    * feat: use radix slot, change logic to be >28 days for retention, update copy
    
    * feat: update promotion copy
    
    * chore: sort keys

--- src/renderer/features/fellowship-evidence/components/SubmitEvidencePopover.tsx
+++ src/renderer/features/fellowship-evidence/components/SubmitEvidencePopover.tsx
@@ -13,6 +13,7 @@ import { EvidencePostModal } from './EvidencePostModal';
 import { IPFSUploadModal } from './IPFSUploadModal';
 import { MarkdownPreviewModal } from './MarkdownPreviewModal';
 import { SubmitEvidenceFromScratch } from './SubmitEvidenceFromScratch';
+import { SubmitPeriodWarningAlert } from './SubmitPeriodWarningAlert';
 
 type Props = PropsWithChildren<{
   wish: 'Promotion' | 'Retention';
@@ -115,22 +116,20 @@ export const SubmitEvidencePopover = ({ wish, children }: Props) => {
         <Popover.Trigger>{childWithIcon}</Popover.Trigger>
         <Popover.Content>
           <Box padding={[1, 1]} gap={1}>
-            <div
-              className="cursor-pointer rounded px-3 py-2 hover:bg-action-background-hover"
-              onClick={handleFromScratch}
-            >
-              <FootnoteText className="text-text-secondary">
-                {t('fellowship.salary.evidence.submitOptions.fromScratch')}
-              </FootnoteText>
-            </div>
-            <div
-              className="cursor-pointer rounded px-3 py-2 hover:bg-action-background-hover"
-              onClick={handleUploadFromIPFS}
-            >
-              <FootnoteText className="text-text-secondary">
-                {t('fellowship.salary.evidence.submitOptions.uploadFromIPFS')}
-              </FootnoteText>
-            </div>
+            <SubmitPeriodWarningAlert wish={wish} onConfirm={handleFromScratch}>
+              <div className="cursor-pointer rounded px-3 py-2 hover:bg-action-background-hover">
+                <FootnoteText className="text-text-secondary">
+                  {t('fellowship.salary.evidence.submitOptions.fromScratch')}
+                </FootnoteText>
+              </div>
+            </SubmitPeriodWarningAlert>
+            <SubmitPeriodWarningAlert wish={wish} onConfirm={handleUploadFromIPFS}>
+              <div className="cursor-pointer rounded px-3 py-2 hover:bg-action-background-hover">
+                <FootnoteText className="text-text-secondary">
+                  {t('fellowship.salary.evidence.submitOptions.uploadFromIPFS')}
+                </FootnoteText>
+              </div>
+            </SubmitPeriodWarningAlert>
           </Box>
         </Popover.Content>
       </Popover>
