b7a837adcf9763b93ad1b32c2b5a6a0f3d356129
Vitya Livshits <64482828+cuteWarmFrog@users.noreply.github.com>
2025-11-28 15:46:53 +0100

    refactor: update flow management in call data and vested transfer forms (#5250)

--- src/renderer/features/vested-transfer/index.tsx
+++ src/renderer/features/vested-transfer/index.tsx
@@ -1,11 +1,11 @@
 import { navigationActionsSlot } from '@/features/app-shell';
 
-import { VestedTransferNavigation } from './components/VestedTransferNavigation';
+import { VestedTransferModal } from './components/VestedTransferModal';
 import { vestedTransferFeature } from './model/feature';
 
 export { vestedTransferFeature };
 
 vestedTransferFeature.inject(navigationActionsSlot, {
   order: 1001,
-  render: () => <VestedTransferNavigation />,
+  render: () => <VestedTransferModal />,
 });
