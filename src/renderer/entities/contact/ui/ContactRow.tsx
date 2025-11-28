39f95d3d894e5f98b6426013d050706ac74c5f36
anton.e <anton.efanov@novasama.io>
2025-11-28 12:45:40 +0100

    refactor: put buttons in a row

--- src/renderer/entities/contact/ui/ContactRow.tsx
+++ src/renderer/entities/contact/ui/ContactRow.tsx
@@ -3,9 +3,9 @@ import { useNavigate } from 'react-router-dom';
 import { type Contact } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
 import { Paths, createLink } from '@/shared/routes';
-import { FootnoteText, Icon, IconButton, Plate } from '@/shared/ui';
+import { IconButton, Plate } from '@/shared/ui';
 import { Address } from '@/shared/ui-entities';
-import { ConfirmModal, Copy, Dropdown } from '@/shared/ui-kit';
+import { ConfirmModal, Copy } from '@/shared/ui-kit';
 import { contactModel } from '../model/contact-model';
 
 type Props = {
@@ -29,39 +29,25 @@ export const ContactRow = ({ contact }: Props) => {
       <div className="flex min-w-0 gap-x-1 p-3">
         <Address address={contact.address} showIcon iconSize={20} variant="truncate" title={contact.name} />
       </div>
-      <div className="ml-auto flex items-center gap-x-3 p-3">
+      <div className="ml-auto flex items-center gap-x-2 p-3">
         <Copy value={contact.address} notification={t('general.notifications.addressCopied')}>
           <IconButton className="shrink-0 self-center text-icon-default" name="copy" />
         </Copy>
 
-        <Dropdown>
-          <Dropdown.Trigger>
-            <IconButton name="more" />
-          </Dropdown.Trigger>
-          <Dropdown.Content>
-            <Dropdown.Item onSelect={handleEdit}>
-              <Icon name="edit" size={16} className="text-icon-accent" />
-              <FootnoteText className="text-text-secondary">{t('addressBook.actions.edit')}</FootnoteText>
-            </Dropdown.Item>
-            <Dropdown.Item onSelect={onDelete}>
-              <ConfirmModal
-                title={t('addressBook.removeConfirm.title')}
-                description={t('addressBook.removeConfirm.description', { name: contact.name })}
-                cancelText={t('addressBook.removeConfirm.cancelButton')}
-                confirmText={t('addressBook.removeConfirm.confirmButton')}
-                type="warning"
-                onConfirm={onDelete}
-              >
-                <ConfirmModal.Trigger>
-                  <button className="flex items-center gap-2">
-                    <Icon name="delete" size={16} className="text-icon-accent" />
-                    <FootnoteText className="text-text-secondary">{t('addressBook.actions.delete')}</FootnoteText>
-                  </button>
-                </ConfirmModal.Trigger>
-              </ConfirmModal>
-            </Dropdown.Item>
-          </Dropdown.Content>
-        </Dropdown>
+        <IconButton className="shrink-0 self-center text-icon-default" name="edit" onClick={handleEdit} />
+
+        <ConfirmModal
+          title={t('addressBook.removeConfirm.title')}
+          description={t('addressBook.removeConfirm.description', { name: contact.name })}
+          cancelText={t('addressBook.removeConfirm.cancelButton')}
+          confirmText={t('addressBook.removeConfirm.confirmButton')}
+          type="warning"
+          onConfirm={onDelete}
+        >
+          <ConfirmModal.Trigger>
+            <IconButton className="shrink-0 self-center text-icon-default" name="delete" />
+          </ConfirmModal.Trigger>
+        </ConfirmModal>
       </div>
     </Plate>
   );
