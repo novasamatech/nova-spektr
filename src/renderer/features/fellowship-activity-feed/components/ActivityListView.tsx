1380ffcebac9bfdef06f214d81d4ac4c78ae1250
Sergey Malivanov <75333966+MaliDo@users.noreply.github.com>
2025-11-28 19:31:36 +0300

    fix: latest events divider and text updates
    Co-authored-by: Sergey Malivanov <sergey.m@knowledgecity.pro>

--- src/renderer/features/fellowship-activity-feed/components/ActivityListView.tsx
+++ src/renderer/features/fellowship-activity-feed/components/ActivityListView.tsx
@@ -2,6 +2,7 @@ import { type PropsWithChildren, memo, useMemo } from 'react';
 
 import { useClock, useDeferredList } from '@/shared/lib/hooks';
 import { nullable } from '@/shared/lib/utils';
+import { Separator } from '@/shared/ui';
 import { AsyncItem } from '@/shared/ui-kit';
 import { useFellowshipChain } from '@/aggregates/fellowship-network';
 
@@ -28,23 +29,35 @@ export const ActivityListView = memo(
     if (nullable(chain)) return null;
 
     return (
-      <div className="flex h-full flex-col gap-y-5 pt-2 pb-4">
+      <div className="flex h-full flex-col pt-2 pb-4">
         {isLoading && placeholders}
 
-        {list.map(event => (
-          <AsyncItem key={`${event.block}-${event.accountId}-${event.type}`} fallback={placeholder}>
-            <EventRecord
-              event={event}
-              description={event.description}
-              duration={(now - event.at.getTime()) / 1000}
-              name={event.actorName || event.name}
-              actorAccountId={event.actorAccountId}
-              chain={chain}
-              withFullAccountInfo={withFullAccountInfo}
-              referendumDetails={event.referendumDetails}
-            />
-          </AsyncItem>
-        ))}
+        {list.map((event, index) => {
+          const key = `${event.block}-${event.accountId}-${event.type}`;
+          const isLast = index === list.length - 1;
+
+          return (
+            <div key={key} className="flex flex-col">
+              <AsyncItem fallback={placeholder}>
+                <EventRecord
+                  event={event}
+                  description={event.description}
+                  duration={(now - event.at.getTime()) / 1000}
+                  name={event.actorName || event.name}
+                  actorAccountId={event.actorAccountId}
+                  chain={chain}
+                  withFullAccountInfo={withFullAccountInfo}
+                  referendumDetails={event.referendumDetails}
+                />
+              </AsyncItem>
+              {!isLast && (
+                <div className="px-5">
+                  <Separator className="py-2.5" />
+                </div>
+              )}
+            </div>
+          );
+        })}
       </div>
     );
   },
