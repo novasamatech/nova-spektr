a3738a158f662b622587e0b68e66617e1e51ce92
Ilia Kalinin <kalinination@gmail.com>
2025-12-05 08:56:30 -0700

    feat: add more chains to blacklist, remove some chains from whitelist

--- src/renderer/shared/api/xcm/service/constants.ts
+++ src/renderer/shared/api/xcm/service/constants.ts
@@ -70,6 +70,10 @@ export const XCM_DESTINATION_BLACKLIST: XcmDestinationBlacklistEntry[] = [
     destinationChainId: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f', // Polkadot Asset Hub
   },
   {
+    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
+    destinationChainId: '0xf6ee56e9c5277df5b4ce6ae9983ee88f3cbed27d31beeb98f9f84f997a1ab0b9', // Mythos
+  },
+  {
     destinationChainId: '0xcdedc8eadbfa209d3f207bba541e57c3c58a667b05a2e1d1e86353c9000758da', // Integritee Parachain
   },
   {
@@ -107,6 +111,30 @@ export const XCM_DESTINATION_BLACKLIST: XcmDestinationBlacklistEntry[] = [
   {
     sourceChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
     destinationChainId: '0x401a1f9dca3da46f5c4091016c8a2f26dcea05865116b286f60f668207d1474b', // Moonriver
+  },
+  {
+    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
+    destinationChainId: '0xfe58ea77779b7abda7da4ec526d14db9b1e9cd40a217c34892af80a9b332b76d', // Moonbeam
+  },
+  {
+    sourceChainId: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe', // Kusama Relay
+    destinationChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
+  },
+  {
+    sourceChainId: '0x00dcb981df86429de8bbacf9803401f09485366c44efbf53af9ecfab03adc7e5', // Kusama Bridge Hub
+    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
+  },
+  {
+    sourceChainId: '0xc1af4cb4eb3918e5db15086c0cc5ec17fb334f728b7c65dd44bfe1e174ff8b3f', // Kusama People
+    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
+  },
+  {
+    sourceChainId: '0xefb56e30d9b4a24099f88820987d0f45fb645992416535d87650d98e00f46fc4', // Polkadot Coretime
+    destinationChainId: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f', // Polkadot Asset Hub
+  },
+  {
+    sourceChainId: '0x638cd2b9af4b3bb54b8c1f0d22711fc89924ca93300f0caf25a580432b29d050', // Kusama Coretime
+    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
   },
 ];
 
@@ -221,89 +249,6 @@ export const XCM_DESTINATION_WHITELIST: XcmDestinationWhitelistEntry[] = [
     destinationAsset: 'DOT',
   },
   {
-    sourceChainId: '0xa85cfb9b9fd4d622a5b28289a02347af987d8f73fa3108450e2b4a11c1ce5755', // Basilisk
-    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
-    sourceAsset: 'KSM-Statemine',
-    destinationAsset: 'KSM',
-  },
-  {
-    sourceChainId: '0xa85cfb9b9fd4d622a5b28289a02347af987d8f73fa3108450e2b4a11c1ce5755', // Basilisk
-    destinationChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
-    sourceAsset: 'KSM-Statemine',
-    destinationAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0xa85cfb9b9fd4d622a5b28289a02347af987d8f73fa3108450e2b4a11c1ce5755', // Basilisk
-    destinationChainId: '0xbaf5aabe40646d11f0ee8abbdc64f4a4b7674925cba08e4a05ff9ebed6e2126b', // Karura
-    sourceAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0xa85cfb9b9fd4d622a5b28289a02347af987d8f73fa3108450e2b4a11c1ce5755', // Basilisk
-    destinationChainId: '0xf1cf9022c7ebb34b162d5b5e34e705a5a740b2d0ecc1009fb89023e62a488108', // Shiden
-    sourceAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
-    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
-    sourceAsset: 'KSM-Statemine',
-    destinationAsset: 'KSM',
-  },
-  {
-    sourceChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
-    destinationChainId: '0xa85cfb9b9fd4d622a5b28289a02347af987d8f73fa3108450e2b4a11c1ce5755', // Basilisk
-    sourceAsset: 'KSM-Statemine',
-    destinationAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
-    destinationChainId: '0xbaf5aabe40646d11f0ee8abbdc64f4a4b7674925cba08e4a05ff9ebed6e2126b', // Karura
-    sourceAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
-    destinationChainId: '0x401a1f9dca3da46f5c4091016c8a2f26dcea05865116b286f60f668207d1474b', // Moonriver
-    destinationAsset: 'MOVR',
-  },
-  {
-    sourceChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
-    destinationChainId: '0xf1cf9022c7ebb34b162d5b5e34e705a5a740b2d0ecc1009fb89023e62a488108', // Shiden
-    sourceAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b', // Bifrost Polkadot
-    destinationChainId: '0xfc41b9bd8ef8fe53d58c7ea67c794c7ec9a73daf05e6d54b14ff6342c99ba64c', // Acala
-    sourceAsset: 'DOT-Statemint',
-  },
-  {
-    sourceChainId: '0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b', // Bifrost Polkadot
-    destinationChainId: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f', // Polkadot Asset Hub
-    sourceAsset: 'DOT-Statemint',
-    destinationAsset: 'DOT',
-  },
-  {
-    sourceChainId: '0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b', // Bifrost Polkadot
-    destinationChainId: '0x9eb76c5184c4ab8679d2d5d819fdf90b9c001403e9e17da2e14b6d8aec4029c6', // Astar
-    sourceAsset: 'DOT-Statemint',
-    destinationAsset: 'USDT-Statemint',
-  },
-  {
-    sourceChainId: '0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b', // Bifrost Polkadot
-    destinationChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    sourceAsset: 'DOT-Statemint',
-    destinationAsset: 'DOT-Statemint',
-  },
-  {
-    sourceChainId: '0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b', // Bifrost Polkadot
-    destinationChainId: '0xfe58ea77779b7abda7da4ec526d14db9b1e9cd40a217c34892af80a9b332b76d', // Moonbeam
-    destinationAsset: 'GLMR',
-  },
-  {
-    sourceChainId: '0x00dcb981df86429de8bbacf9803401f09485366c44efbf53af9ecfab03adc7e5', // Kusama Bridge Hub
-    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
-    sourceAsset: 'KSM',
-    destinationAsset: 'KSM',
-  },
-  {
     sourceChainId: '0x00dcb981df86429de8bbacf9803401f09485366c44efbf53af9ecfab03adc7e5', // Kusama Bridge Hub
     destinationChainId: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe', // Kusama Relay
     sourceAsset: 'KSM',
@@ -347,60 +292,15 @@ export const XCM_DESTINATION_WHITELIST: XcmDestinationWhitelistEntry[] = [
   },
   {
     sourceChainId: '0x638cd2b9af4b3bb54b8c1f0d22711fc89924ca93300f0caf25a580432b29d050', // Kusama Coretime
-    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
-    sourceAsset: 'KSM',
-    destinationAsset: 'KSM',
-  },
-  {
-    sourceChainId: '0x638cd2b9af4b3bb54b8c1f0d22711fc89924ca93300f0caf25a580432b29d050', // Kusama Coretime
     destinationChainId: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe', // Kusama Relay
     sourceAsset: 'KSM',
     destinationAsset: 'KSM',
   },
   {
     sourceChainId: '0xefb56e30d9b4a24099f88820987d0f45fb645992416535d87650d98e00f46fc4', // Polkadot Coretime
-    destinationChainId: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f', // Polkadot Asset Hub
-    sourceAsset: 'DOT',
-    destinationAsset: 'DOT',
-  },
-  {
-    sourceChainId: '0xefb56e30d9b4a24099f88820987d0f45fb645992416535d87650d98e00f46fc4', // Polkadot Coretime
     destinationChainId: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3', // Polkadot Relay
     sourceAsset: 'DOT',
     destinationAsset: 'DOT',
-  },
-  {
-    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    destinationChainId: '0xfc41b9bd8ef8fe53d58c7ea67c794c7ec9a73daf05e6d54b14ff6342c99ba64c', // Acala
-    sourceAsset: 'DOT-Statemint',
-  },
-  {
-    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    destinationChainId: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f', // Polkadot Asset Hub
-    sourceAsset: 'DOT-Statemint',
-    destinationAsset: 'DOT',
-  },
-  {
-    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    destinationChainId: '0x9eb76c5184c4ab8679d2d5d819fdf90b9c001403e9e17da2e14b6d8aec4029c6', // Astar
-    sourceAsset: 'DOT-Statemint',
-    destinationAsset: 'ASTR',
-  },
-  {
-    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    destinationChainId: '0x262e1b2ad728475fd6fe88e62d34c200abe6fd693931ddad144059b1eb884e5b', // Bifrost Polkadot
-    sourceAsset: 'DOT-Statemint',
-    destinationAsset: 'DOT-Statemint',
-  },
-  {
-    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    destinationChainId: '0xfe58ea77779b7abda7da4ec526d14db9b1e9cd40a217c34892af80a9b332b76d', // Moonbeam
-    destinationAsset: 'GLMR',
-  },
-  {
-    sourceChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
-    destinationChainId: '0xf6ee56e9c5277df5b4ce6ae9983ee88f3cbed27d31beeb98f9f84f997a1ab0b9', // Mythos
-    destinationAsset: 'MYTH',
   },
   {
     sourceChainId: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe', // Kusama Relay
@@ -411,12 +311,6 @@ export const XCM_DESTINATION_WHITELIST: XcmDestinationWhitelistEntry[] = [
   {
     sourceChainId: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe', // Kusama Relay
     destinationChainId: '0xa85cfb9b9fd4d622a5b28289a02347af987d8f73fa3108450e2b4a11c1ce5755', // Basilisk
-    sourceAsset: 'KSM',
-    destinationAsset: 'KSM-Statemine',
-  },
-  {
-    sourceChainId: '0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe', // Kusama Relay
-    destinationChainId: '0x9f28c6a68e0fc9646eff64935684f6eeeece527e37bbe1f213d22caa1d9d6bed', // Bifrost Kusama
     sourceAsset: 'KSM',
     destinationAsset: 'KSM-Statemine',
   },
@@ -477,12 +371,6 @@ export const XCM_DESTINATION_WHITELIST: XcmDestinationWhitelistEntry[] = [
     sourceChainId: '0xf6ee56e9c5277df5b4ce6ae9983ee88f3cbed27d31beeb98f9f84f997a1ab0b9', // Mythos
     destinationChainId: '0xafdc188f45c71dacbaa0b62e16a91f726c7b8699a9748cdf715459de6b7f366d', // Hydration
     sourceAsset: 'MYTH',
-  },
-  {
-    sourceChainId: '0xc1af4cb4eb3918e5db15086c0cc5ec17fb334f728b7c65dd44bfe1e174ff8b3f', // Kusama People
-    destinationChainId: '0x48239ef607d7928874027a43a67689209727dfb3d3dc5e5b03a39bdc2eda771a', // Kusama Asset Hub
-    sourceAsset: 'KSM',
-    destinationAsset: 'KSM',
   },
   {
     sourceChainId: '0xc1af4cb4eb3918e5db15086c0cc5ec17fb334f728b7c65dd44bfe1e174ff8b3f', // Kusama People
