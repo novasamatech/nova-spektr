c33ce8295f5702259ae7cf55960b34b06881670e
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-12-09 16:41:45 -0300

    fix: vested transfer operation text crop

--- src/renderer/features/multisig-operations/components/Operation.tsx
+++ src/renderer/features/multisig-operations/components/Operation.tsx
@@ -83,8 +83,8 @@ export const Operation = memo(({ operation, multisigAccount, isDefaultOpen = fal
       className="rounded bg-block-background-default transition-shadow hover:shadow-card-shadow focus-visible:shadow-card-shadow"
     >
       <Accordion.Button buttonClass="px-2" iconWrapper="px-1.5">
-        <div className="flex h-[52px] w-full items-center gap-4 overflow-hidden">
-          <div className="flex w-full items-center gap-4 overflow-hidden">
+        <div className="flex h-[52px] w-full items-center overflow-hidden">
+          <div className="flex w-full items-center gap-x-2 overflow-hidden">
             <OperationTitleDate operation={operation} />
             <OperationIcon operation={operation} account={multisigAccount} />
             {titleNode}
