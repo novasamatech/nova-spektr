ecc6e9b6ba5bd5ee13851d67abfe9b28b149d73a
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2025-12-18 00:46:26 -0300

    fix: remove isTransaction checks
    As TODO says this component needs to be implemented for each operation

--- src/renderer/features/transfer-operation-details/components/TransactionAmount.tsx
+++ src/renderer/features/transfer-operation-details/components/TransactionAmount.tsx
@@ -2,13 +2,7 @@ import { cnTw } from '@/shared/lib/utils';
 import { AssetBalance } from '@/shared/ui-entities';
 import { type MultisigOperation } from '@/domains/network';
 import { AssetFiatBalance } from '@/entities/price';
-import {
-  findCoreTransaction,
-  getTransactionAmount,
-  isTransferTransaction,
-  isXcmTransaction,
-  useTransactionAsset,
-} from '@/entities/transaction';
+import { getTransactionAmount, useTransactionAsset } from '@/entities/transaction';
 
 type Props = {
   operation: MultisigOperation;
@@ -17,12 +11,6 @@ type Props = {
 
 // TODO it should be separated into multiple components for each set of operations (transfer/staking)
 export const TransactionAmount = ({ operation, className }: Props) => {
-  const coreTx = findCoreTransaction(operation.transaction);
-
-  if (!isTransferTransaction(coreTx) && !isXcmTransaction(coreTx)) {
-    return null;
-  }
-
   const transaction = operation.transaction;
   const value = transaction ? getTransactionAmount(transaction) : null;
   const asset = useTransactionAsset(transaction, operation.chainId);
