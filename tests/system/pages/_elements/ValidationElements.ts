56d8f224a3a04f2508cff1090de804b258f556f8
Sergey Malivanov <75333966+MaliDo@users.noreply.github.com>
2025-12-29 10:19:15 +0300

    test: fix transfer and validation tests (#5332)
    * test: fix transfer and validation tests
    
    * test: db update
    
    * test: mark failing tests
    
    * test: fix watch-only test
    
    * test: remove paths until fix
    
    * test: db improvement and validation fix
    
    ---------
    
    Co-authored-by: Sergey Malivanov <sergey.m@knowledgecity.pro>

--- tests/system/pages/_elements/ValidationElements.ts
+++ tests/system/pages/_elements/ValidationElements.ts
@@ -3,8 +3,8 @@ import { TEST_IDS } from '@/shared/constants';
 export class ValidationElements {
   static sendingAmountError = `${TEST_IDS.VALIDATIONS.BALANCE}:sending amount`;
   static networkFeeAmountError = `${TEST_IDS.VALIDATIONS.BALANCE}:fee`;
-  static crossChainFeeAmountError = `${TEST_IDS.VALIDATIONS.BALANCE}:cross-chain fee`;
-  static deliveryFeeAmountError = `${TEST_IDS.VALIDATIONS.BALANCE}:delivery fee`;
+  static originFeeAmountError = `${TEST_IDS.VALIDATIONS.BALANCE}:origin fee`;
+  static destinationFeeAmountError = `${TEST_IDS.VALIDATIONS.BALANCE}:destination fee`;
   static proxyDepositError = `${TEST_IDS.VALIDATIONS.BALANCE}:proxy deposit`;
   static multisigDepositError = `${TEST_IDS.VALIDATIONS.BALANCE}:multisig deposit`;
 }
