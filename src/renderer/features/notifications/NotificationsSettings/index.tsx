6b5071312644cd70ac5b3cce5bfe7a500f18a7c5
anton.e <anton.efanov@novasama.io>
2026-01-12 21:38:48 +0100

    refactor: proper feature creation

--- src/renderer/features/notifications/NotificationsSettings/index.tsx
+++ src/renderer/features/notifications/NotificationsSettings/index.tsx
@@ -1,16 +1,5 @@
-import { createFeature } from '@/shared/feature';
-import { generalActionsSlot } from '@/pages/Settings/Overview/components';
-
+import { notificationsSettingsFeature } from './feature';
 import { notificationsSettingsModel } from './model/notifications-settings-model';
 import { NotificationsSettingsModal } from './ui/NotificationsSettingsModal';
 
-export const notificationsSettingsFeature = createFeature({
-  name: 'notifications/settings',
-});
-
-export { notificationsSettingsModel, NotificationsSettingsModal };
-
-notificationsSettingsFeature.inject(generalActionsSlot, {
-  order: 1,
-  render: () => <NotificationsSettingsModal />,
-});
+export { notificationsSettingsModel, notificationsSettingsFeature, NotificationsSettingsModal };
