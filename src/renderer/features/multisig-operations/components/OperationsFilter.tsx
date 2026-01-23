acf2f061c852d72c9d46f77176e02a8e91d4d309
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2026-01-23 10:30:39 -0300

    feat: search within filters options

--- src/renderer/features/multisig-operations/components/OperationsFilter.tsx
+++ src/renderer/features/multisig-operations/components/OperationsFilter.tsx
@@ -1,10 +1,10 @@
 import { useUnit } from 'effector-react';
 import { type TFunction } from 'i18next';
-import { memo, useMemo } from 'react';
+import { memo, useMemo, useState } from 'react';
 
 import { TransactionType } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
-import { toAddress } from '@/shared/lib/utils';
+import { performSearch, toAddress } from '@/shared/lib/utils';
 import { Button, MultiSelect } from '@/shared/ui';
 import { type DropdownResult } from '@/shared/ui/types';
 import { Address } from '@/shared/ui-entities';
@@ -22,6 +22,10 @@ export const OperationsFilter = memo(() => {
   const chains = useUnit(networkModel.$chains);
   const multisigAccountsMap = useUnit(operationsContextModel.$multisigAccountsMap);
 
+  const [accountSearchQuery, setAccountSearchQuery] = useState('');
+  const [networkSearchQuery, setNetworkSearchQuery] = useState('');
+  const [typeSearchQuery, setTypeSearchQuery] = useState('');
+
   const multisigAccounts = useMemo(() => Array.from(multisigAccountsMap.values()), [multisigAccountsMap]);
   const resolvedMultisigAccounts = useAccountsNames(multisigAccounts);
 
@@ -33,18 +37,44 @@ export const OperationsFilter = memo(() => {
   }));
 
   const filtersOptions = useMemo(() => {
-    const AccountOptions = resolvedMultisigAccounts.map(account => ({
+    const filteredAccountOptions = performSearch({
+      query: accountSearchQuery,
+      records: resolvedMultisigAccounts,
+      getMeta: account => ({
+        address: toAddress(account.accountId),
+      }),
+      weights: { name: 1, address: 0.8 },
+    }).map(account => ({
       id: account.accountId,
       value: account.accountId,
       element: <Address showIcon variant="truncate" address={toAddress(account.accountId)} title={account.name} />,
     }));
 
+    const filteredNetworkOptions = performSearch({
+      query: networkSearchQuery,
+      records: NetworkOptions,
+      weights: { element: 1 },
+    });
+
+    const filteredTypeOptions = performSearch({
+      query: typeSearchQuery,
+      records: TransactionOptions,
+      weights: { element: 1 },
+    });
+
     return {
-      account: AccountOptions,
-      network: NetworkOptions,
-      type: TransactionOptions,
+      account: filteredAccountOptions,
+      network: filteredNetworkOptions,
+      type: filteredTypeOptions,
     };
-  }, [resolvedMultisigAccounts, NetworkOptions, TransactionOptions]);
+  }, [
+    resolvedMultisigAccounts,
+    NetworkOptions,
+    TransactionOptions,
+    accountSearchQuery,
+    networkSearchQuery,
+    typeSearchQuery,
+  ]);
 
   const handleFilterChange = (values: DropdownResult[], filterName: FilterName) => {
     const newSelectedOptions = { ...selectedOptions, [filterName]: values.map(v => v.id) };
@@ -88,20 +118,25 @@ export const OperationsFilter = memo(() => {
         selectedIds={selectedOptions.account}
         options={[...filtersOptions.account]}
         onChange={value => handleFilterChange(value, 'account')}
+        onSearch={setAccountSearchQuery}
       />
       <MultiSelect
+        showSelectAll
         className="w-[136px]"
         placeholder={t('operations.filters.networkPlaceholder')}
         selectedIds={selectedOptions.network}
         options={[...filtersOptions.network]}
         onChange={value => handleFilterChange(value, 'network')}
+        onSearch={setNetworkSearchQuery}
       />
       <MultiSelect
+        showSelectAll
         className="w-[136px]"
         placeholder={t('operations.filters.operationTypePlaceholder')}
         selectedIds={selectedOptions.type}
         options={[...filtersOptions.type]}
         onChange={value => handleFilterChange(value, 'type')}
+        onSearch={setTypeSearchQuery}
       />
     </div>
   );
