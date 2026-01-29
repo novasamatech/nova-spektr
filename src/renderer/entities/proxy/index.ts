f5a11c5ceb1f2b66d5d3cf2a2f2d7dfce13fa7c2
Anton <653026+savvyer@users.noreply.github.com>
2026-01-29 12:30:43 -0500

    feat: msig operations proxy type filter
    * feat: msig operations proxy type filter
    
    * fix: proxy type names
    
    * fix: imports

--- src/renderer/entities/proxy/index.ts
+++ src/renderer/entities/proxy/index.ts
@@ -1,5 +1,5 @@
 export { proxyModel } from './model/proxy-model';
-export { ProxyTypeName } from './lib/constants';
+export { ProxyTypeName, ProxyTypeOperation } from './lib/constants';
 export { proxyUtils } from './lib/proxy-utils';
 export { ProxyAccount } from './ui/ProxyAccount/ProxyAccount';
 export { ProxyPopover } from './ui/ProxyPopover';
