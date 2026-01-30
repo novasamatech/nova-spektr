b78652491e51440e41d1392f03b9d25b7b670174
Anton <653026+savvyer@users.noreply.github.com>
2026-01-30 10:24:18 -0500

    Feat/multisig operations search (#5579)
    * refactor: msig operations filters
    
    * feat: search msig operations
    
    * refactor: operations filters
    
    * fix: search by address
    
    * fix: date range filter

--- src/renderer/features/multisig-operations/components/OperationsFilter.tsx
+++ src/renderer/features/multisig-operations/components/OperationsFilter.tsx
@@ -6,17 +6,14 @@ import { TransactionType } from '@/shared/core';
 import { useI18n } from '@/shared/i18n';
 import { nonNullable, performSearch, toAddress } from '@/shared/lib/utils';
 import { Button, MultiSelect } from '@/shared/ui';
-import { type DropdownResult } from '@/shared/ui/types';
 import { Hash, WalletAccountIcon } from '@/shared/ui-entities';
-import { type DateRange, DateRangePicker } from '@/shared/ui-kit';
+import { DateRangePicker } from '@/shared/ui-kit';
 import { accountService, useWalletsNames } from '@/domains/network';
 import { networkModel } from '@/entities/network';
 import { ProxyTypeName } from '@/entities/proxy';
 import { accountUtils } from '@/entities/wallet';
 import { walletSelectService } from '@/aggregates/wallet-select';
 import { operationsContextModel } from '../model/context';
-
-type FilterName = 'account' | 'network' | 'type' | 'proxyType';
 
 export const OperationsFilter = memo(() => {
   const { t } = useI18n();
@@ -131,16 +128,6 @@ export const OperationsFilter = memo(() => {
     proxyTypeSearchQuery,
   ]);
 
-  const handleFilterChange = (values: DropdownResult[], filterName: FilterName) => {
-    const newSelectedOptions = { ...selectedOptions, [filterName]: values.map(v => v.id) };
-    operationsContextModel.setFilters(newSelectedOptions);
-  };
-
-  const handleDateRangeChange = (range: DateRange | undefined) => {
-    const newSelectedOptions = { ...selectedOptions, dateRange: range };
-    operationsContextModel.setFilters(newSelectedOptions);
-  };
-
   const clearFilters = () => {
     operationsContextModel.resetFilters();
   };
@@ -156,7 +143,7 @@ export const OperationsFilter = memo(() => {
         <DateRangePicker
           value={selectedOptions.dateRange}
           placeholder={t('operations.filters.dateRangePlaceholder')}
-          onChange={handleDateRangeChange}
+          onChange={range => operationsContextModel.setFilter({ dateRange: range })}
         />
       </div>
       <MultiSelect
@@ -165,7 +152,7 @@ export const OperationsFilter = memo(() => {
         placeholder={t('operations.filters.accountPlaceholder')}
         selectedIds={selectedOptions.account}
         options={[...filtersOptions.account]}
-        onChange={value => handleFilterChange(value, 'account')}
+        onChange={value => operationsContextModel.setFilter({ account: value.map(v => v.id) })}
         onSearch={setAccountSearchQuery}
       />
       <MultiSelect
@@ -174,7 +161,7 @@ export const OperationsFilter = memo(() => {
         placeholder={t('operations.filters.proxyTypePlaceholder')}
         selectedIds={selectedOptions.proxyType}
         options={[...filtersOptions.proxyType]}
-        onChange={value => handleFilterChange(value, 'proxyType')}
+        onChange={value => operationsContextModel.setFilter({ proxyType: value.map(v => v.id) })}
         onSearch={setProxyTypeSearchQuery}
       />
       <MultiSelect
@@ -183,7 +170,7 @@ export const OperationsFilter = memo(() => {
         placeholder={t('operations.filters.networkPlaceholder')}
         selectedIds={selectedOptions.network}
         options={[...filtersOptions.network]}
-        onChange={value => handleFilterChange(value, 'network')}
+        onChange={value => operationsContextModel.setFilter({ network: value.map(v => v.id) })}
         onSearch={setNetworkSearchQuery}
       />
       <MultiSelect
@@ -192,7 +179,7 @@ export const OperationsFilter = memo(() => {
         placeholder={t('operations.filters.operationTypePlaceholder')}
         selectedIds={selectedOptions.type}
         options={[...filtersOptions.type]}
-        onChange={value => handleFilterChange(value, 'type')}
+        onChange={value => operationsContextModel.setFilter({ type: value.map(v => v.id) })}
         onSearch={setTypeSearchQuery}
       />
     </div>
