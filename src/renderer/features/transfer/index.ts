f61b0b7740268345dc28b76b98f1a8f8a783d3d7
Anton <aaa_eee@icloud.com>
2026-02-11 00:25:53 +0100

    feat(address book): send to. SPEK-73. (#5618)
    * feat: add send to modal
    
    * chore(transfer): mv to feature
    
    * chore(transfer): handle nullable price
    
    * chore(transfer): handle nullable price
    
    ---------
    
    Co-authored-by: Vitya Livshits <64482828+cuteWarmFrog@users.noreply.github.com>

---
+++ src/renderer/features/transfer/index.ts
@@ -0,0 +1,6 @@
+export { transferModel as defaultTransferModel } from './model/transfer-model';
+export { Transfer as DefaultTransfer } from './ui/Transfer';
+
+export { xcmSpellTransferModel } from './model/xcm-spell-transfer-model';
+
+export type { BalanceMap, NetworkStore, TransferStore } from './lib/types';
