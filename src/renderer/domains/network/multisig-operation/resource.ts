d811a73a3116f926b6778986a968e376c80d7eab
anton.e <anton.efanov@novasama.io>
2026-02-19 18:17:07 +0100

    refactor: fix tests

--- src/renderer/domains/network/multisig-operation/resource.ts
+++ src/renderer/domains/network/multisig-operation/resource.ts
@@ -23,7 +23,8 @@ import { polkadotjsHelpers } from '@/shared/polkadotjs-helpers';
 import { type AccountId, pjsSchema } from '@/shared/polkadotjs-schemas';
 import { createQueryResource, createSubscriptionResource } from '@/shared/query';
 import { type MapCacheFn } from '@/shared/query/types';
-import { decodeCallData } from '@/entities/transaction';
+// eslint-disable-next-line boundaries/entry-point
+import { decodeCallData } from '@/entities/transaction/lib/callDataDecoder';
 
 import { INDEXER_URL } from './constants';
 import { multisigOperationService } from './service';
