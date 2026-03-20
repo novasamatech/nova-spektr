335a5e6dbff5e82b076ad7cc16760f708126788e
Anton Savvyer <653026+savvyer@users.noreply.github.com>
2026-03-20 21:11:36 +0000

    fix: support single parity signer accounts

--- src/renderer/features/contacts/BackendConfiguration/model/auth-model.ts
+++ src/renderer/features/contacts/BackendConfiguration/model/auth-model.ts
@@ -3,6 +3,7 @@ import { t } from 'i18next';
 import { interval, once } from 'patronum';
 import { toast } from 'sonner';
 
+import { RelayChains } from '@/shared/lib/utils';
 import { type AccountId } from '@/shared/polkadotjs-schemas';
 import { type AnyAccount, accountService } from '@/domains/network';
 import { accountUtils, walletModel, walletUtils } from '@/entities/wallet';
@@ -67,7 +68,11 @@ const $signableAccounts = walletModel.$wallets.map((wallets): SignableAccount[] 
     .filter((w) => walletUtils.isPolkadotVaultGroup(w))
     .flatMap((wallet) =>
       wallet.accounts.flatMap((account): SignableAccount[] => {
-        if (accountUtils.isVaultChainAccount(account) || accountUtils.isVaultShardAccount(account)) {
+        const isSignable = walletUtils.isSingleShard(wallet)
+          ? accountUtils.isVaultBaseAccount(account)
+          : accountUtils.isVaultChainAccount(account) || accountUtils.isVaultShardAccount(account);
+
+        if (isSignable) {
           return [
             {
               account,
@@ -203,7 +208,7 @@ sample({
     const signatory = account!.account;
     const messageText = buildSignMessage(challengeData.nonce);
     const message = new TextEncoder().encode(messageText);
-    const chainId = accountService.isChainAccount(signatory) ? signatory.chainId : undefined;
+    const chainId = accountService.isChainAccount(signatory) ? signatory.chainId : RelayChains.POLKADOT;
 
     return { message, signatory, chainId };
   },
