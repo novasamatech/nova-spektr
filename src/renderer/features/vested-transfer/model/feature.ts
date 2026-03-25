81db0173c6e910e05f0f9094e946ae49f77c2b54
Anton <653026+savvyer@users.noreply.github.com>
2026-03-25 10:24:06 -0500

    fix: restore custom operations features (#5742)

--- src/renderer/features/vested-transfer/model/feature.ts
+++
@@ -1,7 +0,0 @@
-import { $features } from '@/shared/config/features';
-import { createFeature } from '@/shared/feature';
-
-export const vestedTransferFeature = createFeature({
-  name: 'vested/transfer',
-  enable: $features.map(({ vestedTransfer }) => vestedTransfer),
-});
